cacaf063a69d1758ac3375f45aa104d6f0003518
xiashuqing <xiashuqing@shizitegong.com>
2018-04-19 13:42:41 +0800

    微服务调用模板

---
+++ hyjf_cs_user/src/main/java/com/hyjf/cs/user/client/AmUserClient.java
@@ -0,0 +1,8 @@
+package com.hyjf.cs.user.client;
+
+/**
+ * @author xiasq
+ * @version AmUserClient, v0.1 2018/4/19 12:44
+ */
+public interface AmUserClient {
+}
---
+++ hyjf_cs_user/src/main/java/com/hyjf/cs/user/client/AmUserClientImpl.java
@@ -0,0 +1,12 @@
+package com.hyjf.cs.user.client;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author xiasq
+ * @version AmUserClientImpl, v0.1 2018/4/19 12:44
+ */
+
+@Service
+public class AmUserClientImpl implements AmUserClient{
+}
