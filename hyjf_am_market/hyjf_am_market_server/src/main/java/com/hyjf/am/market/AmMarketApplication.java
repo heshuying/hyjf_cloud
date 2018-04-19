7d23a7458a7ec335c81924f301cf11f5c4cf1f6c
xiashuqing <xiashuqing@shizitegong.com>
2018-04-19 16:38:31 +0800

    解决batch不能通过main启动问题

--- hyjf_am_market/hyjf_am_market_server/src/main/java/com/hyjf/am/market/AmMarketApplication.java
+++ hyjf_am_market/hyjf_am_market_server/src/main/java/com/hyjf/am/market/AmMarketApplication.java
@@ -3,10 +3,18 @@ package com.hyjf.am.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AmMarketApplication {
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmMarketApplication.class, args);
---
+++ hyjf_batch/README.md
@@ -0,0 +1,5 @@
+#批处理微服务
+## 启动
+  导入sql文件（batch.sql）, tables_mysql_innodb.sql是从官方下载的但是索引长度超长，修改为batch.sql
+   
+  
