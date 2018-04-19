bc5743126d4a5eaf8ea7c9209a329709b786d8c1
dengxiaojiang <dengxiaojiang@hyjf.com>
2018-04-19 10:22:53 +0800

    重新整理工程结构

---
+++ hyjf_am_user/hyjf_am_user_server/src/test/java/com/hyjf/am/user/redis/TestRedis.java
@@ -0,0 +1,51 @@
+package com.hyjf.am.user.redis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.hyjf.am.user.AmUserApplication;
+
+/**
+ * @author xiasq
+ * @version TestDao, v0.1 2018/4/19 9:04
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AmUserApplication.class)
+public class TestRedis {
+    Logger logger = LoggerFactory.getLogger(TestRedis.class);
+
+    @Autowired
+    RedisUtils redisUtils;
+
+    @Autowired
+    StringRedisUtil stringRedisUtil;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testRedisWrite(){
+        redisUtils.set("key","value");
+    }
+
+//    @Test
+//    public String testRedisRead(){
+//        String value = stringRedisUtil.get("key");
+//        return value;
+//    }
+//
+//    @Test
+//    public void testBaseRedisOpertator(){
+//        redisTemplate.opsForValue().set("base","testValue1");
+//        Object value = redisTemplate.opsForValue().get("base");
+//        logger.info("testBaseRedisOpertator value is :{}", value.toString());
+//    }
+
+}
