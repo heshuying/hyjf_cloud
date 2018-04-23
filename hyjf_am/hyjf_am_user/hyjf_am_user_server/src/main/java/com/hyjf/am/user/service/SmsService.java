a0b80e0822745b98b10215ac5eb1a1e037c4dddd
xiashuqing <xiashuqing@shizitegong.com>
2018-04-23 17:21:56 +0800

    合并版本

---
+++ hyjf_am/hyjf_am_user/hyjf_am_user_server/src/main/java/com/hyjf/am/user/service/SmsService.java
@@ -0,0 +1,25 @@
+package com.hyjf.am.user.service;
+
+/**
+ * @author xiasq
+ * @version SmsService, v0.1 2018/4/12 17:32
+ */
+public interface SmsService {
+    int save(String mobile, String verificationType, String verificationCode, String platform, Integer status);
+
+    /**
+     * 检查短信验证码
+     */
+    int checkMobileCode(String mobile, String verificationCode, String verificationType, String platform, Integer status, Integer updateStatus);
+
+    /**
+     * 发送的短信验证码保存到数据库
+     * @param mobile
+     * @param verificationCode
+     * @param verificationType
+     * @param status
+     * @param platform
+     * @return
+     */
+    int saveSmsCode(String mobile, String verificationCode, String verificationType, Integer status, String platform);
+}
