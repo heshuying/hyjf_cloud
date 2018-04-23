2744ce5bbdaf38e71e3a307103b2fd60847e55f9
xiashuqing <xiashuqing@shizitegong.com>
2018-04-23 17:54:18 +0800

    恢复误删除文件

---
+++ hyjf_common/src/main/java/com/hyjf/common/util/MessageConstants.java
@@ -0,0 +1,14 @@
+package com.hyjf.common.util;
+
+public interface MessageConstants {
+
+	/** ECOML001=用户名或密码不正确，请重新输入。 */
+	String ECOML001 = "ECOML001";
+
+	/** ECOMS001=邮件服务器连接失败。 */
+	String ECOMS001 = "ECOMS001";
+
+	/** 用户还没有开户 */
+	String ERRORS_NOTOPENACCOUNT = "notopenaccount";
+
+}
