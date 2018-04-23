998c1cebcbcba5023bbf21a944f77a43cdc5da96
xiashuqing <xiashuqing@shizitegong.com>
2018-04-23 17:02:17 +0800

    优化项目结构

--- hyjf_am_server/hyjf_am_market/hyjf_am_market_server/src/main/java/com/hyjf/am/market/service/ActivityBatchService.java
+++
@@ -1,9 +0,0 @@
-package com.hyjf.am.market.service;
-
-/**
- * @author xiasq
- * @version ActivityBatchService, v0.1 2018/4/19 15:42
- */
-public interface ActivityBatchService {
-    void updateActivityEndStatus();
-}
