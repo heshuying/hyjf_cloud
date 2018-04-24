7390d8bb3527c56bfb60bf9337bb92038cfb032c
xiashuqing <xiashuqing@shizitegong.com>
2018-04-24 17:30:06 +0800

    解决编译错误

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/client/impl/AmConfigClientImpl.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/client/impl/AmConfigClientImpl.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 
 import com.hyjf.cs.user.client.AmConfigClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @author xiasq
  * @version AmConfigClientImpl, v0.1 2018/4/23 20:01
  */
+@Service
 public class AmConfigClientImpl implements AmConfigClient {
     private static Logger logger = LoggerFactory.getLogger(AmConfigClientImpl.class);
 
