5a45d2d8d958249fe2ec0f885c9b4fb40c6b1331
xiashuqing <xiashuqing@shizitegong.com>
2018-04-25 13:56:58 +0800

    删除报错代码

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/service/BankOpenService.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/service/BankOpenService.java
@@ -1,14 +1,5 @@
 package com.hyjf.cs.user.service;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.hyjf.common.web.WebViewUser;
-import com.hyjf.am.vo.user.UserVO;
-import com.hyjf.common.exception.MQException;
-import com.hyjf.common.exception.ReturnMessageException;
-import com.hyjf.cs.user.vo.RegisterVO;
-
 /**
  * 
  * @author Administrator
@@ -16,12 +7,5 @@ import com.hyjf.cs.user.vo.RegisterVO;
  */
 
 public interface BankOpenService {
-	
-	UserVO register(RegisterVO registerVO, HttpServletRequest request, HttpServletResponse response) throws ReturnMessageException;
 
-	boolean existUser(String mobile);
-
-	WebViewUser getWebViewUserByUserId(Integer userId);
-
-	void sendSmsCode(String validCodeType, String mobile, HttpServletRequest request) throws MQException;
 }
