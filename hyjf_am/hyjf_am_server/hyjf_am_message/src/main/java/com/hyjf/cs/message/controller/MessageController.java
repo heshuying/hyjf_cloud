50e36feae7dcb4b8f540a224a22e5f03ee836606
xiashuqing <xiashuqing@shizitegong.com>
2018-05-07 11:43:20 +0800

    删除无用文件

---
+++ hyjf_am/hyjf_am_server/hyjf_am_message/src/main/java/com/hyjf/cs/message/controller/MessageController.java
@@ -0,0 +1,12 @@
+package com.hyjf.cs.message.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiasq
+ * @version MessageController, v0.1 2018/5/7 9:55
+ * 提供消息查询服务:  app消息
+ */
+@RestController
+public class MessageController {
+}
