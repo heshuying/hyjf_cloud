5ca389071b841daa8c7ca4233fa141eab87c59d5
xiashuqing <xiashuqing@shizitegong.com>
2018-05-08 16:35:32 +0800

    提交测试类

---
+++ hyjf_cs_server/hyjf_cs_message/src/test/java/com/hyjf/cs/message/test/MqTest.java
@@ -0,0 +1,18 @@
+package com.hyjf.cs.message.test;
+
+import com.hyjf.cs.message.CsMessageApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author xiasq
+ * @version MqTest, v0.1 2018/5/8 16:31
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CsMessageApplication.class)
+public class MqTest {
+
+
+}
