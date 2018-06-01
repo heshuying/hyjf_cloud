51cac963b8f1027894ee635fb595d6ec605e3460
xiashuqing <xiashuqing@shizitegong.com>
2018-06-01 17:06:11 +0800

    增加资产表

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_borrow/src/main/java/com/hyjf/am/borrow/dao/mapper/auto/HjhAssetBorrowTypeMapper.java
@@ -0,0 +1,30 @@
+package com.hyjf.am.borrow.dao.mapper.auto;
+
+import com.hyjf.am.borrow.dao.model.auto.HjhAssetBorrowType;
+import com.hyjf.am.borrow.dao.model.auto.HjhAssetBorrowTypeExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface HjhAssetBorrowTypeMapper {
+    int countByExample(HjhAssetBorrowTypeExample example);
+
+    int deleteByExample(HjhAssetBorrowTypeExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(HjhAssetBorrowType record);
+
+    int insertSelective(HjhAssetBorrowType record);
+
+    List<HjhAssetBorrowType> selectByExample(HjhAssetBorrowTypeExample example);
+
+    HjhAssetBorrowType selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") HjhAssetBorrowType record, @Param("example") HjhAssetBorrowTypeExample example);
+
+    int updateByExample(@Param("record") HjhAssetBorrowType record, @Param("example") HjhAssetBorrowTypeExample example);
+
+    int updateByPrimaryKeySelective(HjhAssetBorrowType record);
+
+    int updateByPrimaryKey(HjhAssetBorrowType record);
+}
