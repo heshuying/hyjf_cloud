ba0b4fa1d4ce5a06dc13b6f0af258bba8ff6819c
zhangqingqing <zhangqingqing@hyjf.com>
2018-06-05 13:05:13 +0800

    修改充值接口bug

---
+++ hyjf_cs_server/hyjf_cs_borrow/src/main/java/com/hyjf/cs/borrow/redis/RedisConfig.java
@@ -0,0 +1,90 @@
+package com.hyjf.cs.borrow.redis;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author xiasq
+ * @version RedisConfig, v0.1 2018/1/21 22:33
+ */
+
+@Configuration
+@ConditionalOnClass({ RedisOperations.class })
+@EnableAutoConfiguration
+public class RedisConfig {
+
+	private static final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+
+	@Bean
+	@ConditionalOnMissingBean(name = "redisTemplate")
+	public RedisTemplate<String, Object> getRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+		template.setConnectionFactory(redisConnectionFactory);
+		// fastjson默认情况下会开启autoType的检查，添加autotype白名单
+		ParserConfig.getGlobalInstance().addAccept("com.hyjf.");
+
+		// 使用fastjson序列化
+		FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
+		// value值的序列化采用fastJsonRedisSerializer
+		template.setValueSerializer(fastJsonRedisSerializer);
+		template.setHashValueSerializer(fastJsonRedisSerializer);
+		// key的序列化采用StringRedisSerializer
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		return template;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(StringRedisTemplate.class)
+	public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		StringRedisTemplate template = new StringRedisTemplate();
+		template.setConnectionFactory(redisConnectionFactory);
+		return template;
+	}
+
+	public static class FastJsonRedisSerializer<T> implements RedisSerializer<T> {
+
+		public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+
+		private Class<T> clazz;
+
+		public FastJsonRedisSerializer(Class<T> clazz) {
+			super();
+			this.clazz = clazz;
+		}
+
+		@Override
+		public byte[] serialize(T t) throws SerializationException {
+			if (null == t) {
+				return new byte[0];
+			}
+			return JSON.toJSONString(t, SerializerFeature.WriteClassName).getBytes(DEFAULT_CHARSET);
+		}
+
+		@Override
+		public T deserialize(byte[] bytes) throws SerializationException {
+			if (null == bytes || bytes.length <= 0) {
+				return null;
+			}
+			String str = new String(bytes, DEFAULT_CHARSET);
+			return (T) JSON.parseObject(str, clazz);
+		}
+	}
+}
