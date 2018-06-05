70beba3c6ccbb38ed44f2330c39de79cf39b48a6
zhangqingqing <zhangqingqing@hyjf.com>
2018-06-05 12:37:35 +0800

    添加 callcenter

---
+++ hyjf_platform_server/hyjf_callcenter/src/main/java/com/hyjf/callcenter/config/SystemConfig.java
@@ -0,0 +1,84 @@
+package com.hyjf.callcenter.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SystemConfig {
+    @Value("${hyjf.web.host}")
+    public String webHost;
+
+    @Value("${http.hyjf.web.host}")
+    public String httpWebHost;
+
+    @Value("${hyjf.bank.instcode}")
+    public String bankInstcode;
+
+    @Value("${hyjf.bank.bankcode}")
+    public String bankCode;
+
+    @Value("${file.domain.head.url}")
+    public String headUrl;
+
+    @Value("${file.domain.url}")
+    public String fileDomainUrl;
+
+    @Value("${file.upload.real.path}")
+    public String fileUpload;
+
+    public String getWebHost() {
+        return webHost;
+    }
+
+    public void setWebHost(String webHost) {
+        this.webHost = webHost;
+    }
+
+    public String getHttpWebHost() {
+        return httpWebHost;
+    }
+
+    public void setHttpWebHost(String httpWebHost) {
+        this.httpWebHost = httpWebHost;
+    }
+
+    public String getBankInstcode() {
+        return bankInstcode;
+    }
+
+    public void setBankInstcode(String bankInstcode) {
+        this.bankInstcode = bankInstcode;
+    }
+
+    public String getBankCode() {
+        return bankCode;
+    }
+
+    public void setBankCode(String bankCode) {
+        this.bankCode = bankCode;
+    }
+
+    public String getHeadUrl() {
+        return headUrl;
+    }
+
+    public void setHeadUrl(String headUrl) {
+        this.headUrl = headUrl;
+    }
+
+    public String getFileDomainUrl() {
+        return fileDomainUrl;
+    }
+
+    public void setFileDomainUrl(String fileDomainUrl) {
+        this.fileDomainUrl = fileDomainUrl;
+    }
+
+    public String getFileUpload() {
+        return fileUpload;
+    }
+
+    public void setFileUpload(String fileUpload) {
+        this.fileUpload = fileUpload;
+    }
+}
