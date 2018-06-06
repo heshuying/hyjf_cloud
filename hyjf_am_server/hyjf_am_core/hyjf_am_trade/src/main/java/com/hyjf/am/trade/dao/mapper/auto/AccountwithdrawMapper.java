ca623bf41e983ebd1a5bd94aa6b02f093ca55727
xiashuqing <xiashuqing@shizitegong.com>
2018-06-06 16:17:48 +0800

    解决部分服务启动失败（找不到的类直接删除了，按需增加）

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/dao/mapper/auto/AccountwithdrawMapper.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/dao/mapper/auto/AccountwithdrawMapper.java
@@ -1,5 +1,6 @@
 package com.hyjf.am.trade.dao.mapper.auto;
 
+import com.hyjf.am.trade.dao.model.auto.Accountwithdraw;
 import com.hyjf.am.trade.dao.model.auto.AccountwithdrawExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
