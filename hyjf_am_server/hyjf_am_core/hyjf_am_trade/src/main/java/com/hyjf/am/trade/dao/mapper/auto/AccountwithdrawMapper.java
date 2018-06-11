538779ac1e0fab6acde91412b81a8af49aa0a84c
xiashuqing <xiashuqing@shizitegong.com>
2018-06-11 15:14:07 +0800

    更新错误的pom文件

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/dao/mapper/auto/AccountwithdrawMapper.java
@@ -0,0 +1,30 @@
+package com.hyjf.am.trade.dao.mapper.auto;
+
+import com.hyjf.am.trade.dao.model.auto.Accountwithdraw;
+import com.hyjf.am.trade.dao.model.auto.AccountwithdrawExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface AccountwithdrawMapper {
+    int countByExample(AccountwithdrawExample example);
+
+    int deleteByExample(AccountwithdrawExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Accountwithdraw record);
+
+    int insertSelective(Accountwithdraw record);
+
+    List<Accountwithdraw> selectByExample(AccountwithdrawExample example);
+
+    Accountwithdraw selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Accountwithdraw record, @Param("example") AccountwithdrawExample example);
+
+    int updateByExample(@Param("record") Accountwithdraw record, @Param("example") AccountwithdrawExample example);
+
+    int updateByPrimaryKeySelective(Accountwithdraw record);
+
+    int updateByPrimaryKey(Accountwithdraw record);
+}
---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.trade.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hyjf.am.trade.dao.mapper.auto.HjhInstConfigMapper;
+import com.hyjf.am.trade.dao.model.auto.HjhInstConfig;
+import com.hyjf.am.trade.dao.model.auto.HjhInstConfigExample;
+import com.hyjf.am.trade.service.UserService;
+
+/**
+ * @author zhangqingqing
+ * @version UserServiceImpl, v0.1 2018/5/28 12:53
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    @Autowired
+    HjhInstConfigMapper hjhInstConfigMapper;
+    /**
+     * 根据机构编号检索机构信息
+     *
+     * @param instCode
+     * @return
+     */
+    @Override
+    public HjhInstConfig selectInstConfigByInstCode(String instCode) {
+        HjhInstConfigExample example = new HjhInstConfigExample();
+        HjhInstConfigExample.Criteria cra = example.createCriteria();
+        cra.andInstCodeEqualTo(instCode);
+        List<HjhInstConfig> list = this.hjhInstConfigMapper.selectByExample(example);
+        if (list != null && list.size() > 0) {
+            return list.get(0);
+        }
+        return null;
+    }
+
+}
