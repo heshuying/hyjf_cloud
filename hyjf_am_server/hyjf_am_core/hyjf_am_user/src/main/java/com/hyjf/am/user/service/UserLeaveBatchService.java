fe1363cb3af4175aa005b1fb8108d9303cb1e9c1
wangjun <273407491@qq.com>
2018-06-12 17:22:45 +0800

    员工入职，离职定时任务

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/service/UserLeaveBatchService.java
@@ -0,0 +1,9 @@
+package com.hyjf.am.user.service;
+
+/**
+ * @author wangjun
+ * @version UserLeaveBatchService, v0.1 2018/6/12 14:58
+ */
+public interface UserLeaveBatchService {
+    void updateUserLeaveInfo();
+}
