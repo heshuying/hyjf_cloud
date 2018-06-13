e09acce38276e0be354f9c059c0dc0dede8a58d2
dengxiaojiang <dengxiaojiang@hyjf.com>
2018-06-13 15:09:48 +0800

    配置中心param 缓存处理

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_config/src/main/java/com/hyjf/am/config/controller/task/SynParamController.java
@@ -0,0 +1,40 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.config.controller.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hyjf.am.config.service.SynParamService;
+import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.BaseVO;
+
+/**
+ * @author dxj
+ * @version SynParamController.java, v0.1 2018年6月13日 上午11:24:09
+ */
+@RestController
+@RequestMapping("/am-config/sync")
+public class SynParamController {
+	
+    @Autowired
+    private SynParamService synParamService;
+
+    
+    /**
+     * 获取银行卡配置信息
+     * @param bankId
+     * @return
+     */
+    @GetMapping("/synParam")
+    public Response<BaseVO> synParam(){
+    	Response<BaseVO>  response = new Response<BaseVO>();
+    	
+    	synParamService.synParam();
+    	
+        return response;
+    }
+}
---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_config/src/main/java/com/hyjf/am/config/dao/model/auto/ParamName.java
@@ -0,0 +1,108 @@
+package com.hyjf.am.config.dao.model.auto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ParamName extends ParamNameKey implements Serializable {
+    private String name;
+
+    private String other1;
+
+    private String other2;
+
+    private String other3;
+
+    private Short sort;
+
+    private Integer delFlag;
+
+    private Integer createUserId;
+
+    private Integer updateUserId;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getOther1() {
+        return other1;
+    }
+
+    public void setOther1(String other1) {
+        this.other1 = other1 == null ? null : other1.trim();
+    }
+
+    public String getOther2() {
+        return other2;
+    }
+
+    public void setOther2(String other2) {
+        this.other2 = other2 == null ? null : other2.trim();
+    }
+
+    public String getOther3() {
+        return other3;
+    }
+
+    public void setOther3(String other3) {
+        this.other3 = other3 == null ? null : other3.trim();
+    }
+
+    public Short getSort() {
+        return sort;
+    }
+
+    public void setSort(Short sort) {
+        this.sort = sort;
+    }
+
+    public Integer getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(Integer delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public Integer getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(Integer createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Integer getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public void setUpdateUserId(Integer updateUserId) {
+        this.updateUserId = updateUserId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
