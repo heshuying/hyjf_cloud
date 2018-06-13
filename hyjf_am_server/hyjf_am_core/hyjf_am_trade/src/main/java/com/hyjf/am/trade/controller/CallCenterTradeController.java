c3fd9f0a611116305bc6cbf0d97dd45796fe0c19
wangjun <273407491@qq.com>
2018-06-13 14:24:22 +0800

    呼叫中心整理

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/CallCenterTradeController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/CallCenterTradeController.java
@@ -12,12 +12,15 @@ import com.hyjf.am.trade.service.CallCenterTradeService;
 import com.hyjf.am.vo.callcenter.CallCenterHtjRepaymentDetailVO;
 import com.hyjf.am.vo.callcenter.CallCenterHztRepaymentDetailVO;
 import com.hyjf.common.util.CommonUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -27,13 +30,16 @@ import java.util.List;
  * @author wangjun
  * @version CallCenterTradeController, v0.1 2018/6/11 17:52
  */
+@Api(value = "呼叫中心TRADE原子层")
 @RestController
 @RequestMapping("/am-trade/callcenter")
 public class CallCenterTradeController {
     @Autowired
     CallCenterTradeService callCenterTradeService;
     private static final Logger logger = LoggerFactory.getLogger(CallCenterTradeController.class);
-    @RequestMapping("/getHztRepaymentDetailList")
+
+    @ApiOperation(value = "查询还款明细（直投产品，含承接的债权）")
+    @RequestMapping(value = "/getHztRepaymentDetailList",method = RequestMethod.POST)
     public CallCenterHztRepaymentResponse getHztRepaymentDetailList(@RequestBody @Valid CallCenterRepaymentRequest request){
         CallCenterHztRepaymentResponse callCenterHztRepaymentResponse = new CallCenterHztRepaymentResponse();
         List<CallCenterHztRepaymentDetailCustomize> list = callCenterTradeService.getHztRepaymentDetailList(request);
@@ -44,7 +50,8 @@ public class CallCenterTradeController {
         return callCenterHztRepaymentResponse;
     }
 
-    @RequestMapping("/getHtjRepaymentDetailList")
+    @ApiOperation(value = "查询还款明细（汇添金）")
+    @RequestMapping(value = "/getHtjRepaymentDetailList", method = RequestMethod.POST)
     public CallCenterHtjRepaymentResponse getHtjRepaymentDetailList(@RequestBody @Valid CallCenterRepaymentRequest request){
         CallCenterHtjRepaymentResponse callCenterHtjRepaymentResponse = new CallCenterHtjRepaymentResponse();
         List<CallCenterHtjRepaymentDetailCustomize> list = callCenterTradeService.getHtjRepaymentDetailList(request);
