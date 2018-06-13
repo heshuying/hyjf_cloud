9cbd4e4599b1a325912092445030424a11284e38
xiashuqing <xiashuqing@shizitegong.com>
2018-06-13 08:58:17 +0800

    优化类名

--- hyjf_cs_server/hyjf_cs_trade/src/test/java/com/hyjf/cs_borrow/CsTradeApplicationTests.java
+++ hyjf_cs_server/hyjf_cs_trade/src/test/java/com/hyjf/cs_borrow/CsTradeApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CsBorrowApplicationTests {
+public class CsTradeApplicationTests {
 
 	@Test
 	public void contextLoads() {
