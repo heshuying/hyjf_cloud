3a0916c033d1524170782deb33996749150ea4f1
xiashuqing <xiashuqing@shizitegong.com>
2018-06-15 13:48:12 +0800

    增加demobatch

--- hyjf_platform_server/hyjf_batch/README.md
+++ hyjf_platform_server/hyjf_batch/README.md
@@ -2,4 +2,4 @@
 ## 启动
   导入sql文件（batch.sql）, tables_mysql_innodb.sql是从官方下载的但是索引长度超长，修改为batch.sql
    
-  
+  Quartz默认并发执行， 如果要禁止你的job并发执行，在job上标注 @DisallowConcurrentExecution 
---
+++ hyjf_platform_server/hyjf_batch/src/main/java/com/hyjf/batch/job/DemoJob.java
@@ -0,0 +1,24 @@
+package com.hyjf.batch.job;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author xiasq
+ * @version DemoJob, v0.1 2018/6/14 16:33
+ */
+@DisallowConcurrentExecution  // 禁止并发执行
+public class DemoJob extends BaseJob implements Job {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        logger.info("demoJob: {} execute...", context.getJobDetail().getKey().getName());
+        restTemplate.getForEntity("http://CS-MARKET/batch/demo", String.class);
+        logger.info("demoJob execute end...");
+    }
+}
