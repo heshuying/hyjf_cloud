e48031642eca8c904afdec688d59680606fb26d9
jun <jijunjun@hyjf.com>
2018-06-16 14:19:47 +0800

    充值掉单提现掉单定时任务

---
+++ hyjf_platform_server/hyjf_batch/src/main/java/com/hyjf/batch/job/exception/bankrecharge/BankRechargeExceptionJob.java
@@ -0,0 +1,28 @@
+package com.hyjf.batch.job.exception.bankrecharge;
+
+import com.hyjf.batch.job.BaseJob;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 充值掉单定时任务 create by jijun 20180612
+ */
+public class BankRechargeExceptionJob extends BaseJob implements Job {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 执行定时任务
+     * @param context
+     * @throws JobExecutionException
+     */
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        logger.info("BankRechargeExceptionJob: {} execute...", context.getJobDetail().getKey().getName());
+        restTemplate.getForEntity("http://AM-TRADE/bankRechargeException/recharge", String.class);
+        logger.info("BankRechargeExceptionJob execute end...");
+    }
+}
