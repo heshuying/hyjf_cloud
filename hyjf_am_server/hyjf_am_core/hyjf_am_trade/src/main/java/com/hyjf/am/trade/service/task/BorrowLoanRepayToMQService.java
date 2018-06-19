f4fbf67bf22e60bb3ce0f085e5c766b569826094
dengxiaojiang <dengxiaojiang@hyjf.com>
2018-06-19 15:49:02 +0800

    放款还款 定时任务

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/task/BorrowLoanRepayToMQService.java
@@ -0,0 +1,14 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.trade.service.task;
+
+/**
+ * @author dxj
+ * @version BorrowLoanRepayToMQService.java, v0.1 2018年6月19日 上午11:28:09
+ */
+public interface BorrowLoanRepayToMQService {
+
+	void taskAssign();
+
+}
