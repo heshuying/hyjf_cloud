bdf86126dec1cf1cfa277d5d20ef85751b4072be
zhangyunkai <zhangyunkai@hyjf.com>
2018-06-19 16:23:16 +0800

    ## 增加债转列表查询

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/resquest/trade/CreditListRequest.java
@@ -0,0 +1,112 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.resquest.trade;
+
+import com.hyjf.am.vo.BaseVO;
+
+/**
+ * 债转列表请求参数
+ *
+ * @author zhangyk
+ */
+public class CreditListRequest extends BaseVO {
+
+    /**
+     * 项目期限最小值
+     */
+    private  int borrowPeriodMin;
+    /**
+     * 项目期限最大值
+     */
+    private int borrowPeriodMax;
+    /**
+     * 项目收益最小值
+     */
+    private int borrowAprMin;
+    /**
+     * 项目收益最大值
+     */
+    private int borrowAprMax;
+    /**
+     * 折价比例排序
+     */
+    private String discountSort;
+    /**
+     * 期限排序
+     */
+    private String termSort;
+    /**
+     * 金额排序
+     */
+    private String capitalSort;
+    /**
+     * 进度排序
+     */
+    private String inProgressSort;
+
+
+    public int getBorrowPeriodMin() {
+        return borrowPeriodMin;
+    }
+
+    public void setBorrowPeriodMin(int borrowPeriodMin) {
+        this.borrowPeriodMin = borrowPeriodMin;
+    }
+
+    public int getBorrowPeriodMax() {
+        return borrowPeriodMax;
+    }
+
+    public void setBorrowPeriodMax(int borrowPeriodMax) {
+        this.borrowPeriodMax = borrowPeriodMax;
+    }
+
+    public int getBorrowAprMin() {
+        return borrowAprMin;
+    }
+
+    public void setBorrowAprMin(int borrowAprMin) {
+        this.borrowAprMin = borrowAprMin;
+    }
+
+    public int getBorrowAprMax() {
+        return borrowAprMax;
+    }
+
+    public void setBorrowAprMax(int borrowAprMax) {
+        this.borrowAprMax = borrowAprMax;
+    }
+
+    public String getDiscountSort() {
+        return discountSort;
+    }
+
+    public void setDiscountSort(String discountSort) {
+        this.discountSort = discountSort;
+    }
+
+    public String getTermSort() {
+        return termSort;
+    }
+
+    public void setTermSort(String termSort) {
+        this.termSort = termSort;
+    }
+
+    public String getCapitalSort() {
+        return capitalSort;
+    }
+
+    public void setCapitalSort(String capitalSort) {
+        this.capitalSort = capitalSort;
+    }
+
+    public String getInProgressSort() {
+        return inProgressSort;
+    }
+
+    public void setInProgressSort(String inProgressSort) {
+        this.inProgressSort = inProgressSort;
+    }
+}
