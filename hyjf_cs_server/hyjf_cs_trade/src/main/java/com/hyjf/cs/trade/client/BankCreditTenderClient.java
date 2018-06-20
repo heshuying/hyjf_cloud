70fa523ee73207206fe6d56bcdb520a9817c5de5
jun <jijunjun@hyjf.com>
2018-06-20 15:38:29 +0800

    修正错误引用

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/BankCreditTenderClient.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/BankCreditTenderClient.java
@@ -3,6 +3,7 @@ package com.hyjf.cs.trade.client;
 import com.hyjf.am.vo.trade.BankCallBeanVO;
 import com.hyjf.am.vo.trade.CreditTenderLogVO;
 import com.hyjf.am.vo.trade.CreditTenderVO;
+import com.hyjf.am.vo.user.BankOpenAccountVO;
 
 import java.util.List;
 
@@ -18,4 +19,7 @@ public interface BankCreditTenderClient {
 
     List<CreditTenderVO> selectCreditTender(String assignNid);
 
+    BankOpenAccountVO getBankOpenAccount(Integer userId);
+
+    Boolean updateCreditTenderLog(CreditTenderLogVO creditTenderLog);
 }
