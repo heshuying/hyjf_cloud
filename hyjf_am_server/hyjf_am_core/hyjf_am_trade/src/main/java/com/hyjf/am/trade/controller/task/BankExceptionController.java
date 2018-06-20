aec471cec3ceb35f03a6a9c492382f04324c44ef
jun <jijunjun@hyjf.com>
2018-06-20 09:09:08 +0800

    代码重构

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/task/BankExceptionController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/task/BankExceptionController.java
@@ -1,12 +1,22 @@
 package com.hyjf.am.trade.controller.task;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hyjf.am.response.trade.AccountwithdrawResponse;
+import com.hyjf.am.trade.dao.model.auto.Accountwithdraw;
 import com.hyjf.am.trade.service.BankRechargeService;
+import com.hyjf.am.trade.service.BankWithdrawService;
+import com.hyjf.am.vo.trade.account.AccountVO;
+import com.hyjf.am.vo.trade.account.AccountWithdrawVO;
+import com.hyjf.common.util.CommonUtils;
 
 import io.swagger.annotations.Api;
 
@@ -19,11 +29,41 @@ public class BankExceptionController {
 
     @Autowired
     private BankRechargeService bankRechargeExceptionService;
+    @Autowired
+    private BankWithdrawService bankWithdrawService;
 
     @RequestMapping("/recharge")
     public void recharge(){
-        logger.info("开始江西银行充值掉单异常处理...");
+        logger.info("recharge...");
         bankRechargeExceptionService.recharge();
     }
+    
+    @RequestMapping(value = "/selectBankWithdrawList")
+    public AccountwithdrawResponse selectBankWithdrawList(){
+        logger.info("selectBankWithdrawList...");
+        AccountwithdrawResponse response = new AccountwithdrawResponse();
+        List<Accountwithdraw> withdrawList=bankWithdrawService.selectBankWithdrawList();
+        if (CollectionUtils.isNotEmpty(withdrawList)){
+            List<AccountWithdrawVO> voList = CommonUtils.convertBeanList(withdrawList, AccountWithdrawVO.class);
+            response.setResultList(voList);
+        }
+        return response;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    @RequestMapping(value = "/updateBankWithdraw")
+    public boolean updateBankWithdraw(@RequestBody AccountVO accountVO){
+    	logger.info("updateBankWithdraw...");
+    	int count = bankWithdrawService.updateBankWithdraw(accountVO);
+    	if(count>0) {
+    		return true;
+    	}else {
+    		return false;
+    	}
+    }
+    
 
 }
