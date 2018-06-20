06510f3870309cba70f072de124691b2f1088fd4
dongzeshan <dongzeshan@hyjf.com>
2018-06-20 18:11:55 +0800

    admin提交功能

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_config/src/main/java/com/hyjf/am/config/service/AdminSystemService.java
@@ -0,0 +1,21 @@
+package com.hyjf.am.config.service;
+
+import java.util.List;
+
+import com.hyjf.am.config.dao.model.customize.AdminSystem;
+import com.hyjf.am.config.dao.model.customize.Tree;
+
+
+
+public interface AdminSystemService {
+
+	public List<AdminSystem> selectLeftMenuTree(AdminSystem adminSystem);
+
+//	public List<Integer> selectAdminGroup(AdminSystem adminSystem);
+
+	public List<AdminSystem> getUserPermission(AdminSystem adminSystem);
+
+	public AdminSystem getUserInfo(AdminSystem adminSystem);
+	public List<Tree> selectLeftMenuTree2(String id);
+
+}
