a0bd448c91bbc2f2d67414b110606a2bd5f1dc6f
nixiaoling <nixiaoling@hyjf.com>
2018-06-22 09:26:39 +0800

    response和Vo提交

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/user/CertificateAuthorityResponse.java
@@ -0,0 +1,17 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.response.user;
+
+import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.user.CertificateAuthorityVO;
+import com.hyjf.am.vo.user.UserBankOpenAccountVO;
+
+/**
+ * @author nxl
+ * @version UserBankOpenAccountResponse, v0.1 2018/6/21 21:53
+ * 用户CA认证记录
+ */
+public class CertificateAuthorityResponse extends Response<CertificateAuthorityVO>{
+
+}
