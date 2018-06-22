d19d28951c624d117b4cc71f77a82e8dd2d84175
xiashuqing <xiashuqing@shizitegong.com>
2018-06-22 14:00:46 +0800

    生成实体

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_config/src/main/java/com/hyjf/am/config/dao/model/auto/BankInterface.java
@@ -0,0 +1,128 @@
+package com.hyjf.am.config.dao.model.auto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class BankInterface implements Serializable {
+    private Integer id;
+
+    private String interfaceType;
+
+    private String interfaceName;
+
+    private Integer interfaceStatus;
+
+    private Date createTime;
+
+    private Integer createUserId;
+
+    private String createUserName;
+
+    private Date updateTime;
+
+    private Integer updateUserId;
+
+    private String updateUserName;
+
+    private Integer isUsable;
+
+    private Integer isDelete;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getInterfaceType() {
+        return interfaceType;
+    }
+
+    public void setInterfaceType(String interfaceType) {
+        this.interfaceType = interfaceType == null ? null : interfaceType.trim();
+    }
+
+    public String getInterfaceName() {
+        return interfaceName;
+    }
+
+    public void setInterfaceName(String interfaceName) {
+        this.interfaceName = interfaceName == null ? null : interfaceName.trim();
+    }
+
+    public Integer getInterfaceStatus() {
+        return interfaceStatus;
+    }
+
+    public void setInterfaceStatus(Integer interfaceStatus) {
+        this.interfaceStatus = interfaceStatus;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(Integer createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName == null ? null : createUserName.trim();
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public void setUpdateUserId(Integer updateUserId) {
+        this.updateUserId = updateUserId;
+    }
+
+    public String getUpdateUserName() {
+        return updateUserName;
+    }
+
+    public void setUpdateUserName(String updateUserName) {
+        this.updateUserName = updateUserName == null ? null : updateUserName.trim();
+    }
+
+    public Integer getIsUsable() {
+        return isUsable;
+    }
+
+    public void setIsUsable(Integer isUsable) {
+        this.isUsable = isUsable;
+    }
+
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
+}
