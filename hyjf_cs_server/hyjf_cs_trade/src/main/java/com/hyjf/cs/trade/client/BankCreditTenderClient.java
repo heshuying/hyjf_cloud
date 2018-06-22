247c82b3bc77ffbfbeadf89aac2f10848205481d
jun <jijunjun@hyjf.com>
2018-06-22 15:28:48 +0800

    银行债转

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/BankCreditTenderClient.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/BankCreditTenderClient.java
@@ -1,11 +1,13 @@
 package com.hyjf.cs.trade.client;
 
-import com.hyjf.am.vo.trade.BankCallBeanVO;
+import com.hyjf.am.vo.trade.BorrowCreditVO;
 import com.hyjf.am.vo.trade.CreditTenderLogVO;
 import com.hyjf.am.vo.trade.CreditTenderVO;
 import com.hyjf.am.vo.trade.account.AccountVO;
-import com.hyjf.am.vo.trade.borrow.BorrowWithBLOBsVO;
 import com.hyjf.am.vo.user.BankOpenAccountVO;
+import com.hyjf.am.vo.user.UserInfoCustomizeVO;
+import com.hyjf.am.vo.user.UserInfoVO;
+import com.hyjf.am.vo.user.UserVO;
 
 import java.util.List;
 
@@ -27,11 +29,16 @@ public interface BankCreditTenderClient {
 
     CreditTenderLogVO selectCreditTenderLogByOrderId(String logOrderId);
 
-    List<CreditTenderLogVO> selectByOrderIdAndUserId(String assignOrderId, Integer userId);
-
-    boolean deleteByOrderIdAndUserId(String assignOrderId, Integer userId);
-
     AccountVO getAccount(int sellerUserId);
 
-    BorrowWithBLOBsVO getBorrowByNid(String borrowNid);
+    void handle();
+
+    CreditTenderVO selectByAssignNidAndUserId(String logOrderId, Integer userId);
+
+    boolean updateTenderCreditInfo(String logOrderId, Integer userId, String authCode,BankOpenAccountVO sellerBankAccount,
+                                   BankOpenAccountVO assignBankAccount, UserVO webUser, UserInfoVO usersInfo,UserInfoCustomizeVO userInfoCustomizeRefCj);
+
+	List<CreditTenderLogVO> getCreditTenderLogs(String logOrderId, Integer userId);
+
+	List<BorrowCreditVO> getBorrowCreditList(String creditNid, int sellerUserId, String tenderOrderId);
 }
