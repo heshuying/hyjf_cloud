2758deef1d6fa3273b09d362091f7ed41042e1da
jun <jijunjun@hyjf.com>
2018-06-23 15:08:23 +0800

    债转掉单

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/trade/BorrowCreditResponse.java
@@ -0,0 +1,12 @@
+package com.hyjf.am.response.trade;
+
+import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.trade.BorrowCreditVO;
+
+/**
+ * @author jun
+ * @since 20180622
+ */
+public class BorrowCreditResponse extends Response<BorrowCreditVO>{
+
+}
