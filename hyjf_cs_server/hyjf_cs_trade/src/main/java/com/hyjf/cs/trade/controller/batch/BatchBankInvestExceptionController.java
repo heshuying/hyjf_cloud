1ac6b49f50de404b6e2098fde95640b3a08310d9
jun <jijunjun@hyjf.com>
2018-06-23 15:24:32 +0800

    债转投资异常

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/batch/BatchBankInvestExceptionController.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/batch/BatchBankInvestExceptionController.java
@@ -1,46 +1,40 @@
 package com.hyjf.cs.trade.controller.batch;
 
-import com.hyjf.cs.common.bean.result.ApiResult;
-import com.hyjf.cs.trade.constants.BankWithdrawError;
-import com.hyjf.cs.trade.service.BankCreditTenderService;
-import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hyjf.cs.trade.service.BatchBankInvestService;
+
+import io.swagger.annotations.ApiOperation;
+
 /**
- * 债转投资异常处理组合层controller
+ * 投资调单异常处理controller
+ * 
  * @author jun
- * @since 20180619
+ * @since 20180623
  */
 @Controller
 @RequestMapping(value = "/cs-trade/investexception")
 public class BatchBankInvestExceptionController {
 
 	Logger logger = LoggerFactory.getLogger(BatchBankInvestExceptionController.class);
-	
+
 	@Autowired
-	private BankCreditTenderService bankCreditTenderExceptionService;
+	private BatchBankInvestService batchBankInvestService;
+
 	/**
 	 * 处理债转投资异常
+	 * 
 	 * @return
 	 */
-	  @ApiOperation(value = "债转投资异常处理", notes = "债转投资异常处理")
-	    @RequestMapping(value = "/handle")
-	    public ApiResult<Object> handle(){
-	        logger.info("债转投资异常处理start...");
-	    	ApiResult<Object> result = new ApiResult<Object>();
-	        result.setStatus(ApiResult.SUCCESS);
-	        result.setStatusDesc(BankWithdrawError.BANK_WITHDRAW_EXCEPTION_HANDLE_SUCCESS.getMsg());
-	        
-	    	try {
-	    		bankCreditTenderExceptionService.handle();
-	        }catch (Exception e){
-	            result.setStatus(ApiResult.FAIL);
-	            result.setStatusDesc(BankWithdrawError.BANK_WITHDRAW_EXCEPTION_HANDLE_ERROR.getMsg());
-	        }
-	    	return result;
-	    }
+	@ApiOperation(value = "投资调单异常处理", notes = "投资调单异常处理")
+	@RequestMapping(value = "/handle")
+	public void handle() {
+		logger.info("投资调单异常处理start...");
+		batchBankInvestService.handle();
+		logger.info("投资调单异常处理end...");
+	}
 }
