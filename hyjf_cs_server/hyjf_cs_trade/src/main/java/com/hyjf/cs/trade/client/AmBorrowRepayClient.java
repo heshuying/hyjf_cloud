c8feb50a46ee707101b9c5423217d37944b68d11
PC-LIUSHOUYI <liushouyi@hyjf.com>
2018-06-23 14:48:53 +0800

    还款短信提醒

---
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/AmBorrowRepayClient.java
@@ -0,0 +1,33 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.cs.trade.client;
+
+import com.hyjf.am.trade.dao.model.auto.BorrowRepay;
+import com.hyjf.am.vo.trade.borrow.BorrowRepayVO;
+
+import java.util.List;
+
+/**
+ * @author PC-LIUSHOUYI
+ * @version AmBorrowRepayClient, v0.1 2018/6/22 18:43
+ */
+public interface AmBorrowRepayClient {
+
+    /**
+     * 检索borrowRecover数据
+     *
+     * @param borrowNid
+     * @param repaySmsReminder
+     * @return
+     */
+    List<BorrowRepayVO> selectBorrowRepayList(String borrowNid, Integer repaySmsReminder);
+
+    /**
+     * 更新borrowRepay
+     *
+     * @param borrowRepay
+     * @return
+     */
+    Integer updateBorrowRepay(BorrowRepay borrowRepay);
+}
