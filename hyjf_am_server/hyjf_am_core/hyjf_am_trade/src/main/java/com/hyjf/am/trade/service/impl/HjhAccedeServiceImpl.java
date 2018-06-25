731a75cdcea70787320d17d9c302b6767a442eea
PC-LIUSHOUYI <liushouyi@hyjf.com>
2018-06-25 10:39:41 +0800

    对计划退出任务进行分派

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/impl/HjhAccedeServiceImpl.java
@@ -0,0 +1,43 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.trade.service.impl;
+
+import com.hyjf.am.trade.dao.mapper.auto.HjhAccedeMapper;
+import com.hyjf.am.trade.dao.model.auto.HjhAccede;
+import com.hyjf.am.trade.dao.model.auto.HjhAccedeExample;
+import com.hyjf.am.trade.service.HjhAccedeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author PC-LIUSHOUYI
+ * @version HjhAccedeServiceImpl, v0.1 2018/6/25 10:24
+ */
+@Service
+public class HjhAccedeServiceImpl implements HjhAccedeService {
+
+    @Autowired
+    HjhAccedeMapper hjhAccedeMapper;
+
+    /**
+     * 查询退出中和待进入锁定期的标的
+     * @return
+     */
+    @Override
+    public List<HjhAccede> selectWaitQuitHjhList() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);//(退出中)准备退出计划
+        list.add(2);//(自动投标成功)准备进入锁定期
+        HjhAccedeExample accedeExample = new HjhAccedeExample();
+        accedeExample.createCriteria().andOrderStatusIn(list);
+        List<HjhAccede> accedeList = this.hjhAccedeMapper.selectByExample(accedeExample);
+        if (accedeList != null && accedeList.size() > 0) {
+            return accedeList;
+        }
+        return null;
+    }
+}
