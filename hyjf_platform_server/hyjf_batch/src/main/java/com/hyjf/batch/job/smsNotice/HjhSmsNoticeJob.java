0f3b0952dbc52dc8d910f2b133f1e14df2a5173b
jun <jijunjun@hyjf.com>
2018-06-26 11:24:22 +0800

    逾期還款短信提醒

---
+++ hyjf_platform_server/hyjf_batch/src/main/java/com/hyjf/batch/job/smsNotice/HjhSmsNoticeJob.java
@@ -0,0 +1,24 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.batch.job.smsNotice;
+
+import com.hyjf.batch.job.BaseJob;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * 逾期還款短信提醒
+ * @author jijun
+ * @version HjhSmsNoticeTask, v0.1 2018/6/26 9:21
+ */
+public class HjhSmsNoticeJob extends BaseJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        logger.info("HjhSmsNoticeTask: {} execute...", context.getJobDetail().getKey().getName());
+        restTemplate.getForEntity("http://CS-TRADE/cs-trade/hjhSmsNotice/overdueSmsNotice", String.class);
+        logger.info("HjhSmsNoticeTask: {} execute...", context.getJobDetail().getKey().getName());
+    }
+}
