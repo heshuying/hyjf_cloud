0c208bd6cb792e32fa376d899983be5849f3a4d0
pangchengchao <pangchengchao@hyjf.com>
2018-06-26 10:48:54 +0800

    web端资产管理列表

--- hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/trade/AssetManageResponse.java
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/trade/AssetManageResponse.java
@@ -5,7 +5,9 @@ import com.hyjf.am.vo.BaseVO;
 
 import com.hyjf.am.vo.trade.TenderCreditDetailCustomizeVO;
 import com.hyjf.am.vo.trade.assetmanage.CurrentHoldObligatoryRightListCustomizeVO;
+import com.hyjf.am.vo.trade.assetmanage.CurrentHoldPlanListCustomizeVO;
 import com.hyjf.am.vo.trade.assetmanage.RepayMentListCustomizeVO;
+import com.hyjf.am.vo.trade.assetmanage.RepayMentPlanListCustomizeVO;
 
 import java.util.List;
 
@@ -29,6 +31,16 @@ public class AssetManageResponse extends Response<BaseVO> {
     private Integer repayMentCount=0;
     //转让债权数量
     private Integer tenderCreditDetailCount=0;
+
+
+    //当前持有计划列表
+    private List<CurrentHoldPlanListCustomizeVO> currentHoldPlanList;
+    //已回款计划列表
+    private List<RepayMentPlanListCustomizeVO> repayMentPlanList;
+    //当前持有计划数量
+    private Integer currentHoldPlanCount=0;
+    //已回款计划数量
+    private Integer repayMentPlanCount=0;
 
     public List<CurrentHoldObligatoryRightListCustomizeVO> getCurrentHoldObligatoryRightList() {
         return currentHoldObligatoryRightList;
@@ -76,5 +88,37 @@ public class AssetManageResponse extends Response<BaseVO> {
 
     public void setTenderCreditDetailCount(Integer tenderCreditDetailCount) {
         this.tenderCreditDetailCount = tenderCreditDetailCount;
+    }
+
+    public List<CurrentHoldPlanListCustomizeVO> getCurrentHoldPlanList() {
+        return currentHoldPlanList;
+    }
+
+    public void setCurrentHoldPlanList(List<CurrentHoldPlanListCustomizeVO> currentHoldPlanList) {
+        this.currentHoldPlanList = currentHoldPlanList;
+    }
+
+    public List<RepayMentPlanListCustomizeVO> getRepayMentPlanList() {
+        return repayMentPlanList;
+    }
+
+    public void setRepayMentPlanList(List<RepayMentPlanListCustomizeVO> repayMentPlanList) {
+        this.repayMentPlanList = repayMentPlanList;
+    }
+
+    public Integer getCurrentHoldPlanCount() {
+        return currentHoldPlanCount;
+    }
+
+    public void setCurrentHoldPlanCount(Integer currentHoldPlanCount) {
+        this.currentHoldPlanCount = currentHoldPlanCount;
+    }
+
+    public Integer getRepayMentPlanCount() {
+        return repayMentPlanCount;
+    }
+
+    public void setRepayMentPlanCount(Integer repayMentPlanCount) {
+        this.repayMentPlanCount = repayMentPlanCount;
     }
 }
