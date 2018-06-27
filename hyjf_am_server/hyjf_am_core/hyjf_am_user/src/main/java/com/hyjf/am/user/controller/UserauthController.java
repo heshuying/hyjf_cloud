03e5c320fc49e0596913383a8254d4b083d4cc44
dongzeshan <dongzeshan@hyjf.com>
2018-06-27 16:31:08 +0800

    代码提交

--- hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/controller/UserauthController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/controller/UserauthController.java
@@ -12,6 +12,8 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,53 +34,100 @@ import com.hyjf.common.paginator.Paginator;
 @RestController
 @RequestMapping("/am-user/userauth")
 public class UserauthController {
+	@Autowired
 	private UserauthService userauthService;
+
 	private static Logger logger = LoggerFactory.getLogger(UserauthController.class);
-	//查询授权明细
+
+	// 查询授权明细
 	@PostMapping("/userauthlist")
-	public AdminUserAuthListResponse userauthlist(@RequestBody @Valid AdminUserAuthListRequest adminUserAuthListRequest) {
-		
-		
-		 Map<String, Object> authUser = this.buildQueryCondition(adminUserAuthListRequest);
-	        int recordTotal = this.userauthService.countRecordTotal(authUser);
-	        if (recordTotal > 0) {
-	            Paginator paginator = new Paginator(adminUserAuthListRequest.getPaginatorPage(), recordTotal);
-	            List<AdminUserAuthListCustomize> recordList = this.userauthService.getRecordList(authUser,
-	                    paginator.getOffset(), paginator.getLimit());
-	            AdminUserAuthListResponse aualr=new AdminUserAuthListResponse();
-	            List<AdminUserAuthListVO> avo=null;
-				if (recordList != null) {
-					BeanUtils.copyProperties(recordList, avo);
-					aualr.setResultList(avo);
-					aualr.setRecordTotal(recordTotal);
-					aualr.setRtn(Response.SUCCESS);
-				}
-				return aualr;
-	        }
+	public AdminUserAuthListResponse userauthlist(
+			@RequestBody @Valid AdminUserAuthListRequest adminUserAuthListRequest) {
+
+		Map<String, Object> authUser = this.buildQueryCondition(adminUserAuthListRequest);
+		int recordTotal = this.userauthService.countRecordTotal(authUser);
+		if (recordTotal > 0) {
+			Paginator paginator = new Paginator(adminUserAuthListRequest.getPaginatorPage(), recordTotal);
+			List<AdminUserAuthListCustomize> recordList = this.userauthService.getRecordList(authUser,
+					paginator.getOffset(), paginator.getLimit());
+			AdminUserAuthListResponse aualr = new AdminUserAuthListResponse();
+			List<AdminUserAuthListVO> avo = null;
+			if (recordList != null) {
+				BeanUtils.copyProperties(recordList, avo);
+				aualr.setResultList(avo);
+				aualr.setRecordTotal(recordTotal);
+				aualr.setRtn(Response.SUCCESS);
+			}
+			return aualr;
+		}
 		return null;
-		
+
 	}
-	   /**
-     * 构建查询条件
-     *
-     * @param form
-     * @return
-     */
-    private Map<String, Object> buildQueryCondition(AdminUserAuthListRequest form) {
-        // 封装查询条件
-        Map<String, Object> authUser = new HashMap<String, Object>();
-        authUser.put("userName", form.getUserName());
-        authUser.put("recommendName", form.getRecommendName());
-        // 投资授权状态
-        authUser.put("autoInvesStatus", form.getAutoInvesStatus());
-        // 债转授权状态
-        authUser.put("autoCreditStatus", form.getAutoCreditStatus());
-        // 授权时间
-        authUser.put("invesAddTimeStart", form.getInvesAddTimeStart());
-        authUser.put("invesAddTimeEnd", form.getInvesAddTimeEnd());
-        // 签约到期日
-        authUser.put("invesEndTimeStart", form.getInvestEndTimeStart());
-        authUser.put("invesEndTimeEnd", form.getInvestEndTimeEnd());
-        return authUser;
-    }
+
+	/**
+	 * 构建查询条件
+	 *
+	 * @param form
+	 * @return
+	 */
+	private Map<String, Object> buildQueryCondition(AdminUserAuthListRequest form) {
+		// 封装查询条件
+		Map<String, Object> authUser = new HashMap<String, Object>();
+		authUser.put("userName", form.getUserName());
+		authUser.put("recommendName", form.getRecommendName());
+		// 投资授权状态
+		authUser.put("autoInvesStatus", form.getAutoInvesStatus());
+		// 债转授权状态
+		authUser.put("autoCreditStatus", form.getAutoCreditStatus());
+		// 授权时间
+		authUser.put("invesAddTimeStart", form.getInvesAddTimeStart());
+		authUser.put("invesAddTimeEnd", form.getInvesAddTimeEnd());
+		// 签约到期日
+		authUser.put("invesEndTimeStart", form.getInvestEndTimeStart());
+		authUser.put("invesEndTimeEnd", form.getInvestEndTimeEnd());
+		return authUser;
+	}
+
+	/**
+	 * 自动投资解约
+	 *
+	 * @param userId,解约id
+	 */
+
+	@RequestMapping("/userinvescancel")
+	public AdminUserAuthListResponse cancelInvestAuth(@PathVariable int userId, @PathVariable String ordId) {
+		// 返回结果
+		AdminUserAuthListResponse result = new AdminUserAuthListResponse();
+		logger.info("自动投资解约开始，用户：{}", userId);
+		// 关闭授权操作
+		userauthService.updateCancelInvestAuth(userId);
+		String authType = "7";
+		// 在auth_log表中插入解约记录
+		userauthService.insertUserAuthLog2(userId, ordId, authType);
+		result.setMessage("自动投资解约成功！");
+		result.setRtn(AdminUserAuthListResponse.SUCCESS);
+
+		return result;
+	}
+
+	/**
+	 * 自动债转解约
+	 *
+	 * @param userId,解约id
+	 */
+	@RequestMapping("/usercreditcancel")
+	public AdminUserAuthListResponse cancelCreditAuth(@PathVariable int userId, @PathVariable String ordId) {
+		// 返回结果
+		AdminUserAuthListResponse result = new AdminUserAuthListResponse();
+
+		String authType = "8";
+		// 关闭授权操作
+		userauthService.updateCancelCreditAuth(userId);
+		// 在auth_log表中插入解约记录
+		userauthService.insertUserAuthLog2(userId, ordId, authType);
+		result.setMessage("自动投资解约成功！");
+		result.setRtn(AdminUserAuthListResponse.SUCCESS);
+		return result;
+	}
+
 }
---
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/UserauthClient.java
@@ -0,0 +1,22 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.admin.client;
+
+
+import com.hyjf.am.response.trade.HjhAccedeResponse;
+import com.hyjf.am.response.user.AdminUserAuthListResponse;
+import com.hyjf.am.resquest.user.AdminUserAuthListRequest;
+
+/**
+ * @author DongZeShan
+ * @version UserauthService.java, v0.1 2018年6月27日 下午2:19:30
+ */
+public interface UserauthClient {
+	public AdminUserAuthListResponse userauthlist(AdminUserAuthListRequest adminUserAuthListRequest);
+	public AdminUserAuthListResponse cancelInvestAuth(int userId, String ordId);
+	public AdminUserAuthListResponse cancelCreditAuth( int userId,  String ordId);
+	 //@GetMapping("/canCancelAuth/{userId}")
+	public HjhAccedeResponse selectByAssignNidAndUserId(Integer userId);
+
+}
