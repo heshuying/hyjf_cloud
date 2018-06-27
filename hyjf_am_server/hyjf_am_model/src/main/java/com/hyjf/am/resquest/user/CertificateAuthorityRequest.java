b5e6af9661cf23eb020355efed07d85ea2ff50b0
jun <jijunjun@hyjf.com>
2018-06-27 14:51:48 +0800

    用户CA认证

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/resquest/user/CertificateAuthorityRequest.java
@@ -0,0 +1,12 @@
+package com.hyjf.am.resquest.user;
+
+import com.hyjf.am.resquest.Request;
+
+/**
+ * 用户CA认证
+ * @author jun
+ * @date 20180627
+ */
+public class CertificateAuthorityRequest extends Request {
+	
+}
