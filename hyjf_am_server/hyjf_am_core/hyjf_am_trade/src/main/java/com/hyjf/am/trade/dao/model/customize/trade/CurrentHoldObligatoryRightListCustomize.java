57e1f075bb2772f27769b824444b623ae309e1f2
pangchengchao <pangchengchao@hyjf.com>
2018-06-28 14:44:59 +0800

    修改数据库字段addtime为create_time

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/dao/model/customize/trade/CurrentHoldObligatoryRightListCustomize.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/dao/model/customize/trade/CurrentHoldObligatoryRightListCustomize.java
@@ -30,7 +30,7 @@ public class CurrentHoldObligatoryRightListCustomize implements Serializable {
 	/**投资订单号*/
 	private String nid;
 	/**投资时间*/
-	private String addtime;
+	private String createTime;
 	/**预计还款时间*/
 	private String investDate;
 	/**待还本金*/
@@ -92,12 +92,15 @@ public class CurrentHoldObligatoryRightListCustomize implements Serializable {
     public void setNid(String nid) {
         this.nid = nid;
     }
-    public String getAddtime() {
-        return addtime;
+
+    public String getCreateTime() {
+        return createTime;
     }
-    public void setAddtime(String addtime) {
-        this.addtime = addtime;
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
     }
+
     public String getInvestDate() {
         if("投资中".equals(this.data)){
             return "--";
