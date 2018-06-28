17069597f3ce0721c532427733a50dce1f6c86d7
xiashuqing <xiashuqing@shizitegong.com>
2018-06-28 11:17:18 +0800

    增加分布式事务推送表

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/mq/transactionmq/AccountTProducer.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/mq/transactionmq/AccountTProducer.java
@@ -1,7 +1,9 @@
 package com.hyjf.am.trade.mq.transactionmq;
 
+import com.hyjf.am.trade.dao.model.auto.ProducerTransactionMessage;
 import org.apache.rocketmq.client.producer.LocalTransactionState;
 import org.apache.rocketmq.client.producer.TransactionSendResult;
+import org.apache.rocketmq.common.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Component;
 import com.alibaba.fastjson.JSON;
 import com.hyjf.am.trade.controller.demo.TransactionService;
 import com.hyjf.common.exception.MQException;
+
+import java.util.Date;
 
 /**
  * @author xiasq
@@ -46,13 +50,30 @@ public class AccountTProducer extends TransactionProducer {
 				logger.error("事务消息发送失败....", e);
 				return LocalTransactionState.ROLLBACK_MESSAGE;
 			}
-			logger.info("事务消息提交成功...");
+			logger.info("事务消息提交确认, 保存本地消息表...");
+			this.saveProducerTransactionMessage(msg);
 			return LocalTransactionState.COMMIT_MESSAGE;
 		});
+		// 事务消息推送成功，修改状态
+		if (result.getLocalTransactionState() == LocalTransactionState.COMMIT_MESSAGE){
 
-		if (result.getLocalTransactionState() == LocalTransactionState.COMMIT_MESSAGE)
 			return true;
+		}
+
 		return false;
+	}
+
+	private void saveProducerTransactionMessage(Message msg){
+		// tags key
+		ProducerTransactionMessage message = new ProducerTransactionMessage();
+
+		//message.setMsgId(msg.get);
+		message.setMessageStatus(MessageStatus.UNKKOWN.getCode());
+		message.setSendTimes(0);
+		message.setTopic(msg.getTopic());
+		message.setBody(new String(msg.getBody()));
+		message.setCreateTime(new Date());
+		message.setUpdateTime(new Date());
 	}
 
 }
--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/mq/transactionmq/MessageStatus.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/mq/transactionmq/MessageStatus.java
@@ -8,12 +8,9 @@ import java.util.Map;
  * @version MessageStatus, v0.1 2018/6/27 14:54
  */
 public enum MessageStatus {
-	UNCONSUMED(0, "未被消费"),
-	CONSUMED(1, "已被消费"),
-	OVER_CONFIRM_RETRY_TIME(2, "超过确认消息重试次数"),
-	OVER_CONSUME_RETRY_TIME(3, "超过消费消息重试次数"),
-	CONSUME_FAILED(4, "消费失败"),
-	ROLLBACK(5, "已被回滚");
+	COMMIT(0, "已确认"),
+	UNKKOWN(1, "未确认"),
+	ROLLBACK(2, "回滚");
 
 	private int code;
 	private String desc;
