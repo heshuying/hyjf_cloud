c577c997c03a5f128abfef089eef407a9101f996
jun <jijunjun@hyjf.com>
2018-06-30 15:04:57 +0800

    法大大相关

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/BorrowRecoverService.java
@@ -0,0 +1,29 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.trade.service;
+
+import com.hyjf.am.resquest.trade.BorrowRegistRequest;
+import com.hyjf.am.resquest.trade.TenderRequest;
+import com.hyjf.am.resquest.user.BorrowFinmanNewChargeRequest;
+import com.hyjf.am.trade.dao.model.auto.*;
+import com.hyjf.am.vo.trade.ProjectCompanyDetailVO;
+import com.hyjf.am.vo.trade.ProjectCustomeDetailVO;
+import com.hyjf.am.vo.trade.WebProjectPersonDetailVO;
+import com.hyjf.am.vo.trade.borrow.BorrowRecoverVO;
+import com.hyjf.am.vo.trade.borrow.TenderBgVO;
+import com.hyjf.am.vo.trade.borrow.TenderRetMsg;
+
+import java.util.List;
+
+/**
+ * @author jijun
+ * @date 20180630
+ */
+public interface BorrowRecoverService{
+
+
+    BorrowRecover selectBorrowRecoverByTenderNid(String tenderAgreementID);
+
+    int updateBorrowRecover(BorrowRecover borrowRecover);
+}
