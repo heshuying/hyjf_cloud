258ee7e83c327bfb96c4d99892750389bcba7247
wangjun <273407491@qq.com>
2018-07-02 20:19:06 +0800

    汇直投-标的备案

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/admin/BorrowRegistController.java
@@ -0,0 +1,131 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.trade.controller.admin;
+
+import com.hyjf.am.response.admin.BorrowRegistCustomizeResponse;
+import com.hyjf.am.response.trade.BorrowProjectTypeResponse;
+import com.hyjf.am.response.trade.BorrowStyleResponse;
+import com.hyjf.am.response.trade.STZHWhiteListResponse;
+import com.hyjf.am.resquest.admin.BorrowRegistListRequest;
+import com.hyjf.am.resquest.trade.BorrowRegistRequest;
+import com.hyjf.am.trade.dao.model.auto.BorrowProjectType;
+import com.hyjf.am.trade.dao.model.auto.BorrowStyle;
+import com.hyjf.am.trade.dao.model.auto.StzhWhiteList;
+import com.hyjf.am.trade.dao.model.customize.trade.BorrowRegistCustomize;
+import com.hyjf.am.trade.service.admin.BorrowRegistService;
+import com.hyjf.am.vo.admin.BorrowRegistCustomizeVO;
+import com.hyjf.am.vo.trade.STZHWhiteListVO;
+import com.hyjf.am.vo.trade.borrow.BorrowProjectTypeVO;
+import com.hyjf.am.vo.trade.borrow.BorrowStyleVO;
+import com.hyjf.common.util.CommonUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * @author wangjun
+ * @version BorrowRegistController, v0.1 2018/6/29 16:41
+ */
+@RestController
+@RequestMapping("/am-trade/borrow_regist")
+public class BorrowRegistController {
+    @Autowired
+    BorrowRegistService borrowRegistService;
+
+    /**
+     * 获取项目类型
+     * @return
+     */
+    @RequestMapping("/select_borrow_project")
+    public BorrowProjectTypeResponse selectBorrowProjectList() {
+        BorrowProjectTypeResponse response = new BorrowProjectTypeResponse();
+        List<BorrowProjectType> borrowProjectTypeList = borrowRegistService.selectBorrowProjectList();
+        if (!CollectionUtils.isEmpty(borrowProjectTypeList)) {
+            List<BorrowProjectTypeVO> voList = CommonUtils.convertBeanList(borrowProjectTypeList, BorrowProjectTypeVO.class);
+            response.setResultList(voList);
+        }
+        return response;
+    }
+
+    /**
+     * 获取还款方式
+     * @return
+     */
+    @RequestMapping("/select_borrow_style")
+    public BorrowStyleResponse selectBorrowStyleList() {
+        BorrowStyleResponse response = new BorrowStyleResponse();
+        List<BorrowStyle> borrowStyleList = borrowRegistService.selectBorrowStyleList();
+        if (!CollectionUtils.isEmpty(borrowStyleList)) {
+            List<BorrowStyleVO> voList = CommonUtils.convertBeanList(borrowStyleList, BorrowStyleVO.class);
+            response.setResultList(voList);
+        }
+        return response;
+    }
+
+    /**
+     * 获取标的列表count
+     * @return
+     */
+    @RequestMapping("/get_regist_count")
+    public Integer getRegistCount(@RequestBody @Valid BorrowRegistListRequest borrowRegistListRequest) {
+        return borrowRegistService.getRegistCount(borrowRegistListRequest);
+    }
+
+    /**
+     * 获取标的列表
+     * @return
+     */
+    @RequestMapping("/select_borrow_regist_list")
+    public BorrowRegistCustomizeResponse selectBorrowRegistList(@RequestBody @Valid BorrowRegistListRequest borrowRegistListRequest) {
+        BorrowRegistCustomizeResponse response = new BorrowRegistCustomizeResponse();
+        List<BorrowRegistCustomize> borrowRegistCustomizeList = borrowRegistService.selectBorrowRegistList(borrowRegistListRequest);
+        if (!CollectionUtils.isEmpty(borrowRegistCustomizeList)) {
+            List<BorrowRegistCustomizeVO> voList = CommonUtils.convertBeanList(borrowRegistCustomizeList, BorrowRegistCustomizeVO.class);
+            response.setResultList(voList);
+        }
+        return response;
+    }
+
+    /**
+     * 获取受托支付电子账户列表
+     * @param instCode
+     * @param entrustedAccountId
+     * @return
+     */
+    @GetMapping("/select_stzf_white_list/{instCode}/{entrustedAccountId}")
+    public STZHWhiteListResponse selectStzfWhiteList(@PathVariable String instCode, @PathVariable String entrustedAccountId) {
+        STZHWhiteListResponse response = new STZHWhiteListResponse();
+        StzhWhiteList stzhWhiteList =  borrowRegistService.selectStzfWhiteList(instCode, entrustedAccountId);
+        if (stzhWhiteList != null) {
+            STZHWhiteListVO stzhWhiteListVO = new STZHWhiteListVO();
+            BeanUtils.copyProperties(stzhWhiteList, stzhWhiteListVO);
+            response.setResult(stzhWhiteListVO);
+        }
+        return response;
+    }
+
+    /**
+     * 更新标的信息
+     * @param request
+     * @return
+     */
+    @RequestMapping("/update_borrow_regist")
+    public int updateBorrowRegistFromList(@RequestBody BorrowRegistRequest request) {
+        return borrowRegistService.updateBorrowRegistFromList(request);
+    }
+
+    /**
+     * 更新标的信息(受托支付备案)
+     * @param request
+     * @return
+     */
+    @RequestMapping("/update_entrusted_borrow_regist")
+    public int updateEntrustedBorrowRegist(@RequestBody BorrowRegistRequest request) {
+        return borrowRegistService.updateEntrustedBorrowRegist(request);
+    }
+}
---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/admin/BorrowRegistCustomizeResponse.java
@@ -0,0 +1,14 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.response.admin;
+
+import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.admin.BorrowRegistCustomizeVO;
+
+/**
+ * @author wangjun
+ * @version BorrowRegistCustomizeResponse, v0.1 2018/6/29 18:44
+ */
+public class BorrowRegistCustomizeResponse extends Response<BorrowRegistCustomizeVO> {
+}
