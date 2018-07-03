c9b26420e0a3d29d58cc0bea8f1f1050dca32828
yaoyong <yaoyong@shizitegong.com>
2018-07-03 10:55:13 +0800

    vip管理

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/admin/VipManageResponse.java
@@ -0,0 +1,24 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.response.admin;
+
+import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.admin.VipManageVO;
+
+/**
+ * @author yaoyong
+ * @version VipManageResponse, v0.1 2018/7/2 16:47
+ */
+public class VipManageResponse extends Response<VipManageVO> {
+    // 数据查询条数 主要用于分页情况，原子层向组合层返回
+    private  Integer  count;
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
