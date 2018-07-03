488c487b61a0690e293ff479d3a7879dce6e5e80
wangjun <273407491@qq.com>
2018-07-03 10:37:13 +0800

    下拉列表共同化

---
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/service/impl/AdminCommonServiceImpl.java
@@ -0,0 +1,34 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.admin.service.impl;
+
+import com.hyjf.admin.client.AdminCommonClient;
+import com.hyjf.admin.service.AdminCommonService;
+import com.hyjf.am.vo.trade.borrow.BorrowStyleVO;
+import com.hyjf.common.cache.CacheUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author wangjun
+ * @version AdminCommonServiceImpl, v0.1 2018/7/3 10:25
+ */
+@Service
+public class AdminCommonServiceImpl implements AdminCommonService {
+    @Autowired
+    AdminCommonClient adminCommonClient;
+    @Override
+    public Map<String, String> getParamNameMap(String param){
+        Map<String,String> resultMap = CacheUtil.getParamNameMap(param);
+        return resultMap;
+    }
+
+    @Override
+    public List<BorrowStyleVO> selectBorrowStyleList(){
+        return adminCommonClient.selectBorrowStyleList();
+    }
+}
