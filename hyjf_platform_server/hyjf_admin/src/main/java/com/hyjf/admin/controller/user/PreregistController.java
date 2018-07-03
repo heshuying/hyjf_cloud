dd12241c353f5b081108efd91a8d79d8e7e12783
dongzeshan <dongzeshan@hyjf.com>
2018-07-03 14:54:01 +0800

    示例代码

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/user/PreregistController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/user/PreregistController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,13 +19,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.fastjson.JSONObject;
+import com.hyjf.admin.common.result.BaseResult;
+import com.hyjf.admin.common.result.ListResult;
+import com.hyjf.admin.common.util.ShiroConstants;
 import com.hyjf.admin.controller.BaseController;
 import com.hyjf.admin.interceptor.AuthorityAnnotation;
 import com.hyjf.admin.service.PreregistService;
 import com.hyjf.am.response.user.AdminPreRegistListResponse;
 import com.hyjf.am.resquest.user.AdminPreRegistListRequest;
+import com.hyjf.am.vo.user.AdminPreRegistListVO;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 
 /**
@@ -36,7 +42,8 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/preregist")
 public class PreregistController extends BaseController {
 	private static final Logger logger = LoggerFactory.getLogger(PreregistController.class);
-
+	//权限名称
+	private static final String PERMISSIONS = "preregist";
 	@Autowired
 	private PreregistService preregistService;
 
@@ -45,33 +52,38 @@ public class PreregistController extends BaseController {
 	 * @Desc :预注册用户页面载入
 	 * @Param: * @param map
 	 * @Date: 16:43 2018/6/15
-	 * @Return: JSONObject
+	 * @Return: BaseResult<ListResult<AdminPreRegistListVO>>
 	 */
 	@ApiOperation(value = "预注册用户页面载入", notes = "预注册用户页面载入")
 	@PostMapping(value = "/init")
 	@ResponseBody
-	@AuthorityAnnotation(key = "312", value = "123")
-	public JSONObject init(HttpServletRequest request, HttpServletResponse response,
-			@RequestBody Map<String, String> map) {
-		System.out.println(request.getRequestURI());
-		JSONObject info = new JSONObject();
+	@AuthorityAnnotation(key = PERMISSIONS, value = ShiroConstants.PERMISSION_VIEW)
+	public BaseResult<ListResult<AdminPreRegistListVO>> init(HttpServletRequest request,
+			@RequestBody AdminPreRegistListRequest adminPreRegistListRequest) {
+		 BaseResult<ListResult<AdminPreRegistListVO>> rs=new BaseResult<ListResult<AdminPreRegistListVO>>();
 		AdminPreRegistListRequest aprlr = new AdminPreRegistListRequest();
-		aprlr.setMobile(map.get("mobile"));
-		aprlr.setReferrer(map.get("referrer"));
-		aprlr.setSource(map.get("source"));
-		aprlr.setRegistFlag(map.get("registFlag"));
-		aprlr.setPaginatorPage(Integer.valueOf(map.get("paginatorPage")));
+		aprlr.setMobile(adminPreRegistListRequest.getMobile());
+		aprlr.setReferrer(adminPreRegistListRequest.getReferrer());
+		aprlr.setSource(adminPreRegistListRequest.getReferrer());
+		aprlr.setRegistFlag(adminPreRegistListRequest.getRegistFlag());
+		aprlr.setCurrPage(adminPreRegistListRequest.getCurrPage());
+		aprlr.setPageSize(adminPreRegistListRequest.getPageSize());
+		/*
+		 * 也可以直接使用
+		 * BeanUtils.copyProperties(adminPreRegistListRequest, aprlr);
+		 */
 		AdminPreRegistListResponse prs = preregistService.getRecordList(aprlr);
-		if (StringUtils.isNotBlank(prs.getMessage())) {
-			info.put("status", "99");
-			info.put("msg", prs.getMessage());
-			return info;
+		
+		if (prs.getRtn().equals("99")) {
+			rs.setStatus(FAIL);
+			rs.setStatusDesc(prs.getMessage());
+			return rs;
 		}
-		info.put("list", prs.getResultList());
-		info.put("recordTotal", prs.getRecordTotal());
-		info.put("status", "0");
-		info.put("msg", "登录成功");
-		return info;
+		ListResult<AdminPreRegistListVO> lrs=new ListResult<AdminPreRegistListVO>();
+		lrs.setCount(prs.getRecordTotal());
+		lrs.setList(prs.getResultList());
+		rs.setData(lrs);
+		return rs;
 	}
 
 	/**
@@ -81,25 +93,27 @@ public class PreregistController extends BaseController {
 	 * @Date: 16:43 2018/6/15
 	 * @Return: JSONObject
 	 */
+
 	@ApiOperation(value = "编辑页信息", notes = "编辑页信息")
 	@PostMapping(value = "/updatepreregistlist")
 	@ResponseBody
-	public JSONObject getMenuTree(HttpServletRequest request, HttpServletResponse response,
-			@RequestBody Map<String, String> map) {
-		JSONObject info = new JSONObject();
-		// AdminSystemVO user = this.getUser(request);
+	@AuthorityAnnotation(key = PERMISSIONS, value = ShiroConstants.PERMISSION_UPDATE)
+	@ApiImplicitParam(name = "id", value = "主键", required = true, dataType = "String")
+	public BaseResult<AdminPreRegistListVO> updatepreregistlist(HttpServletRequest request, 
+			@RequestBody String id) {
+		BaseResult<AdminPreRegistListVO> result=new BaseResult<AdminPreRegistListVO>();
 		AdminPreRegistListRequest aprlr = new AdminPreRegistListRequest();
-		aprlr.setId(map.get("id"));
+		aprlr.setId(id);
 		AdminPreRegistListResponse prs = preregistService.getPreRegist(aprlr);
-		if (StringUtils.isNotBlank(prs.getMessage())) {
-			info.put("status", "99");
-			info.put("msg", prs.getMessage());
-			return info;
+		if (prs.getRtn().equals("99")) {
+			result.setStatus(FAIL);
+			result.setStatusDesc(prs.getMessage());
+			return result;
 		}
-		info.put("status", "0");
-		info.put("msg", "成功");
-		info.put("preRegist", prs.getResult());
-		return info;
+		result.setData(prs.getResult());
+		result.setStatus(SUCCESS);
+		result.setStatusDesc(SUCCESS_DESC);
+		return result;
 	}
 
 	/**
@@ -112,24 +126,20 @@ public class PreregistController extends BaseController {
 	@ApiOperation(value = "编辑页保存", notes = "编辑页保存")
 	@PostMapping(value = "/savepreregistlist")
 	@ResponseBody
-	public JSONObject getUserPermission(HttpServletRequest request, HttpServletResponse response,
-			@RequestBody Map<String, String> map) {
-		JSONObject info = new JSONObject();
+	public BaseResult getUserPermission(HttpServletRequest request,
+			@RequestBody AdminPreRegistListRequest adminPreRegistListRequest) {
+		BaseResult result=new BaseResult<>();
 		AdminPreRegistListRequest aprlr = new AdminPreRegistListRequest();
-		aprlr.setId(map.get("id"));
-		aprlr.setReferrer(map.get("referrer"));
-		aprlr.setSource(map.get("source"));
-		aprlr.setUtm(map.get("utm"));
-		aprlr.setRemark(map.get("remark"));
+		BeanUtils.copyProperties(adminPreRegistListRequest, aprlr);
 		AdminPreRegistListResponse prs = preregistService.savePreRegist(aprlr);
-		if (StringUtils.isNotBlank(prs.getMessage())) {
-			info.put("status", "99");
-			info.put("msg", prs.getMessage());
-			return info;
+		if (prs.getRtn().equals("99")) {
+			result.setStatus(FAIL);
+			result.setStatusDesc(prs.getMessage());
+			return result;
 		}
-		info.put("status", "0");
-		info.put("msg", "成功");
-		return info;
+		result.setStatus(SUCCESS);
+		result.setStatusDesc(SUCCESS_DESC);
+		return result;
 	}
 
 }
