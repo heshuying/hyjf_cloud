9c99341eb4a1a1f01b885e970d80fa0c7f0b03c9
yaoyong <yaoyong@shizitegong.com>
2018-07-05 09:52:42 +0800

    后台优惠券列表接口

---
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/beans/request/CouponCheckRequestBean.java
@@ -0,0 +1,58 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.admin.beans.request;
+
+import com.hyjf.admin.beans.BaseRequest;
+import com.hyjf.am.vo.config.CouponCheckVO;
+import com.hyjf.common.paginator.Paginator;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author yaoyong
+ * @version CouponCheckRequestBean, v0.1 2018/7/3 16:05
+ */
+public class CouponCheckRequestBean extends BaseRequest implements Serializable {
+    private Integer id;
+
+    private String fileName;
+
+    private Integer status;
+
+    private String remark;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
