f945f94c3062b40aabe44be0c547216891c98956
jun <jijunjun@hyjf.com>
2018-07-06 09:33:50 +0800

    代码优化

--- hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/controller/admin/exception/BorrowRegistExceptionController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/controller/admin/exception/BorrowRegistExceptionController.java
@@ -3,11 +3,8 @@
  */
 package com.hyjf.am.user.controller.admin.exception;
 
-import com.hyjf.am.response.trade.BankCallBeanResponse;
 import com.hyjf.am.response.user.BankOpenAccountResponse;
 import com.hyjf.am.user.service.admin.exception.BorrowRegistExceptionService;
-import com.hyjf.am.vo.trade.BankCallBeanVO;
-import com.hyjf.am.vo.trade.borrow.BorrowRegistExceptionVO;
 import com.hyjf.am.vo.user.BankOpenAccountVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
