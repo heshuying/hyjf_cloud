ef92c1fe2270ecc037ce29cce3efb63cf85802b7
TonyLee <libin@hyjf.com>
2018-07-06 09:17:42 +0800

    后台admin产品中心计划列表框架做成

---
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/PlanListClient.java
@@ -0,0 +1,12 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.admin.client;
+
+/**
+ * @author libin
+ * @version PlanListClient.java, v0.1 2018年7月6日 上午9:11:33
+ */
+public interface PlanListClient {
+
+}
---
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/service/PlanListService.java
@@ -0,0 +1,12 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.admin.service;
+
+/**
+ * @author libin
+ * @version PlanListService.java, v0.1 2018年7月6日 上午9:09:16
+ */
+public interface PlanListService {
+
+}
