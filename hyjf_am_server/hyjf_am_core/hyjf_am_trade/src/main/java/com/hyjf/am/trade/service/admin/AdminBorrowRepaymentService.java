fa863bc08b69282bd18713203c368e096fa895be
pangchengchao <pangchengchao@hyjf.com>
2018-07-07 14:03:19 +0800

    产品中心-汇直投-还款信息

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/admin/AdminBorrowRepaymentService.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/admin/AdminBorrowRepaymentService.java
@@ -2,9 +2,13 @@ package com.hyjf.am.trade.service.admin;
 
 import com.hyjf.am.resquest.admin.BorrowRecoverRequest;
 import com.hyjf.am.resquest.admin.BorrowRepaymentPlanRequest;
+import com.hyjf.am.trade.dao.model.auto.BorrowRepay;
+import com.hyjf.am.trade.dao.model.auto.BorrowRepayPlan;
 import com.hyjf.am.trade.dao.model.customize.admin.AdminBorrowRepaymentCustomize;
 import com.hyjf.am.trade.dao.model.customize.admin.AdminBorrowRepaymentPlanCustomize;
+import com.hyjf.am.trade.dao.model.customize.admin.AdminRepayDelayCustomize;
 
+import java.text.ParseException;
 import java.util.List;
 
 /**
@@ -19,4 +23,12 @@ public interface AdminBorrowRepaymentService {
     AdminBorrowRepaymentCustomize sumBorrowRecoverList(BorrowRecoverRequest request);
 
     List<AdminBorrowRepaymentPlanCustomize> exportRepayClkActBorrowRepaymentInfoList(BorrowRepaymentPlanRequest request);
+
+    AdminRepayDelayCustomize selectBorrowInfo(String borrowNid) throws ParseException;
+
+    BorrowRepay getBorrowRepayDelay(String borrowNid, String borrowApr, String borrowStyle) throws ParseException;
+
+    BorrowRepayPlan getBorrowRepayPlanDelay(String borrowNid, String borrowApr, String borrowStyle) throws ParseException;
+
+    int updateBorrowRepayDelayDays(String borrowNid, String delayDays) throws ParseException;
 }
---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/admin/AdminRepayDelayResponse.java
@@ -0,0 +1,12 @@
+package com.hyjf.am.response.admin;
+
+import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.admin.AdminRepayDelayCustomizeVO;
+import com.hyjf.am.vo.admin.AssetDetailCustomizeVO;
+
+/**
+ * @author pangchengchao
+ * @version AdminRepayDelayResponse, v0.1 2018/7/6 10:25
+ */
+public class AdminRepayDelayResponse extends Response<AdminRepayDelayCustomizeVO> {
+}
