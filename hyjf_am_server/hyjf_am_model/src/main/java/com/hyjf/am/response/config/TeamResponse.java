290d49a85605568811424dac2079dba44ca185c1
fuqiang <18251359382@163.com>
2018-07-09 17:01:24 +0800

    关于我们原子层

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/config/TeamResponse.java
@@ -0,0 +1,14 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.response.config;
+
+import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.config.TeamVO;
+
+/**
+ * @author fuqiang
+ * @version TeamResponse, v0.1 2018/7/9 16:43
+ */
+public class TeamResponse extends Response<TeamVO> {
+}
