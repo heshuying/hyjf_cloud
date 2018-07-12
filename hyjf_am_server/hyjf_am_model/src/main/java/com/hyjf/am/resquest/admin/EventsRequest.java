8f04b04f6e1fc125aa648c1393f5dcdd0afe5d09
fuqiang <18251359382@163.com>
2018-07-12 09:43:14 +0800

    公司管理公司记事

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/resquest/admin/EventsRequest.java
@@ -0,0 +1,167 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.resquest.admin;
+
+import java.util.Date;
+
+import com.hyjf.am.vo.BasePage;
+
+/**
+ * @author fuqiang
+ * @version EventsRequest, v0.1 2018/7/12 9:12
+ */
+public class EventsRequest extends BasePage {
+	private Integer id;
+
+	private String eventTime;
+
+	private String title;
+
+	private String content;
+
+	private Integer eventYear;
+
+	private Integer status;
+
+	private String addAdmin;
+
+	private Integer actTime;
+
+	private String thumb;
+
+	private Integer createUserId;
+
+	private Integer updateUserId;
+
+	private Date createTime;
+
+	private Date updateTime;
+
+	private static final long serialVersionUID = 1L;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEventTime() {
+		return eventTime;
+	}
+
+	public void setEventTime(String eventTime) {
+		this.eventTime = eventTime == null ? null : eventTime.trim();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title == null ? null : title.trim();
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content == null ? null : content.trim();
+	}
+
+	public Integer getEventYear() {
+		return eventYear;
+	}
+
+	public void setEventYear(Integer eventYear) {
+		this.eventYear = eventYear;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getAddAdmin() {
+		return addAdmin;
+	}
+
+	public void setAddAdmin(String addAdmin) {
+		this.addAdmin = addAdmin == null ? null : addAdmin.trim();
+	}
+
+	public Integer getActTime() {
+		return actTime;
+	}
+
+	public void setActTime(Integer actTime) {
+		this.actTime = actTime;
+	}
+
+	public String getThumb() {
+		return thumb;
+	}
+
+	public void setThumb(String thumb) {
+		this.thumb = thumb == null ? null : thumb.trim();
+	}
+
+	public Integer getCreateUserId() {
+		return createUserId;
+	}
+
+	public void setCreateUserId(Integer createUserId) {
+		this.createUserId = createUserId;
+	}
+
+	public Integer getUpdateUserId() {
+		return updateUserId;
+	}
+
+	public void setUpdateUserId(Integer updateUserId) {
+		this.updateUserId = updateUserId;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	/** 开始时间 */
+	private String startTime;
+	/** 结束时间 */
+	private String endTime;
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+}
