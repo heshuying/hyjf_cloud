952f592df8a2c031473538605d4e665e7ea35cd7
tanyangyang <591628420@qq.com>
2018-07-16 17:31:35 +0800

    着落页配置

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/vo/config/LandingPageVo.java
@@ -0,0 +1,95 @@
+package com.hyjf.am.vo.config;
+
+import com.hyjf.am.vo.BaseVO;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author tanyy
+ * @version LandingPageVo, v0.1 2018/7/16 11:38
+ */
+
+public class LandingPageVo extends BaseVO implements Serializable {
+    private Integer id;
+
+    private String pageName;
+
+    private String channelName;
+
+    private String pageUrl;
+
+    private String codeUrl;
+
+    private String remark;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPageName() {
+        return pageName;
+    }
+
+    public void setPageName(String pageName) {
+        this.pageName = pageName == null ? null : pageName.trim();
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName == null ? null : channelName.trim();
+    }
+
+    public String getPageUrl() {
+        return pageUrl;
+    }
+
+    public void setPageUrl(String pageUrl) {
+        this.pageUrl = pageUrl == null ? null : pageUrl.trim();
+    }
+
+    public String getCodeUrl() {
+        return codeUrl;
+    }
+
+    public void setCodeUrl(String codeUrl) {
+        this.codeUrl = codeUrl == null ? null : codeUrl.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
