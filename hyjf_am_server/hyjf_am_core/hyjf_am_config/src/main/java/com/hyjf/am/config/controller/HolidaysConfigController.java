1da5e769a596c56e99bd8b9f465c01f461d932e3
xiashuqing <xiashuqing@shizitegong.com>
2018-07-16 19:17:43 +0800

    提交节假日配置

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_config/src/main/java/com/hyjf/am/config/controller/HolidaysConfigController.java
@@ -0,0 +1,23 @@
+package com.hyjf.am.config.controller;
+
+import com.hyjf.am.response.trade.HolidaysConfigResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiasq
+ * @version HolidaysConfigController, v0.1 2018/7/16 17:13
+ * 节假日配置
+ */
+@RestController
+@RequestMapping("/am-config/holidays")
+public class HolidaysConfigController {
+
+    @RequestMapping("/save")
+    public String save(){
+        HolidaysConfigResponse response = new HolidaysConfigResponse();
+
+
+        return "success";
+    }
+}
