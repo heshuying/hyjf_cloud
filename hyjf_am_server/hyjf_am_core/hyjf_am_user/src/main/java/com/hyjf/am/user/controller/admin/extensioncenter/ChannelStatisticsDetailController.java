755f794a125bcd7bad927338a0e5dfd7e6b61ae0
tanyangyang <591628420@qq.com>
2018-07-17 14:24:07 +0800

    PC統計明細  原子层

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/controller/admin/extensioncenter/ChannelStatisticsDetailController.java
@@ -0,0 +1,38 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.user.controller.admin.extensioncenter;
+
+import com.hyjf.am.config.controller.BaseConfigController;
+import com.hyjf.am.config.service.ChannelStatisticsDetailService;
+import com.hyjf.am.response.admin.ChannelStatisticsDetailResponse;
+import com.hyjf.am.resquest.admin.ChannelStatisticsDetailRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tanyy
+ * @version ChannelStatisticsDetailController, v0.1 2018/7/16 14:32
+ */
+@RestController
+@RequestMapping("/am-config/extensioncenter/channelstatisticsdetail")
+public class ChannelStatisticsDetailController extends BaseConfigController {
+	@Autowired
+	private ChannelStatisticsDetailService channelStatisticsDetailService;
+
+	/**
+	 * 根据条件查询pc统计明细
+	 *
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("/searchaction")
+	public ChannelStatisticsDetailResponse searchAction(@RequestBody ChannelStatisticsDetailRequest request) {
+		logger.info("pc统计明细查询开始......");
+		ChannelStatisticsDetailResponse response = channelStatisticsDetailService.searchAction(request);
+		return response;
+	}
+
+}
