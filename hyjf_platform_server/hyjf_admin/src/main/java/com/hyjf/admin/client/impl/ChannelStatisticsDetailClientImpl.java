a3bd0470092eca61de031c086f55911d2a3a432a
tanyangyang <591628420@qq.com>
2018-07-17 15:16:21 +0800

    PC統計明細  原子层

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/impl/ChannelStatisticsDetailClientImpl.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/impl/ChannelStatisticsDetailClientImpl.java
@@ -1,6 +1,8 @@
 package com.hyjf.admin.client.impl;
 
 import com.hyjf.admin.client.ChannelStatisticsDetailClient;
+import com.hyjf.am.response.user.ChannelStatisticsDetailResponse;
+import com.hyjf.am.resquest.user.ChannelStatisticsDetailRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
