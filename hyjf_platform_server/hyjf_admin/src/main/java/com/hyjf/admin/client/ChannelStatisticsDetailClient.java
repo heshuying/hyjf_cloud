634bd146536aac1dd37eab3b548740236e17d02a
tanyangyang <591628420@qq.com>
2018-07-17 15:16:08 +0800

    PC統計明細  原子层

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/ChannelStatisticsDetailClient.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/ChannelStatisticsDetailClient.java
@@ -1,5 +1,8 @@
 package com.hyjf.admin.client;
 
+import com.hyjf.am.response.user.ChannelStatisticsDetailResponse;
+import com.hyjf.am.resquest.user.ChannelStatisticsDetailRequest;
+
 public interface ChannelStatisticsDetailClient {
 
 	public ChannelStatisticsDetailResponse searchAction(ChannelStatisticsDetailRequest request);
