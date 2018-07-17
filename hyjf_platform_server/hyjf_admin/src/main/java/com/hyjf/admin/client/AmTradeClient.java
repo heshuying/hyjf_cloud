984fa4c5332fbcfe713d6539ccb646c7b7ee947a
TonyLee <libin@hyjf.com>
2018-07-17 09:30:32 +0800

    admin资产中心client更换到指定接口

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/AmTradeClient.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/AmTradeClient.java
@@ -936,4 +936,61 @@ public interface AmTradeClient {
      * @return
      */
     List<HjhInstConfigVO> selectHjhInstConfigList();
+    
+	/**
+	 * 获取资金来源
+	 *
+	 * @param 
+	 * @return List<HjhInstConfigVO>
+	 */
+    List<HjhInstConfigVO> findHjhInstConfigList();
+
+	/**
+	 * 产品类型下拉联动
+	 *
+	 * @param instCodeSrch
+	 * @return List<HjhAssetTypeVO>
+	 */
+    List<HjhAssetTypeVO> findHjhAssetTypeList(String instCodeSrch);
+
+    /**
+     * param
+     * @param request
+     * @return
+     */
+    Map<String, String> findParamNameMap(String param);
+
+    /**
+     * 查询资产列表
+     * @param request
+     * @return
+     */
+    AssetListCustomizeResponse findAssetList(AssetListRequest request);
+
+    /**
+     * 详情查询
+     * @param request
+     * @return
+     */
+    AssetDetailCustomizeVO findDetailById(AssetListRequest assetListRequest);
+    
+    /**
+     * 总计
+     * @param request
+     * @return
+     */
+    Integer getRecordCount(AssetListRequest request);
+    
+    /**列总数
+     * @param request
+     * @return
+     */
+    BigDecimal sumAccount(AssetListRequest request);
+
+    /**
+     * 处理保证金不足的资产
+     * @param assetId
+     * @param menuHide
+     */
+	void updateCashDepositeStatus(String assetId, String menuHide);
 }
--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/impl/AmTradeClientImpl.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/impl/AmTradeClientImpl.java
@@ -28,6 +28,7 @@ import com.hyjf.am.vo.trade.hjh.HjhDebtCreditVO;
 import com.hyjf.am.vo.trade.hjh.HjhPlanVO;
 import com.hyjf.am.vo.trade.repay.BankRepayFreezeLogVO;
 import com.hyjf.am.vo.user.HjhInstConfigVO;
+import com.hyjf.common.cache.CacheUtil;
 import com.hyjf.common.validator.Validator;
 import com.hyjf.pay.lib.bank.bean.BankCallBean;
 import org.slf4j.Logger;
@@ -2012,4 +2013,127 @@ public class AmTradeClientImpl implements AmTradeClient{
         }
         return null;
     }
+    
+    /**
+	 * 获取资金来源
+	 *
+	 * @param 
+	 * @return List<HjhInstConfigVO>
+	 */
+	@Override
+	public List<HjhInstConfigVO> findHjhInstConfigList() {
+		HjhInstConfigResponse response = restTemplate.
+				getForEntity("http://AM-TRADE/am-trade/hjhInstConfig/selectInstConfigAll", HjhInstConfigResponse.class).
+				getBody();
+		if (response != null && Response.SUCCESS.equals(response.getRtn())) {
+			return response.getResultList();
+		}
+		return null;
+	}
+	
+	/**
+	 * 产品类型下拉联动
+	 *
+	 * @param instCodeSrch
+	 * @return List<HjhAssetTypeVO>
+	 */
+	@Override
+	public List<HjhAssetTypeVO> findHjhAssetTypeList(String instCodeSrch) {
+		HjhAssetTypeResponse response = restTemplate.
+				getForEntity("http://AM-TRADE/am-trade/hjhAssetType/selectAssetTypeAll/"+ instCodeSrch, HjhAssetTypeResponse.class).
+				getBody();
+		if (response != null && Response.SUCCESS.equals(response.getRtn())) {
+			return response.getResultList();
+		}
+		return null;
+	}
+
+    /**
+     * 查询ParamName
+     * @param request
+     * @return
+     */
+	@Override
+	public Map<String, String> findParamNameMap(String param) {
+		Map<String,String> resultMap = CacheUtil.getParamNameMap(param);
+		return resultMap;
+	}
+
+    /**
+     * 查询资产列表
+     * @param request
+     * @return
+     */
+	@Override
+	public AssetListCustomizeResponse findAssetList(AssetListRequest request) {
+		AssetListCustomizeResponse response = restTemplate
+				.postForEntity("http://AM-TRADE/am-trade/assetList/findAssetList" ,request,
+						AssetListCustomizeResponse.class)
+				.getBody();
+		if (response != null && Response.SUCCESS.equals(response.getRtn())) {
+			return response;
+		}
+		return null;
+	}
+	
+	/**
+	 * 查询详情
+	 *
+	 * @param request
+	 * @return 查询详情
+	 */
+	@Override
+	public AssetDetailCustomizeVO findDetailById(AssetListRequest assetListRequest) {
+		AssetDetailCustomizeResponse response = restTemplate
+				.postForEntity("http://AM-TRADE/am-trade/assetList/findDetailById",assetListRequest,
+						AssetDetailCustomizeResponse.class)
+				.getBody();
+		if (response != null && Response.SUCCESS.equals(response.getRtn())) {
+			return response.getResult();
+		}
+		return null;
+	}
+	
+	/**
+	 * 查询记录总数
+	 *
+	 * @param request
+	 * @return 查询详情
+	 */
+	@Override
+	public Integer getRecordCount(AssetListRequest request) {
+		AssetListCustomizeResponse response = restTemplate
+				.postForEntity("http://AM-TRADE/am-trade/assetList/findRecordCount" ,request,
+						AssetListCustomizeResponse.class)
+				.getBody();
+		if (response != null && Response.SUCCESS.equals(response.getRtn())) {
+			return response.getCount();
+		}
+		return null;
+	}
+
+	/**
+	 * 查询列总计
+	 *
+	 * @param request
+	 * @return 查询详情
+	 */
+	@Override
+	public BigDecimal sumAccount(AssetListRequest request) {
+		AssetListCustomizeResponse response = restTemplate
+				.postForEntity("http://AM-TRADE/am-trade/assetList/sumAccount" ,request,
+						AssetListCustomizeResponse.class)
+				.getBody();
+		if (response != null && Response.SUCCESS.equals(response.getRtn())) {
+			return response.getSum();
+		}
+		return null;
+	}
+
+	@Override
+	public void updateCashDepositeStatus(String assetId, String menuHide) {
+		String url = "http://AM-TRADE/am-trade/assetList/updateCashDepositeStatus/"+assetId+"/"+menuHide;
+		restTemplate.getForEntity(url, String.class).getBody();
+		
+	}
 }
