5f06a26feaf06ab341efa5e3b6733dee4681d9be
sunshuaishuai <sunshuaishuai@hyjf.com>
2018-07-18 17:42:59 +0800

    CreditClient 合并

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/AmTradeClient.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/AmTradeClient.java
@@ -1,6 +1,7 @@
 package com.hyjf.cs.trade.client;
 
 import com.hyjf.am.response.trade.CreditListResponse;
+import com.hyjf.am.response.trade.MyCreditListQueryResponse;
 import com.hyjf.am.response.trade.ProjectListResponse;
 import com.hyjf.am.response.trade.coupon.CouponResponse;
 import com.hyjf.am.resquest.trade.*;
@@ -1177,4 +1178,112 @@ public interface AmTradeClient {
      * @return
      */
     Integer insertTransferExLog(TransferExceptionLogWithBLOBsVO transferExceptionLog);
+
+    /**
+     * 判断用户所处的渠道如果不允许债转，可债转金额为0  start
+     * @param userId
+     * @return
+     */
+    boolean isAllowChannelAttorn(Integer userId);
+
+    /**
+     * 获取可债转金额   转让中本金   累计已转让本金
+     * @param userId
+     * @return
+     */
+    CreditPageVO selectCreditPageMoneyTotal(Integer userId);
+
+    /**
+     * 查询可债转列表数量
+     * @param request
+     * @return
+     */
+    MyCreditListQueryResponse countMyCreditList(MyCreditListQueryRequest request);
+
+    /**
+     * 查询可债转列表
+     * @param request
+     * @return
+     */
+    MyCreditListQueryResponse searchMyCreditList(MyCreditListQueryRequest request);
+
+    /**
+     * 查询债转详情
+     * @param userId
+     * @param borrowNid
+     * @param tenderNid
+     * @return
+     */
+    TenderCreditCustomizeVO selectTenderToCreditDetail(Integer userId, String borrowNid, String tenderNid);
+
+    /**
+     * 债转详细预计服务费计算
+     * @param borrowNid
+     * @param tenderNid
+     * @return
+     */
+    ExpectCreditFeeVO selectExpectCreditFee(String borrowNid, String tenderNid);
+
+    /**
+     * 验证投资人当天是否可以债转
+     * @param userId
+     * @return
+     */
+    Integer tenderAbleToCredit(Integer userId);
+
+    /**
+     * 根据投资订单号检索已债转还款信息
+     * @param tenderId
+     * @return
+     */
+    List<CreditRepayVO> selectCreditRepayList(Integer tenderId);
+
+    List<CreditRepayVO> selectCreditRepayList(String borrowNid, String tenderOrderId, Integer periodNow, Integer status);
+
+    /**
+     * 插入债转  我要债转
+     * @param borrowCredit
+     */
+    Integer insertCredit(BorrowCreditVO borrowCredit);
+
+    /**
+     * 前端Web页面投资可债转输入投资金额后收益提示 用户未登录 (包含查询条件)
+     * @param creditNid
+     * @param assignCapital
+     * @param userId
+     * @return
+     */
+    TenderToCreditAssignCustomizeVO getInterestInfo(String creditNid, String assignCapital, Integer userId);
+
+    /**
+     * 获取债转数据
+     * @param creditNid
+     * @return
+     */
+    BorrowCreditVO getBorrowCreditByCreditNid(String creditNid);
+
+    /**
+     * 债转修改日志表状态
+     * @param logOrderId
+     * @param logUserId
+     * @param retCode
+     * @param retMsg
+     */
+    Integer updateCreditTenderResult(String logOrderId, String logUserId, String retCode, String retMsg);
+
+    /**
+     * 查询债转失败原因
+     * @param logOrdId
+     * @param userId
+     * @return
+     */
+    String getFailResult(String logOrdId, Integer userId);
+
+    /**
+     * 根据logOrdId和userId 查询债转信息
+     * @param logOrdId
+     * @param userId
+     * @return
+     */
+    CreditTenderVO getCreditTenderByUserIdOrdId(String logOrdId, Integer userId);
 }
--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/impl/AmTradeClientImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/impl/AmTradeClientImpl.java
@@ -3,6 +3,7 @@ package com.hyjf.cs.trade.client.impl;
 import com.hyjf.am.response.IntegerResponse;
 import com.hyjf.am.response.MapResponse;
 import com.hyjf.am.response.Response;
+import com.hyjf.am.response.admin.CouponConfigCustomizeResponse;
 import com.hyjf.am.response.admin.CouponRecoverResponse;
 import com.hyjf.am.response.admin.TransferExceptionLogResponse;
 import com.hyjf.am.response.trade.*;
@@ -10,6 +11,7 @@ import com.hyjf.am.response.trade.coupon.CouponResponse;
 import com.hyjf.am.response.user.BankOpenAccountResponse;
 import com.hyjf.am.response.user.HjhPlanResponse;
 import com.hyjf.am.response.user.HjhUserAuthResponse;
+import com.hyjf.am.response.user.UtmPlatResponse;
 import com.hyjf.am.response.wdzj.BorrowDataResponse;
 import com.hyjf.am.response.wdzj.PreapysListResponse;
 import com.hyjf.am.resquest.trade.*;
@@ -2611,5 +2613,230 @@ public class AmTradeClientImpl implements AmTradeClient {
             return response.getFlag();
         }
         return 0;
+    }
+
+    /**
+     * 判断用户所处的渠道如果不允许债转，可债转金额为0  start
+     *
+     * @param userId
+     * @return
+     */
+    @Override
+    public boolean isAllowChannelAttorn(Integer userId) {
+        // 根据userId获取用户注册推广渠道
+        UtmPlatResponse response = restTemplate
+                .getForEntity("http://AM-USER/am-user/user/selectUtmPlatByUserId/" + userId,UtmPlatResponse.class)
+                .getBody();
+        if (Response.isSuccess(response) && response.getResult().getAttornFlag()==0) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 获取可债转金额   转让中本金   累计已转让本金
+     *
+     * @param userId
+     * @return
+     */
+    @Override
+    public CreditPageVO selectCreditPageMoneyTotal(Integer userId) {
+        // 根据userId获取用户注册推广渠道
+        CreaditPageResponse response = restTemplate
+                .getForEntity("http://AM-TRADE/am-trade/creditTender/select_credit_page_money_total/" + userId,CreaditPageResponse.class)
+                .getBody();
+        if (Response.isSuccess(response) ) {
+            return response.getResult();
+        }
+        return null;
+    }
+
+    /**
+     * 查询可债转列表数量
+     *
+     * @param request
+     * @return
+     */
+    @Override
+    public MyCreditListQueryResponse countMyCreditList(MyCreditListQueryRequest request) {
+        MyCreditListQueryResponse response =  restTemplate.postForEntity("http://AM-TRADE/am-trade/creditTender/countMyCreditList",request,MyCreditListQueryResponse.class).getBody();
+        return response;
+    }
+
+    /**
+     * 查询可债转列表
+     *
+     * @param request
+     * @return
+     */
+    @Override
+    public MyCreditListQueryResponse searchMyCreditList(MyCreditListQueryRequest request) {
+        MyCreditListQueryResponse response =  restTemplate.postForEntity("http://AM-TRADE/am-trade/creditTender/seachMyCreditList",request,MyCreditListQueryResponse.class).getBody();
+        return response;
+    }
+
+    /**
+     * 查询债转详情
+     *
+     * @param userId
+     * @param borrowNid
+     * @param tenderNid
+     * @return
+     */
+    @Override
+    public TenderCreditCustomizeVO selectTenderToCreditDetail(Integer userId, String borrowNid, String tenderNid) {
+        String url = "http://AM-TRADE/am-trade/creditTender/selectTenderToCreditDetail/" + userId + "/" + borrowNid + "/" + tenderNid;
+        MyCreditListQueryResponse response = restTemplate.getForEntity(url, MyCreditListQueryResponse.class).getBody();
+        if (Response.isSuccess(response)) {
+            return response.getResult();
+        }
+        return null;
+    }
+
+    /**
+     * 债转详细预计服务费计算
+     *
+     * @param borrowNid
+     * @param tenderNid
+     * @return
+     */
+    @Override
+    public ExpectCreditFeeVO selectExpectCreditFee(String borrowNid, String tenderNid) {
+        String url = "http://AM-TRADE/am-trade/creditTender/selectExpectCreditFee/" + borrowNid + "/" + tenderNid ;
+        ExpectCreditFeeResponse response = restTemplate.getForEntity(url, ExpectCreditFeeResponse.class).getBody();
+        if (Response.isSuccess(response)) {
+            return response.getResult();
+        }
+        return null;
+    }
+
+    /**
+     * 验证投资人当天是否可以债转
+     *
+     * @param userId
+     * @return
+     */
+    @Override
+    public Integer tenderAbleToCredit(Integer userId) {
+        String url = "http://AM-TRADE/am-trade/creditTender/get_tender_credit_count/" + userId ;
+        Integer response = restTemplate.getForEntity(url, Integer.class).getBody();
+        if (response != null) {
+            return response;
+        }
+        return null;
+    }
+
+    /**
+     * 根据投资订单号检索已债转还款信息
+     *
+     * @param tenderId
+     * @return
+     */
+    @Override
+    public List<CreditRepayVO> selectCreditRepayList(Integer tenderId) {
+        CreditRepayResponse response = restTemplate.getForEntity("http://AM-TRADE/am-trade/creditTender/select_credit_repay_list/" + tenderId , CreditRepayResponse.class).getBody();
+        if (response != null) {
+            return response.getResultList();
+        }
+        return null;
+    }
+
+    @Override
+    public List<CreditRepayVO> selectCreditRepayList(String borrowNid, String tenderOrderId, Integer periodNow, Integer status) {
+        String url = "http://AM-TRADE/am-trade/creditTender/select_credit_repay_list/" + borrowNid + "/" + tenderOrderId + "/" + periodNow + "/" + status;
+        CreditRepayResponse response = restTemplate.getForEntity(url, CreditRepayResponse.class).getBody();
+        if (response != null) {
+            return response.getResultList();
+        }
+        return null;
+    }
+
+    /**
+     * 插入债转  我要债转
+     *
+     * @param borrowCredit
+     */
+    @Override
+    public Integer insertCredit(BorrowCreditVO borrowCredit) {
+        String url = "http://AM-TRADE/am-trade/creditTender/save_credit_tender";
+        return restTemplate.postForEntity(url, borrowCredit, Integer.class).getBody();
+    }
+
+    /**
+     * 前端Web页面投资可债转输入投资金额后收益提示 用户未登录 (包含查询条件)
+     *
+     * @param creditNid
+     * @param assignCapital
+     * @param userId
+     * @return
+     */
+    @Override
+    public TenderToCreditAssignCustomizeVO getInterestInfo(String creditNid, String assignCapital, Integer userId) {
+        String url = "http://AM-TRADE/am-trade/creditTender/get_interest_info/" + creditNid + "/" + assignCapital + "/" + userId;
+        CreditAssignCustomizeResponse response = restTemplate.getForEntity(url, CreditAssignCustomizeResponse.class).getBody();
+        if (Response.isSuccess(response)) {
+            return response.getResult();
+        }
+        return null;
+    }
+
+    /**
+     * 获取债转数据
+     *
+     * @param creditNid
+     * @return
+     */
+    @Override
+    public BorrowCreditVO getBorrowCreditByCreditNid(String creditNid) {
+        String url = "http://AM-TRADE/am-trade/creditTender/get_borrow_credit_by_credit_nid/" + creditNid ;
+        BorrowCreditResponse response = restTemplate.getForEntity(url, BorrowCreditResponse.class).getBody();
+        if (Response.isSuccess(response)) {
+            return response.getResult();
+        }
+        return null;
+    }
+
+    /**
+     * 债转修改日志表状态
+     *
+     * @param logOrderId
+     * @param logUserId
+     * @param retCode
+     * @param retMsg
+     */
+    @Override
+    public Integer updateCreditTenderResult(String logOrderId, String logUserId, String retCode, String retMsg) {
+        String url = "http://AM-TRADE/am-trade/creditTender/updateCreditTenderResult/" + logOrderId+"/"+logUserId+"/"+retCode+"/"+retMsg ;
+        return restTemplate.getForEntity(url, Integer.class).getBody();
+    }
+
+    /**
+     * 查询债转失败原因
+     *
+     * @param logOrdId
+     * @param userId
+     * @return
+     */
+    @Override
+    public String getFailResult(String logOrdId, Integer userId) {
+        String url = "http://AM-TRADE/am-trade/creditTender/getFaileResult/" + logOrdId+"/"+userId ;
+        return restTemplate.getForEntity(url, String.class).getBody();
+    }
+
+    /**
+     * 根据logOrdId和userId 查询债转信息
+     *
+     * @param logOrdId
+     * @param userId
+     * @return
+     */
+    @Override
+    public CreditTenderVO getCreditTenderByUserIdOrdId(String logOrdId, Integer userId) {
+        String url = "http://AM-TRADE/am-trade/creditTender/getCreditTenderByUserIdOrdId/" + logOrdId + "/" + userId;
+        CreditTenderResponse response = restTemplate.getForEntity(url, CreditTenderResponse.class).getBody();
+        if (Response.isSuccess(response)) {
+            return response.getResult();
+        }
+        return null;
     }
 }
