da9f6ee269a4d90bc6456a8cd96b32212a6a7b66
fuqiang <18251359382@163.com>
2018-07-18 16:23:51 +0800

    运营数据统计定时组合层

--- hyjf_cs_server/hyjf_cs_market/src/main/java/com/hyjf/cs/market/client/impl/AmDataCollectClientImpl.java
+++ hyjf_cs_server/hyjf_cs_market/src/main/java/com/hyjf/cs/market/client/impl/AmDataCollectClientImpl.java
@@ -6,14 +6,14 @@ package com.hyjf.cs.market.client.impl;
 import org.springframework.stereotype.Service;
 
 import com.hyjf.am.vo.datacollect.BorrowUserStatisticVO;
-import com.hyjf.cs.market.client.AmDataCollect;
+import com.hyjf.cs.market.client.AmDataCollectClient;
 
 /**
  * @author fuqiang
  * @version AmDataCollectImpl, v0.1 2018/7/18 13:56
  */
 @Service
-public class AmDataCollectImpl implements AmDataCollect {
+public class AmDataCollectClientImpl implements AmDataCollectClient {
     @Override
     public BorrowUserStatisticVO selectBorrowUserStatistic() {
         return null;
