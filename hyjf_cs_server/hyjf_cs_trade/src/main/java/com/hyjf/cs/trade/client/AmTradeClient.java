940c7685d5eaa7df8d34dc3933be3df9796a9ae2
sunshuaishuai <sunshuaishuai@hyjf.com>
2018-07-18 15:29:08 +0800

    BorrowRecoverClient 合并

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/AmTradeClient.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/AmTradeClient.java
@@ -770,4 +770,38 @@ public interface AmTradeClient {
      * @param creditTenderBg
      */
     Integer saveCreditBgData(CreditTenderBgVO creditTenderBg);
+
+    BorrowRecoverVO selectBorrowRecoverByTenderNid(String tenderAgreementID);
+
+    BorrowRecoverVO selectBorrowRecoverById(Integer id);
+
+    BorrowRecoverVO selectBorrowRecoverByNid(String nid);
+
+    List<BorrowRecoverVO> selectBorrowRecoverByBorrowNid(String borrowNid);
+
+    void updateBorrowRecover(BorrowRecoverVO borrowRecover);
+
+    /**
+     * 根据tenderNid   和  bidNid 查询
+     * @param tenderNid
+     * @param bidNid
+     * @return
+     */
+    BorrowRecoverVO getBorrowRecoverByTenderNidBidNid(String tenderNid, String bidNid);
+
+    /**
+     * 根据tenderNid 查询
+     * @param tenderNid
+     * @return
+     */
+    BorrowRecoverVO getBorrowRecoverByTenderNid(String tenderNid);
+
+    /**
+     * 获取borrow_recover_plan更新每次还款时间
+     * @param bidNid
+     * @param creditTenderNid
+     * @param periodNow
+     * @return
+     */
+    BorrowRecoverPlanVO getPlanByBidTidPeriod(String bidNid, String creditTenderNid, int periodNow);
 }
--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/impl/AmTradeClientImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/impl/AmTradeClientImpl.java
@@ -1861,4 +1861,100 @@ public class AmTradeClientImpl implements AmTradeClient {
         return response;
     }
 
+    @Override
+    public BorrowRecoverVO selectBorrowRecoverByTenderNid(String tenderAgreementID) {
+        String url = "http://AM-TRADE/am-trade/borrowRecover/selectBorrowRecoverByTenderNid/"+tenderAgreementID;
+        BorrowRecoverResponse response = restTemplate.getForEntity(url,BorrowRecoverResponse.class).getBody();
+        if (Validator.isNotNull(response)){
+            return response.getResult();
+        }
+        return null;
+    }
+
+    @Override
+    public BorrowRecoverVO selectBorrowRecoverById(Integer id) {
+        String url = "http://AM-TRADE/am-trade/borrowRecover/select_by_id/"+id;
+        BorrowRecoverResponse response = restTemplate.getForEntity(url,BorrowRecoverResponse.class).getBody();
+        if (Validator.isNotNull(response)){
+            return response.getResult();
+        }
+        return null;
+    }
+
+    @Override
+    public BorrowRecoverVO selectBorrowRecoverByNid(String nid) {
+        String url = "http://AM-TRADE/am-trade/borrowRecover/select_by_nid/"+nid;
+        BorrowRecoverResponse response = restTemplate.getForEntity(url,BorrowRecoverResponse.class).getBody();
+        if (Validator.isNotNull(response)){
+            return response.getResult();
+        }
+        return null;
+    }
+
+    @Override
+    public List<BorrowRecoverVO> selectBorrowRecoverByBorrowNid(String borrowNid) {
+        String url = "http://AM-TRADE/am-trade/borrowRecover/select_by_borrownid/"+borrowNid;
+        BorrowRecoverResponse response = restTemplate.getForEntity(url,BorrowRecoverResponse.class).getBody();
+        if (Validator.isNotNull(response)){
+            return response.getResultList();
+        }
+        return null;
+    }
+
+    @Override
+    public void updateBorrowRecover(BorrowRecoverVO borrowRecover) {
+        String url = "http://AM-TRADE/am-trade/borrowRecover/updateBorrowRecover";
+        restTemplate.postForEntity(url,borrowRecover,Boolean.class).getBody();
+    }
+
+    /**
+     * 根据tenderNid   和  bidNid 查询
+     *
+     * @param tenderNid
+     * @param bidNid
+     * @return
+     */
+    @Override
+    public BorrowRecoverVO getBorrowRecoverByTenderNidBidNid(String tenderNid, String bidNid) {
+        String url = "http://AM-TRADE/am-trade/borrowRecover/getBorrowRecoverByTenderNidBidNid/"+tenderNid+"/"+bidNid;
+        BorrowRecoverResponse response = restTemplate.getForEntity(url,BorrowRecoverResponse.class).getBody();
+        if (Validator.isNotNull(response)){
+            return response.getResult();
+        }
+        return null;
+    }
+
+    /**
+     * 根据tenderNid 查询
+     *
+     * @param tenderNid
+     * @return
+     */
+    @Override
+    public BorrowRecoverVO getBorrowRecoverByTenderNid(String tenderNid) {
+        String url = "http://AM-TRADE/am-trade/borrowRecover/getBorrowRecoverByTenderNid/"+tenderNid;
+        BorrowRecoverResponse response = restTemplate.getForEntity(url,BorrowRecoverResponse.class).getBody();
+        if (Validator.isNotNull(response)){
+            return response.getResult();
+        }
+        return null;
+    }
+
+    /**
+     * 获取borrow_recover_plan更新每次还款时间
+     *
+     * @param bidNid
+     * @param creditTenderNid
+     * @param periodNow
+     * @return
+     */
+    @Override
+    public BorrowRecoverPlanVO getPlanByBidTidPeriod(String bidNid, String creditTenderNid, int periodNow) {
+        String url = "http://AM-TRADE/am-trade/borrowRecover/getPlanByBidTidPeriod/"+bidNid+"/"+creditTenderNid+"/"+periodNow;
+        BorrowRecoverPlanResponse response = restTemplate.getForEntity(url,BorrowRecoverPlanResponse.class).getBody();
+        if (Validator.isNotNull(response)){
+            return response.getResult();
+        }
+        return null;
+    }
 }
