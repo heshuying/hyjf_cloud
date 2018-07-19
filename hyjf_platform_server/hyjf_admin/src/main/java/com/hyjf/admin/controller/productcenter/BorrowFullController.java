6edc64dfe552291f9de651759db41ed94376e05e
wangjun <273407491@qq.com>
2018-07-19 19:22:57 +0800

    代码优化

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/BorrowFullController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/BorrowFullController.java
@@ -6,7 +6,9 @@ package com.hyjf.admin.controller.productcenter;
 import com.hyjf.admin.beans.request.BorrowFullRequestBean;
 import com.hyjf.admin.beans.response.BorrowFullResponseBean;
 import com.hyjf.admin.common.result.AdminResult;
+import com.hyjf.admin.common.util.ShiroConstants;
 import com.hyjf.admin.controller.BaseController;
+import com.hyjf.admin.interceptor.AuthorityAnnotation;
 import com.hyjf.admin.service.BorrowFullService;
 import com.hyjf.am.resquest.admin.BorrowFullRequest;
 import io.swagger.annotations.Api;
@@ -22,16 +24,20 @@ import javax.validation.Valid;
  * @author wangjun
  * @version BorrowFullController, v0.1 2018/7/6 9:31
  */
-@Api(value = "汇直投-借款复审接口")
+@Api(value = "汇直投-借款复审接口", description = "汇直投-借款复审接口")
 @RestController
 @RequestMapping("/hyjf-admin/borrow_full")
 public class BorrowFullController extends BaseController {
     @Autowired
     BorrowFullService borrowFullService;
 
+    /** 权限 */
+    public static final String PERMISSIONS = "borrowfull";
+
     @ApiOperation(value = "借款复审初始化/获取列表共用接口", notes = "借款复审初始化/获取列表共用接口")
-    @PostMapping("/get_borrow_full_list")
+    @PostMapping("/search")
     @ResponseBody
+    @AuthorityAnnotation(key = PERMISSIONS, value = ShiroConstants.PERMISSION_SEARCH)
     public AdminResult getBorrowFullList(@RequestBody BorrowFullRequestBean borrowFullRequestBean) {
         BorrowFullRequest borrowFullRequest = new BorrowFullRequest();
         BeanUtils.copyProperties(borrowFullRequestBean, borrowFullRequest);
@@ -42,6 +48,7 @@ public class BorrowFullController extends BaseController {
     @ApiOperation(value = "借款复审详细信息", notes = "借款复审详细信息")
     @PostMapping("/get_full_info")
     @ResponseBody
+    @AuthorityAnnotation(key = PERMISSIONS, value = ShiroConstants.BORROW_FULL)
     public AdminResult getFullInfo(@RequestBody BorrowFullRequestBean borrowFullRequestBean) {
         BorrowFullRequest borrowFullRequest = new BorrowFullRequest();
         BeanUtils.copyProperties(borrowFullRequestBean, borrowFullRequest);
@@ -51,6 +58,7 @@ public class BorrowFullController extends BaseController {
     @ApiOperation(value = "复审提交", notes = "复审提交")
     @PostMapping("/update_borrow_full")
     @ResponseBody
+    @AuthorityAnnotation(key = PERMISSIONS, value = ShiroConstants.BORROW_FULL)
     public AdminResult updateBorrowFull(HttpServletRequest request, @RequestBody @Valid BorrowFullRequestBean borrowFullRequestBean) {
         //todo wangjun 取得当前用户信息 备用 后期修改
 //        AdminSystemVO currUser = getUser(request);
@@ -64,6 +72,7 @@ public class BorrowFullController extends BaseController {
     @ApiOperation(value = "流标", notes = "流标")
     @GetMapping("/update_borrow_over/{borrowNid}")
     @ResponseBody
+    @AuthorityAnnotation(key = PERMISSIONS, value = ShiroConstants.BORROW_OVER)
     public AdminResult updateBorrowOver(HttpServletRequest request, @PathVariable String borrowNid) {
         //todo wangjun 取得当前用户信息 备用 后期修改
 //        AdminSystemVO currUser = getUser(request);
