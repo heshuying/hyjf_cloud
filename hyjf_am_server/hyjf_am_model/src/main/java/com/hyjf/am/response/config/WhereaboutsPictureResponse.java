201f0747aeb7e0896f5205a1dc452fe02aa7b5af
tanyangyang <591628420@qq.com>
2018-07-20 09:28:43 +0800

    移动端着陆页管理

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/config/WhereaboutsPictureResponse.java
@@ -0,0 +1,24 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.response.config;
+
+import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.config.WhereaboutsPagePictureVo;
+import com.hyjf.am.vo.config.WhereaboutsPageVo;
+
+/**
+ * @author tanyy
+ * @version WhereaboutsPictureResponse, v0.1 2018/7/16 14:19
+ */
+public class WhereaboutsPictureResponse extends Response<WhereaboutsPagePictureVo> {
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
