f444c8564508572f4f7a1ab3207c138260869a56
nixiaoling <nixiaoling@hyjf.com>
2018-07-23 09:38:19 +0800

    修改注释

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/beans/vo/BankcardManagerCustomizeVO.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/beans/vo/BankcardManagerCustomizeVO.java
@@ -1,12 +1,6 @@
 /**
  * Description:银行卡绑定列表前端显示所用PO
  * Copyright: Copyright (HYJF Corporation) 2015
- * Company: HYJF Corporation
- * @author: 王坤
- * @version: 1.0
- * Created at: 2015年11月11日 下午2:17:31
- * Modification History:
- * Modified by : 
  */
 
 package com.hyjf.admin.beans.vo;
