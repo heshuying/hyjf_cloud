75d6a1fe9f81878e7d5c123f925807d878f53f38
pangchengchao <pangchengchao@hyjf.com>
2018-07-24 09:07:41 +0800

    swagger-ui注释添加

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/web/wirhdraw/WebBankWithdrawController.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/web/wirhdraw/WebBankWithdrawController.java
@@ -30,7 +30,7 @@ import java.util.Map;
  * @author pangchengchao
  * @version BankWithdrawController, v0.1 2018/6/12 18:32
  */
-@Api(value = "web端用户提现接口")
+@Api(value = "web端用户提现接口" ,description = "web端用户提现接口")
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/web/withdraw")
