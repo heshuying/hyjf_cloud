57f9103133ad2329e6ac48af71059be2bb59aa40
tanyangyang <591628420@qq.com>
2018-07-24 10:02:32 +0800

    运营报告第二期代码迁移

--- hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/message/OperationReportResponse.java
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/message/OperationReportResponse.java
@@ -11,13 +11,76 @@ import com.hyjf.am.vo.datacollect.OperationReportVO;
  * @version OperationReportResponse, v0.1 2018/7/23 14:00
  */
 public class OperationReportResponse extends Response<OperationReportVO> {
-    private  Integer  count;
-
+    private Integer count;
+    private String operationId;
+    private String monthlyOperationReportId;
+    private String userOperationReportId;
+    private String tenthOperationReportId;
+    private String yearlyOperationReportId;
+    private String quarterOperationReportId;
+    private String halfYearOperationReport;
     public Integer getCount() {
         return count;
     }
 
     public void setCount(Integer count) {
         this.count = count;
+    }
+
+    public String getOperationId() {
+        return operationId;
+    }
+
+    public void setOperationId(String operationId) {
+        this.operationId = operationId;
+    }
+
+    public String getMonthlyOperationReportId() {
+        return monthlyOperationReportId;
+    }
+
+    public void setMonthlyOperationReportId(String monthlyOperationReportId) {
+        this.monthlyOperationReportId = monthlyOperationReportId;
+
+    }
+
+    public String getUserOperationReportId() {
+        return userOperationReportId;
+    }
+
+    public void setUserOperationReportId(String userOperationReportId) {
+        this.userOperationReportId = userOperationReportId;
+    }
+
+    public String getTenthOperationReportId() {
+        return tenthOperationReportId;
+    }
+
+    public void setTenthOperationReportId(String tenthOperationReportId) {
+        this.tenthOperationReportId = tenthOperationReportId;
+    }
+
+    public String getYearlyOperationReportId() {
+        return yearlyOperationReportId;
+    }
+
+    public void setYearlyOperationReportId(String yearlyOperationReportId) {
+        this.yearlyOperationReportId = yearlyOperationReportId;
+    }
+
+    public String getQuarterOperationReportId() {
+        return quarterOperationReportId;
+    }
+
+    public void setQuarterOperationReportId(String quarterOperationReportId) {
+        this.quarterOperationReportId = quarterOperationReportId;
+    }
+
+    public String getHalfYearOperationReport() {
+        return halfYearOperationReport;
+    }
+
+    public void setHalfYearOperationReport(String halfYearOperationReport) {
+        this.halfYearOperationReport = halfYearOperationReport;
     }
 }
