ad45e2f6e2020db98dd96b6d429fefca633b39f9
zhangyunkai <zhangyunkai@hyjf.com>
2018-07-25 18:01:28 +0800

    ## 债转详情承接记录

---
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/bean/WebCreditRequestBean.java
@@ -0,0 +1,16 @@
+package com.hyjf.cs.trade.bean;
+
+import com.hyjf.am.vo.BasePage;
+
+public class WebCreditRequestBean extends BasePage {
+
+    private String creditNid;
+
+    public String getCreditNid() {
+        return creditNid;
+    }
+
+    public void setCreditNid(String creditNid) {
+        this.creditNid = creditNid;
+    }
+}
