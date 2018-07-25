0eab85d693d03943ac4f6e2ea1bae2a89d8d342e
fuqiang <18251359382@163.com>
2018-07-25 16:19:00 +0800

    初次提交

---
+++ hyjf_cs_server/hyjf_cs_message/src/main/java/com/hyjf/cs/message/service/news/config/impl/AppNewsConfigServiceImpl.java
@@ -0,0 +1,25 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.cs.message.service.news.config.impl;
+
+import com.hyjf.am.vo.user.UserVO;
+import com.hyjf.cs.message.client.AmConfigClient;
+import com.hyjf.cs.message.service.news.config.AppNewsConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author fq
+ * @version AppNewsConfigServiceImpl, v0.1 2018/7/25 15:49
+ */
+@Service
+public class AppNewsConfigServiceImpl implements AppNewsConfigService {
+    @Autowired
+    private AmConfigClient amConfigClient;
+
+    @Override
+    public int updateAppNewsConfig(UserVO users) {
+        return 0;// todo
+    }
+}
