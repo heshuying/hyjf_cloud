d1370f5d9ae1e76690772e36006f6dd5847decd4
sunpeikai <spk123456>
2018-07-25 15:55:38 +0800

    获取我的账单(汇天利)

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/dao/model/customize/ProductRedeemCustomize.java
@@ -0,0 +1,199 @@
+package com.hyjf.am.trade.dao.model.customize;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ProductRedeemCustomize implements Serializable {
+    private Integer id;//主键
+    private Integer userId;
+    private String orderId;
+    private BigDecimal amount;
+    private String redeemTime;
+    private BigDecimal interest;
+    private BigDecimal total;
+    private Integer referee;
+    private Integer area;
+    private Integer company;
+    private Integer department;
+    private String remark;
+    private Integer client;
+    private String username;
+    private String refername;
+    private String mobile;
+    private String turename;
+    private Integer status;
+	private String regionName;
+	private String branceName;
+	private String departmentName;
+
+    public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRefername() {
+		return refername;
+	}
+
+	public void setRefername(String refername) {
+		this.refername = refername;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getTurename() {
+		return turename;
+	}
+
+	public void setTurename(String turename) {
+		this.turename = turename;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId == null ? null : orderId.trim();
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getRedeemTime() {
+        return redeemTime;
+    }
+
+    public void setRedeemTime(String redeemTime) {
+        this.redeemTime = redeemTime;
+    }
+
+    public BigDecimal getInterest() {
+        return interest;
+    }
+
+    public void setInterest(BigDecimal interest) {
+        this.interest = interest;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public Integer getReferee() {
+        return referee;
+    }
+
+    public void setReferee(Integer referee) {
+        this.referee = referee;
+    }
+
+    public Integer getArea() {
+        return area;
+    }
+
+    public void setArea(Integer area) {
+        this.area = area;
+    }
+
+    public Integer getCompany() {
+        return company;
+    }
+
+    public void setCompany(Integer company) {
+        this.company = company;
+    }
+
+    public Integer getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Integer department) {
+        this.department = department;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Integer getClient() {
+        return client;
+    }
+
+    public void setClient(Integer client) {
+        this.client = client;
+    }
+    
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+
+	public String getBranceName() {
+		return branceName;
+	}
+
+	public void setBranceName(String branceName) {
+		this.branceName = branceName;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+}
---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/resquest/user/HtlTradeRequest.java
@@ -0,0 +1,71 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.resquest.user;
+
+import com.hyjf.am.vo.BasePage;
+
+/**
+ * @author: sunpeikai
+ * @version: HtlTradeRequest, v0.1 2018/7/25 14:46
+ */
+public class HtlTradeRequest extends BasePage {
+    // 用户id
+    private Integer userId;
+    //状态
+    private Integer status;
+    //查询用  时间
+    private String timeStartSrch;
+    //查询用  时间
+    private String timeEndSrch;
+    private int limitStart = -1;
+    private int limitEnd = -1;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getTimeStartSrch() {
+        return timeStartSrch;
+    }
+
+    public void setTimeStartSrch(String timeStartSrch) {
+        this.timeStartSrch = timeStartSrch;
+    }
+
+    public String getTimeEndSrch() {
+        return timeEndSrch;
+    }
+
+    public void setTimeEndSrch(String timeEndSrch) {
+        this.timeEndSrch = timeEndSrch;
+    }
+
+    public int getLimitStart() {
+        return limitStart;
+    }
+
+    public void setLimitStart(int limitStart) {
+        this.limitStart = limitStart;
+    }
+
+    public int getLimitEnd() {
+        return limitEnd;
+    }
+
+    public void setLimitEnd(int limitEnd) {
+        this.limitEnd = limitEnd;
+    }
+}
