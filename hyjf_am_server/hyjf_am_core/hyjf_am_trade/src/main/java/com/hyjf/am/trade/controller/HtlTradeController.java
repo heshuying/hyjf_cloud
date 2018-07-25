cb1630db56dbaab6acfb6d0da3c8a845c932c4bb
sunpeikai <spk123456>
2018-07-25 16:58:25 +0800

    获取我的账单(汇天利)

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/HtlTradeController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/HtlTradeController.java
@@ -3,14 +3,26 @@
  */
 package com.hyjf.am.trade.controller;
 
+import com.hyjf.am.response.Response;
 import com.hyjf.am.response.trade.HtlProductIntoRecordResponse;
 import com.hyjf.am.response.trade.HtlProductRedeemResponse;
 import com.hyjf.am.resquest.user.HtlTradeRequest;
+import com.hyjf.am.trade.dao.model.customize.ProductIntoRecordCustomize;
+import com.hyjf.am.trade.dao.model.customize.ProductRedeemCustomize;
+import com.hyjf.am.trade.service.HtlTradeService;
+import com.hyjf.am.vo.trade.HtlProductIntoRecordVO;
+import com.hyjf.am.vo.trade.HtlProductRedeemVO;
+import com.hyjf.common.paginator.Paginator;
+import com.hyjf.common.util.CommonUtils;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author: sunpeikai
@@ -19,7 +31,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "获取我的账单-汇天利",description = "获取我的账单-汇天利")
 @RestController
 @RequestMapping(value = "/am-trade/htl")
-public class HtlTradeController {
+public class HtlTradeController extends BaseController{
+
+    @Autowired
+    private HtlTradeService htlTradeService;
 
     /**
      * 根据条件获取汇天利购买记录count
@@ -29,7 +44,11 @@ public class HtlTradeController {
      */
     @PostMapping(value = "/countHtlIntoRecord")
     public HtlProductIntoRecordResponse countHtlIntoRecord(@RequestBody HtlTradeRequest request){
-        return null;
+        HtlProductIntoRecordResponse response = new HtlProductIntoRecordResponse();
+        int count = htlTradeService.countHtlIntoRecord(request);
+        response.setCount(count);
+        response.setRtn(Response.SUCCESS);
+        return response;
     }
 
     /**
@@ -40,7 +59,23 @@ public class HtlTradeController {
      */
     @PostMapping(value = "/getIntoRecordList")
     public HtlProductIntoRecordResponse getIntoRecordList(@RequestBody HtlTradeRequest request){
-        return null;
+        HtlProductIntoRecordResponse response = new HtlProductIntoRecordResponse();
+        int count = htlTradeService.countHtlIntoRecord(request);
+        // currPage<0 为全部,currPage>0 为具体某一页
+        if(request.getCurrPage()>0){
+            Paginator paginator = new Paginator(request.getCurrPage(),count);
+            request.setLimitStart(paginator.getOffset());
+            request.setLimitEnd(paginator.getLimit());
+        }
+        logger.info("getIntoRecordList::::::::::limitStart=[{}],limitEnd=[{}]",request.getLimitStart(),request.getLimitEnd());
+        List<ProductIntoRecordCustomize> productIntoRecordCustomizes = htlTradeService.getIntoRecordList(request);
+        if(!CollectionUtils.isEmpty(productIntoRecordCustomizes)){
+            List<HtlProductIntoRecordVO> htlProductIntoRecordVOList = CommonUtils.convertBeanList(productIntoRecordCustomizes,HtlProductIntoRecordVO.class);
+            response.setRtn(Response.SUCCESS);
+            response.setResultList(htlProductIntoRecordVOList);
+        }
+
+        return response;
     }
     /**
      * 根据条件获取汇天利赎回记录count
@@ -50,7 +85,11 @@ public class HtlTradeController {
      */
     @PostMapping(value = "/countProductRedeemRecord")
     public HtlProductRedeemResponse countProductRedeemRecord(@RequestBody HtlTradeRequest request){
-        return null;
+        HtlProductRedeemResponse response = new HtlProductRedeemResponse();
+        int count = htlTradeService.countProductRedeemRecord(request);
+        response.setCount(count);
+        response.setRtn(Response.SUCCESS);
+        return response;
     }
 
     /**
@@ -61,6 +100,21 @@ public class HtlTradeController {
      */
     @PostMapping(value = "/getRedeemRecordList")
     public HtlProductRedeemResponse getRedeemRecordList(@RequestBody HtlTradeRequest request){
-        return null;
+        HtlProductRedeemResponse response = new HtlProductRedeemResponse();
+        int count = htlTradeService.countProductRedeemRecord(request);
+        // currPage<0 为全部,currPage>0 为具体某一页
+        if(request.getCurrPage()>0){
+            Paginator paginator = new Paginator(request.getCurrPage(),count);
+            request.setLimitStart(paginator.getOffset());
+            request.setLimitEnd(paginator.getLimit());
+        }
+        logger.info("getRedeemRecordList::::::::::limitStart=[{}],limitEnd=[{}]",request.getLimitStart(),request.getLimitEnd());
+        List<ProductRedeemCustomize> productRedeemCustomizes = htlTradeService.getRedeemRecordList(request);
+        if(!CollectionUtils.isEmpty(productRedeemCustomizes)){
+            List<HtlProductRedeemVO> htlProductIntoRecordVOList = CommonUtils.convertBeanList(productRedeemCustomizes,HtlProductRedeemVO.class);
+            response.setRtn(Response.SUCCESS);
+            response.setResultList(htlProductIntoRecordVOList);
+        }
+        return response;
     }
 }
