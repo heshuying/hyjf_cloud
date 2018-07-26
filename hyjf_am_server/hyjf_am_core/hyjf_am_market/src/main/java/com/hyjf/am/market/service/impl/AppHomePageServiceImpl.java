2cf6d87099751b8b4c13c2acfcf2713f42c1644e
dangzewen <dzw123456>
2018-07-26 14:34:30 +0800

    APP补充接口 /hyjf-app/homepage/getStartPage

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_market/src/main/java/com/hyjf/am/market/service/impl/AppHomePageServiceImpl.java
@@ -0,0 +1,33 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.market.service.impl;
+
+import com.hyjf.am.market.dao.mapper.auto.AdsMapper;
+import com.hyjf.am.market.dao.model.customize.app.AppAdsCustomize;
+import com.hyjf.am.market.service.AppHomePageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author dangzw
+ * @version AppHomePageServiceImpl, v0.1 2018/7/26 10:58
+ */
+@Service
+public class AppHomePageServiceImpl implements AppHomePageService {
+
+    @Autowired
+    AdsMapper adsMapper;
+
+    /**
+     * 查询首页的bannner列表
+     */
+    @Override
+    public List<AppAdsCustomize> searchBannerList(Map<String, Object> ads) {
+        List<AppAdsCustomize> adsList = adsMapper.selectAdsList(ads);
+        return adsList;
+    }
+}
