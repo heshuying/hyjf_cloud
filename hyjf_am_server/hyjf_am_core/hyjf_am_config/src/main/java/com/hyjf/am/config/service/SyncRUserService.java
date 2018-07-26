ec3e2114b3495865b88a60ed10ab0ece0b8b2472
dengxiaojiang <dengxiaojiang@hyjf.com>
2018-07-26 17:50:28 +0800

    同步各服务用户信息

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_config/src/main/java/com/hyjf/am/config/service/SyncRUserService.java
@@ -0,0 +1,13 @@
+ package com.hyjf.am.config.service;
+
+import com.alibaba.fastjson.JSONObject;
+
+public interface SyncRUserService{
+
+    void updateUserInfo(JSONObject jsonObj);
+
+    void insertUser(JSONObject jsonObj);
+
+    void updateSpreadUser(JSONObject jsonObj);
+
+}
