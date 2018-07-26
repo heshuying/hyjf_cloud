5fe397d2fb105e8fac7d22a1b4347bff09416531
wangjun <273407491@qq.com>
2018-07-26 10:57:40 +0800

    wechat端获取充值规则

---
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/bean/WxRechargeDescResultBean.java
@@ -0,0 +1,68 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.cs.trade.bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wangjun
+ * @version WxRechargeDescResultBean, v0.1 2018/7/26 9:13
+ */
+public class WxRechargeDescResultBean implements Serializable {
+    // 充值限额说明
+    private List<RechargeLimitAmountDesc> list;
+
+    public WxRechargeDescResultBean(){
+        list = new ArrayList<>();
+    }
+
+    public static class RechargeLimitAmountDesc {
+        private String bankName;
+        private String once;
+        private String day;
+        private String month;
+
+        public String getBankName() {
+            return bankName;
+        }
+
+        public void setBankName(String bankName) {
+            this.bankName = bankName;
+        }
+
+        public String getOnce() {
+            return once;
+        }
+
+        public void setOnce(String once) {
+            this.once = once;
+        }
+
+        public String getDay() {
+            return day;
+        }
+
+        public void setDay(String day) {
+            this.day = day;
+        }
+
+        public String getMonth() {
+            return month;
+        }
+
+        public void setMonth(String month) {
+            this.month = month;
+        }
+    }
+
+    public List<RechargeLimitAmountDesc> getList() {
+        return list;
+    }
+
+    public void setList(List<RechargeLimitAmountDesc> list) {
+        this.list = list;
+    }
+}
