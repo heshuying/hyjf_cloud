6c23708e8933378b5c4a35142c5f25ab12683dcd
tanyangyang <591628420@qq.com>
2018-07-26 17:02:34 +0800

    运营报告第二期代码迁移

--- hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/vo/trade/OperationReportJobVO.java
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/vo/trade/OperationReportJobVO.java
@@ -14,7 +14,12 @@ public class OperationReportJobVO extends BaseVO implements Serializable {
 	public String name;
 	public int count;
 	public int sex;
-
+	public String title;//标题
+	public BigDecimal sumAccount;//总金额
+	public String dealMonth;//交易月份
+	public Integer dealSum;//成交笔数
+	public Integer userId;//用户id
+	public String userName;//用户名
 	public int getCitycode() {
 		return citycode;
 	}
@@ -39,5 +44,53 @@ public class OperationReportJobVO extends BaseVO implements Serializable {
 	}
 	public void setSex(int sex) {
 		this.sex = sex;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public BigDecimal getSumAccount() {
+		return sumAccount;
+	}
+
+	public void setSumAccount(BigDecimal sumAccount) {
+		this.sumAccount = sumAccount;
+	}
+
+	public String getDealMonth() {
+		return dealMonth;
+	}
+
+	public void setDealMonth(String dealMonth) {
+		this.dealMonth = dealMonth;
+	}
+
+	public Integer getDealSum() {
+		return dealSum;
+	}
+
+	public void setDealSum(Integer dealSum) {
+		this.dealSum = dealSum;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 }
