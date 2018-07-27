b2b89901c1c7e99644ce30da6989dfaf8c9abc42
jun <jijunjun@hyjf.com>
2018-07-27 10:58:02 +0800

    汇计划投资记录

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/vo/trade/htj/DebtPlanAccedeCustomizeVO.java
@@ -0,0 +1,104 @@
+/**
+ * Description:计划加入明细
+ * Copyright: Copyright (HYJF Corporation) 2015
+ * Company: HYJF Corporation
+ * @author: 王坤
+ * @version: 1.0
+ * Created at: 2015年11月11日 下午2:17:31
+ * Modification History:
+ * Modified by : 
+ */
+	
+package com.hyjf.am.vo.trade.htj;
+
+import com.hyjf.am.vo.BaseVO;
+
+import java.io.Serializable;
+
+/**
+ * @author jijun
+ */
+
+public class DebtPlanAccedeCustomizeVO extends BaseVO implements Serializable {
+
+	/**序列化id */
+	private static final long serialVersionUID = 6315391112691033945L;
+	//用户id
+	public String userId;
+	//vip等级
+    private String vipId;
+    //vip等级
+    private String vipLevel;
+	//用户名
+	public String userName;
+	//加入金额
+	public String accedeAccount;
+	//加入时间
+	public String accedeTime;
+	//加入平台
+	public String client;
+	/** 构造方法*/
+	public DebtPlanAccedeCustomizeVO() {
+		super();
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getVipId() {
+		return vipId;
+	}
+
+	public void setVipId(String vipId) {
+		this.vipId = vipId;
+	}
+
+	public String getVipLevel() {
+		return vipLevel;
+	}
+
+	public void setVipLevel(String vipLevel) {
+		this.vipLevel = vipLevel;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getAccedeAccount() {
+		return accedeAccount;
+	}
+
+	public void setAccedeAccount(String accedeAccount) {
+		this.accedeAccount = accedeAccount;
+	}
+
+	public String getAccedeTime() {
+		return accedeTime;
+	}
+
+	public void setAccedeTime(String accedeTime) {
+		this.accedeTime = accedeTime;
+	}
+
+	public String getClient() {
+		return client;
+	}
+
+	public void setClient(String client) {
+		this.client = client;
+	}
+
+
+}
+
+	
