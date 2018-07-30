f94d4ce5b931f57add256883b963b49151bf64b7
fuqiang <18251359382@163.com>
2018-07-30 09:59:21 +0800

    app短信充值发送验证码

---
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/service/recharge/AppRechargeService.java
@@ -0,0 +1,13 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.cs.user.service.recharge;
+
+import com.hyjf.cs.user.service.BaseUserService;
+
+/**
+ * @author fq
+ * @version AppRechargeService, v0.1 2018/7/30 9:45
+ */
+public interface AppRechargeService extends BaseUserService {
+}
