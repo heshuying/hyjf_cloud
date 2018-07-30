92c35e7fe89449ad92f1c0ff26d2ef742e813ace
pangchengchao <pangchengchao@hyjf.com>
2018-07-30 15:28:01 +0800

    微服务微信接口修改

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/wechat/user/myAsset/WechatMyProjectController.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/wechat/user/myAsset/WechatMyProjectController.java
@@ -54,11 +54,10 @@ public class WechatMyProjectController extends BaseTradeController {
     @ApiOperation(value = "微信端:获取我的散标信息", notes = "微信端:获取我的散标信息")
     @PostMapping(value = "/queryScatteredProject.do", produces = "application/json; charset=utf-8")
     public WeChatResult<QueryMyProjectVO> queryScatteredProject( HttpServletRequest request,
-                                                                 @RequestHeader(value = "token", required = false) String token,
-                                                                 @RequestHeader(value = "type", required = false) String type) {
+                                                                 @RequestHeader(value = "token", required = false) String token) {
 
         WeChatResult weChatResult = new WeChatResult();
-
+        String type = request.getParameter("type");
         WebViewUserVO webViewUserVO = RedisUtils.getObj(RedisConstants.USER_TOKEN_REDIS + token, WebViewUserVO.class);
         if (Strings.isNullOrEmpty(type)) {
             return new WeChatResult(MsgEnum.ERR_PARAM_NUM);
