14a1ae0e11ec3621c5a21014febf68853aa4146b
tanyangyang <591628420@qq.com>
2018-07-31 16:58:26 +0800

    运营报告第二期代码迁移

--- hyjf_am_server/hyjf_am_core/hyjf_am_config/src/main/java/com/hyjf/am/config/controller/admin/content/IdCardController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_config/src/main/java/com/hyjf/am/config/controller/admin/content/IdCardController.java
@@ -63,14 +63,14 @@ public class IdCardController extends BaseConfigController {
     /**
      * @Author walter.tanyy
      * @user walter.tanyy
-     * @Description  查询身份证对应的地区配置表
+     * @Description  查询分组信息
      * @Date 14:59 2018/7/24
-     * @Param idCardCustomize
+     * @Param request
      * @return
      */
     @RequestMapping("/tendercitygroupby")
     public OperationReportJobResponse tenderCityGroupBy(@RequestBody OperationReportJobRequest request) {
-        logger.info("查询身份证对应的地区配置表......");
+        logger.info("查询分组信息......");
         OperationReportJobResponse response = new OperationReportJobResponse();
         List<OperationReportJobVO> list = idCardService.getTenderCityGroupBy(request.getOperationReportJobVOList());
         response.setResultList(list);
