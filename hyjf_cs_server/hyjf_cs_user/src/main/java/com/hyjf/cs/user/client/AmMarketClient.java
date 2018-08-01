488ce3cbf045afefc23a6aabcd820f3dca9a8810
jun <jijunjun@hyjf.com>
2018-08-01 20:43:39 +0800

    优惠券用户列表bug修复

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/client/AmMarketClient.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/client/AmMarketClient.java
@@ -5,6 +5,8 @@ import com.hyjf.am.vo.market.AdsVO;
 import com.hyjf.am.vo.market.AppAdsCustomizeVO;
 import com.hyjf.am.vo.user.ActivityListVO;
 
+import java.util.List;
+
 /**
  * @author xiasq
  * @version AmMarketClient, v0.1 2018/5/14 16:11
@@ -20,4 +22,6 @@ public interface AmMarketClient {
     ActivityListVO selectActivityList(int activityId);
 
     AppAdsCustomizeVO searchBanner(AdsRequest adsRequest);
+
+    List<com.hyjf.am.vo.market.ActivityListVO> getActivityList();
 }
