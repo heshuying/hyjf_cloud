257ba673f1f4a111e78df7f25cff679620234b65
zhangqingqing <zhangqingqing@hyjf.com>
2018-08-01 10:59:44 +0800

    修改异步回掉路径

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/controller/app/password/AppPassWordController.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/controller/app/password/AppPassWordController.java
@@ -48,7 +48,7 @@ import java.util.regex.Pattern;
 @Api(value = "app端密码相关服务",description = "app端-密码相关服务")
 @Controller
 @RestController
-@RequestMapping("/hyjf-app")
+@RequestMapping("/")
 public class AppPassWordController {
 
     @Autowired
@@ -71,7 +71,7 @@ public class AppPassWordController {
      */
     @ApiOperation(value = "修改登陆密码", notes = "修改登陆密码")
     @ResponseBody
-    @RequestMapping(value = "/appUser/updatePasswordAction", method = RequestMethod.POST)
+    @RequestMapping(value = "/hyjf-app/appUser/updatePasswordAction", method = RequestMethod.POST)
     public JSONObject updatePasswordAction(@RequestHeader(value = "key") String key, @RequestHeader(value = "userId") Integer userId, HttpServletRequest request) {
 
         JSONObject ret = new JSONObject();
@@ -125,7 +125,7 @@ public class AppPassWordController {
      * @return
      */
     @ApiOperation(value = "设置交易密码", notes = "设置交易密码")
-    @PostMapping(value = "/setTeaderPassword")
+    @PostMapping(value = "/bank/user/transpassword/setPassword.do")
     public ModelAndView setPassword(@RequestHeader(value = "token") String token,@RequestHeader(value = "sign") String sign,HttpServletRequest request) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView = new ModelAndView("/jumpHTML");
@@ -141,10 +141,10 @@ public class AppPassWordController {
         BankOpenAccountVO bankOpenAccount = passWordService.getBankOpenAccount(userId);
         // 同步调用路径
         String retUrl = systemConfig.getAppHost() + request.getContextPath() +  CommonConstant.REQUEST_MAPPING
-                + CommonConstant.RETURL_SYN_PASSWORD_ACTION ;
+                + CommonConstant.RETURL_SYN_PASSWORD_ACTION+ ".do" ;
         // 异步调用路
         String bgRetUrl = systemConfig.getAppHost() + request.getContextPath() +  CommonConstant.REQUEST_MAPPING
-                + CommonConstant.RETURN_ASY_PASSWORD_ACTION ;
+                + CommonConstant.RETURN_ASY_PASSWORD_ACTION+ ".do" ;
         // 调用设置密码接口
         BankCallBean bean = new BankCallBean();
         bean.setTxCode(BankCallConstant.TXCODE_PASSWORD_SET);
@@ -192,7 +192,7 @@ public class AppPassWordController {
      * @param response
      * @return
      */
-    @RequestMapping("/passwordReturn")
+    @RequestMapping("/bank/user/transpassword/passwordReturn.do")
     public ModelAndView passwordReturn(HttpServletRequest request, HttpServletResponse response,
                                        @ModelAttribute BankCallBean bean) {
         ModelAndView modelAndView = new ModelAndView();
@@ -255,14 +255,13 @@ public class AppPassWordController {
     /**
      * 设置交易密码异步回调
      *
-     * @param request
-     * @param response
+     * @param
+     * @param
      * @return
      */
     @ResponseBody
-    @RequestMapping(value = "passwordBgreturn")
-    public String passwordBgreturn(HttpServletRequest request, HttpServletResponse response,
-                                   @ModelAttribute BankCallBean bean) {
+    @RequestMapping(value = "/bank/user/transpassword/passwordBgreturn.do")
+    public String passwordBgreturn(@ModelAttribute BankCallBean bean) {
         BankCallResult result = new BankCallResult();
         bean.convert();
         LogAcqResBean acqes = bean.getLogAcqResBean();
@@ -291,7 +290,7 @@ public class AppPassWordController {
      * @param response
      * @return
      */
-    @RequestMapping(value = "/resetPassword")
+    @RequestMapping(value = "/bank/user/transpassword/resetPassword.do")
     public ModelAndView resetPassword(@RequestHeader(value = "token") String token,@RequestHeader(value = "sign") String sign,HttpServletRequest request, HttpServletResponse response) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView = new ModelAndView("/jumpHTML");
@@ -307,10 +306,10 @@ public class AppPassWordController {
         UserInfoVO usersInfo= passWordService.getUserInfo(userId);
         // 同步调用路径
         String retUrl = systemConfig.getAppHost() + request.getContextPath() +  CommonConstant.REQUEST_MAPPING
-                + CommonConstant.RETURL_SYN_RESETPASSWORD_ACTION ;
+                + CommonConstant.RETURL_SYN_RESETPASSWORD_ACTION + ".do";
         // 异步调用路
         String bgRetUrl = systemConfig.getAppHost() + request.getContextPath() +  CommonConstant.REQUEST_MAPPING
-                + CommonConstant.RETURN_ASY_RESETPASSWORD_ACTION ;
+                + CommonConstant.RETURN_ASY_RESETPASSWORD_ACTION+ ".do" ;
         // 调用设置密码接口
         BankCallBean bean = new BankCallBean();
         bean.setTxCode(BankCallConstant.TXCODE_PASSWORD_RESET);
@@ -359,7 +358,7 @@ public class AppPassWordController {
      * @param response
      * @return
      */
-    @RequestMapping(value = "/resetPasswordReturn")
+    @RequestMapping(value = "/bank/user/transpassword/resetPasswordReturn.do")
     public ModelAndView resetPasswordReturn(HttpServletRequest request, HttpServletResponse response,
                                             @ModelAttribute BankCallBean bean) {
 
@@ -391,7 +390,7 @@ public class AppPassWordController {
      * @return
      */
     @ResponseBody
-    @RequestMapping(value = "resetPasswordBgreturn")
+    @RequestMapping(value = "/bank/user/transpassword/resetPasswordBgreturn.do")
     public String resetPasswordBgreturn(HttpServletRequest request, HttpServletResponse response,
                                         @ModelAttribute BankCallBean bean) {
         BankCallResult result = new BankCallResult();
@@ -410,7 +409,7 @@ public class AppPassWordController {
      */
     @ResponseBody
     @ApiOperation(value = "找回密码",notes = "找回密码")
-    @RequestMapping(value = "/appUser/getBackPasswordAction", method = RequestMethod.POST)
+    @RequestMapping(value = "/hyjf-app/appUser/getBackPasswordAction", method = RequestMethod.POST)
     public JSONObject getBackPasswordAction(@RequestHeader(value = "key") String key,HttpServletRequest request, HttpServletResponse response) {
         JSONObject ret = new JSONObject();
         ret.put("request", "/hyjf-app/appUser/getBackPasswordAction");
