c4d5890a274ed092c05e44d8bcd491ddb1695628
xiashuqing <xiashuqing@shizitegong.com>
2018-08-01 12:08:13 +0800

    处理form-data方式提交数据

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/controller/app/login/AppLoginController.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/controller/app/login/AppLoginController.java
@@ -247,7 +247,7 @@ public class AppLoginController extends BaseUserController {
     }
 
     /**
-     * 上传头像
+     * 上传头像 - 使用form-data提交方式的类似这个方法处理，网关有特殊处理
      *
      * @param request
      * @param response
@@ -257,66 +257,19 @@ public class AppLoginController extends BaseUserController {
     @ResponseBody
     @PostMapping(value = "/uploadAvatarAction")
     @ApiOperation(value = "上传头像",notes = "上传头像")
-    public JSONObject uploadAvatarAction(HttpServletRequest request, HttpServletResponse response) {
+    public JSONObject uploadAvatarAction(@RequestParam String version,
+                                         @RequestParam String netStatus,
+                                         @RequestParam String platform,
+                                         @RequestParam String sign,
+                                         @RequestParam String randomString,
+                                         @RequestParam String token,
+                                         @RequestParam String order,
+                                         @RequestParam(value = "iconImg", required = false) MultipartFile iconImg) {
         JSONObject ret = new JSONObject();
         ret.put("request", "/appUser/uploadAvatarAction");
 
-        // 转型为MultipartHttpRequest(重点的所在)
-        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
-        MultipartHttpServletRequest multipartRequest = commonsMultipartResolver.resolveMultipart(request);
-
-        // 版本号
-        String version = multipartRequest.getParameter("version");
-        // 网络状态
-        String netStatus = multipartRequest.getParameter("netStatus");
-        // 平台
-        String platform = multipartRequest.getParameter("platform");
-        // token
-        String token = multipartRequest.getParameter("token");
-        // 唯一标识
-        String sign = multipartRequest.getParameter("sign");
-        // 随机字符串
-        String randomString = multipartRequest.getParameter("randomString");
-        // Order
-        String order = multipartRequest.getParameter("order");
-        // 获得第1张图片（根据前台的name名称得到上传的文件）
-        MultipartFile iconImg = multipartRequest.getFile("iconImg");
-
-        // 检查参数正确性
-        if (Validator.isNull(version) || Validator.isNull(netStatus) || Validator.isNull(platform) || Validator.isNull(token) || Validator.isNull(sign) || Validator.isNull(randomString) || Validator.isNull(order)) {
-            ret.put("status", "1");
-            ret.put("statusDesc", "请求参数非法");
-            return ret;
-        }
-        // 验证sign
-        if (RedisUtils.get(RedisConstants.SIGN+sign) == null) {
-            ret.put("status", "1");
-            ret.put("statusDesc", "请求参数非法");
-            return ret;
-        }
-
-        // 验证key
-        String key = SecretUtil.getKey(sign);
-        if (Validator.isNull(key)) {
-            ret.put("status", "1");
-            ret.put("statusDesc", "请求参数非法");
-            return ret;
-        }
-
-        // 验证Order
-        if (!SecretUtil.checkOrder(key, token, randomString, order)) {
-            ret.put("status", "1");
-            ret.put("statusDesc", "请求参数非法");
-            return ret;
-        }
-
         // 业务逻辑
         try {
-            if (Validator.isNull(iconImg)) {
-                ret.put("status", "1");
-                ret.put("statusDesc", "上传图片不能为空");
-                return ret;
-            }
             // 单位字节
             Long allowFileLength = 5000000L;
 
