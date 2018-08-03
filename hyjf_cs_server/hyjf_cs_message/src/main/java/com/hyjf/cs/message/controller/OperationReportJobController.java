762ad281805aa30aca8b8be797fe75ed4a0c610f
xiehuili <xiehuili@shizitegong.com>
2018-08-03 17:08:09 +0800

    测试bug修改

--- hyjf_cs_server/hyjf_cs_message/src/main/java/com/hyjf/cs/message/controller/OperationReportJobController.java
+++ hyjf_cs_server/hyjf_cs_message/src/main/java/com/hyjf/cs/message/controller/OperationReportJobController.java
@@ -3,7 +3,6 @@
  */
 package com.hyjf.cs.message.controller;
 
-import com.hyjf.common.cache.RedisConstants;
 import com.hyjf.common.cache.RedisUtils;
 import com.hyjf.common.util.GetDate;
 import com.hyjf.cs.common.controller.BaseController;
@@ -34,10 +33,10 @@ public class OperationReportJobController extends BaseController {
 	@RequestMapping("/countOperationReport")
 	public void countOperationReport() {
 		logger.info("开始 从数据库获取运营报告的数据...");
-		boolean flag = RedisUtils.tranactionSet(RedisConstants.Statistics_Operation_Report);
-		if(!flag){
-			return;
-		}
+//		boolean flag = RedisUtils.tranactionSet(RedisConstants.Statistics_Operation_Report);
+//		if(!flag){
+//			return;
+//		}
 		Calendar cal = Calendar.getInstance();
 
 		try {
