dde8a38b8fb0ff81f9ae8bdc72839c267ed1bfff
zhangqingqing <zhangqingqing@hyjf.com>
2018-08-06 15:49:05 +0800

    规范代码

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/finance/bankaleve/BankAleveController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/finance/bankaleve/BankAleveController.java
@@ -32,7 +32,7 @@ import java.util.List;
  * 资金中心->银行账务明细
  */
 
-@Api(value = "资金中心->银行账务明细",description = "资金中心->银行账务明细")
+@Api(value = "资金中心->银行账务明细",tags = "资金中心->银行账务明细")
 @RestController
 @RequestMapping("/hyjf-admin/bankaleve")
 public class BankAleveController {
