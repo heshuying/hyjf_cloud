b89d68dacd14b46ab9714ef3ee5cdc8f61fcfeae
yinhui <yinhui@shizitegong.com>
2018-08-08 11:21:06 +0800

    数据中心-汇计划统计

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/dao/mapper/customize/admin/HjhAccountBalanceCustomizeMapper.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/dao/mapper/customize/admin/HjhAccountBalanceCustomizeMapper.java
@@ -3,7 +3,6 @@ package com.hyjf.am.trade.dao.mapper.customize.admin;
 import com.hyjf.am.resquest.admin.HjhAccountBalanceRequest;
 import com.hyjf.am.vo.admin.HjhAccountBalanceVO;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -11,20 +10,6 @@ import java.util.List;
  * @Date: 2018/8/7  11:51
  */
 public interface HjhAccountBalanceCustomizeMapper {
-
-    /**
-     * 按日查询总计
-     *
-     * @return
-     */
-    HjhAccountBalanceVO getHjhAccountBalanceSum(HjhAccountBalanceRequest request);
-
-    /**
-     * 按月查询总计
-     *
-     * @return
-     */
-    HjhAccountBalanceVO getHjhAccountBalanceMonthSum(HjhAccountBalanceRequest request);
 
     /**
      * 按日统计查询数量
@@ -46,14 +31,6 @@ public interface HjhAccountBalanceCustomizeMapper {
     List<HjhAccountBalanceVO> getHjhAccountBalanceList(HjhAccountBalanceRequest entity);
 
     /**
-     * 获取该日期的实际债转和复投金额
-     *
-     * @param date
-     * @return
-     */
-    List<HjhAccountBalanceVO> selectHjhAccountBalanceForActList(Date date);
-
-    /**
      * 按月统计查询数据
      *
      * @return
@@ -62,6 +39,4 @@ public interface HjhAccountBalanceCustomizeMapper {
 
     int getHjhAccountBalanceMonthCountNew(HjhAccountBalanceRequest request);
 
-    List<HjhAccountBalanceVO> getHjhAccountBalanceMonthListNew(
-            HjhAccountBalanceRequest request);
 }
