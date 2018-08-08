8e0f775753e054ff8601745f28389b9efece5853
zhangqingqing <zhangqingqing@hyjf.com>
2018-08-08 10:28:18 +0800

    am-trade分包整理

---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/admin/borrow/BorrowFirstController.java
@@ -0,0 +1,100 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.trade.controller.admin.borrow;
+
+import com.hyjf.am.response.admin.BorrowFirstCustomizeResponse;
+import com.hyjf.am.resquest.admin.BorrowFireRequest;
+import com.hyjf.am.resquest.admin.BorrowFirstRequest;
+import com.hyjf.am.trade.dao.model.customize.admin.BorrowFirstCustomize;
+import com.hyjf.am.trade.service.admin.BorrowFirstService;
+import com.hyjf.am.vo.admin.BorrowFirstCustomizeVO;
+import com.hyjf.common.util.CommonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * @author wangjun
+ * @version BorrowFirstController, v0.1 2018/7/3 15:56
+ */
+@RestController
+@RequestMapping("/am-trade/borrow_first")
+public class BorrowFirstController {
+    @Autowired
+    BorrowFirstService borrowFirstService;
+
+    /**
+     * 获取借款初审列表count
+     *
+     * @return
+     */
+    @RequestMapping("/count_borrow_first")
+    public BorrowFirstCustomizeResponse countBorrowFirst(@RequestBody @Valid BorrowFirstRequest borrowFirstRequest) {
+        BorrowFirstCustomizeResponse response = new BorrowFirstCustomizeResponse();
+        int count = borrowFirstService.countBorrowFirst(borrowFirstRequest);
+        response.setTotal(count);
+        return response;
+    }
+
+    /**
+     * 获取借款初审列表
+     *
+     * @return
+     */
+    @RequestMapping("/select_borrow_first_list")
+    public BorrowFirstCustomizeResponse selectBorrowFirstList(@RequestBody @Valid BorrowFirstRequest borrowFirstRequest) {
+        BorrowFirstCustomizeResponse response = new BorrowFirstCustomizeResponse();
+        List<BorrowFirstCustomize> borrowRegistCustomizeList = borrowFirstService.selectBorrowFirstList(borrowFirstRequest);
+        if (!CollectionUtils.isEmpty(borrowRegistCustomizeList)) {
+            List<BorrowFirstCustomizeVO> voList = CommonUtils.convertBeanList(borrowRegistCustomizeList, BorrowFirstCustomizeVO.class);
+            response.setResultList(voList);
+        }
+        return response;
+    }
+
+    /**
+     * 获取借款初审列表合计
+     *
+     * @return
+     */
+    @RequestMapping("/sum_borrow_first_account")
+    public BorrowFirstCustomizeResponse sumBorrowFirstAccount(@RequestBody @Valid BorrowFirstRequest borrowFirstRequest) {
+        BorrowFirstCustomizeResponse response = new BorrowFirstCustomizeResponse();
+        String sumAccount = borrowFirstService.getSumBorrowFirstAccount(borrowFirstRequest);
+        response.setSumAccount(sumAccount);
+        return response;
+    }
+
+    /**
+     * 交保证金
+     *
+     * @return
+     */
+    @RequestMapping("/insert_borrow_bail/{borrowNid}/{currUserId}")
+    public BorrowFirstCustomizeResponse insertBorrowBail(@PathVariable String borrowNid, @PathVariable String currUserId) {
+        BorrowFirstCustomizeResponse response = new BorrowFirstCustomizeResponse();
+        boolean flag = borrowFirstService.insertBorrowBail(borrowNid, currUserId);
+        response.setFlag(flag);
+        return response;
+    }
+
+    /**
+     * 更新-发标
+     *
+     * @param borrowFireRequest
+     */
+    @RequestMapping("/update_ontime_record")
+    public BorrowFirstCustomizeResponse updateOntimeRecord(@RequestBody @Valid BorrowFireRequest borrowFireRequest) {
+        BorrowFirstCustomizeResponse response = new BorrowFirstCustomizeResponse();
+        boolean flag = borrowFirstService.updateOntimeRecord(borrowFireRequest);
+        response.setFlag(flag);
+        return response;
+    }
+}
---
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/admin/borrow/BorrowFlowController.java
@@ -0,0 +1,171 @@
+package com.hyjf.am.trade.controller.admin.borrow;
+
+import com.alibaba.fastjson.JSONObject;
+import com.hyjf.am.response.Response;
+import com.hyjf.am.response.admin.AdminBorrowFlowResponse;
+import com.hyjf.am.resquest.admin.AdminBorrowFlowRequest;
+import com.hyjf.am.trade.dao.model.auto.HjhAssetBorrowtype;
+import com.hyjf.am.trade.service.admin.BorrowFlowService;
+import com.hyjf.am.vo.admin.HjhAssetTypeVO;
+import com.hyjf.am.vo.trade.borrow.BorrowProjectTypeVO;
+import com.hyjf.am.vo.trade.hjh.HjhAssetBorrowTypeVO;
+import com.hyjf.am.vo.user.HjhInstConfigVO;
+import com.hyjf.common.paginator.Paginator;
+import com.hyjf.common.util.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author by xiehuili on 2018/7/30.
+ */
+@RestController
+@RequestMapping("/am-trade/config/borrowflow")
+public class BorrowFlowController {
+
+    @Autowired
+    private BorrowFlowService borrowFlowService;
+    private static Logger logger = LoggerFactory.getLogger(BorrowFlowService.class);
+    /**
+     * 项目类型
+     * @return
+     */
+    @RequestMapping("/borrowProjectTypeList/{borrowTypeCd}")
+    public List<BorrowProjectTypeVO> selectBorrowProjectTypeList(@PathVariable String borrowTypeCd) {
+        logger.info("项目类型..." + JSONObject.toJSON(borrowTypeCd));
+        return borrowFlowService.selectBorrowProjectTypeList(borrowTypeCd);
+    }
+    /**
+     * 资金来源
+     * @return
+     */
+    @RequestMapping("/hjhInstConfigList")
+    public List<HjhInstConfigVO> hjhInstConfigList(){
+        return borrowFlowService.hjhInstConfigList("");
+    }
+
+    /**
+     * 根据表的类型,期数,项目类型检索管理费件数
+     * @param instCode assetType
+     * @return
+     */
+    @RequestMapping("/countRecordByPK/{instCode}/{assetType}")
+    public int countRecordByPK(@PathVariable String instCode, @PathVariable Integer assetType){
+        return borrowFlowService.countRecordByPK(instCode,assetType);
+    }
+    /**
+     * 根据资金来源查询产品类型
+     * @param instCode
+     * @return
+     */
+    @RequestMapping("/hjhAssetTypeList/{instCode}")
+    public List<HjhAssetTypeVO> hjhAssetTypeList(@PathVariable String instCode){
+        return borrowFlowService.hjhAssetTypeList(instCode);
+    }
+    /**
+     * 分页查询
+     * @param adminRequest
+     * @return
+     */
+    @RequestMapping("/selectBorrowFlowList")
+    public AdminBorrowFlowResponse selectBorrowFlowList(@RequestBody  AdminBorrowFlowRequest adminRequest){
+        AdminBorrowFlowResponse response = new AdminBorrowFlowResponse();
+        int total = this.borrowFlowService.countRecord(adminRequest);
+        if (total > 0) {
+            Paginator paginator = new Paginator(adminRequest.getCurrPage(), total);
+            List<HjhAssetBorrowtype> recordList =
+                    this.borrowFlowService.getRecordList(adminRequest, paginator.getOffset(), paginator.getLimit());
+            if(!CollectionUtils.isEmpty(recordList)){
+                List<HjhAssetBorrowTypeVO>  hjhAssetBorrowTypeVOS = CommonUtils.convertBeanList(recordList,HjhAssetBorrowTypeVO.class);
+                response.setResultList(hjhAssetBorrowTypeVOS);
+                response.setRtn(Response.SUCCESS);
+                return response;
+            }
+            return null;
+        }
+        return null;
+    }
+    /**
+     *详情
+     * @param adminRequest
+     * @return
+     */
+    @RequestMapping("/info")
+    public AdminBorrowFlowResponse selectBorrowFlowInfo(@RequestBody  AdminBorrowFlowRequest adminRequest){
+        logger.info("分账名单详情页面..." + JSONObject.toJSON(adminRequest));
+        AdminBorrowFlowResponse result=new AdminBorrowFlowResponse();
+        if (adminRequest.getId() != null) {
+            HjhAssetBorrowtype record = this.borrowFlowService.selectBorrowFlowInfo(adminRequest.getId());
+            HjhAssetBorrowTypeVO recordVo = new HjhAssetBorrowTypeVO();
+            if(null != record){
+                BeanUtils.copyProperties(record, recordVo);
+                result.setResult(recordVo);
+                result.setRtn(Response.SUCCESS);
+            }
+            return result;
+        }
+        return null;
+    }
+    /**
+     * 添加
+     * @param adminRequest
+     * @return
+     */
+    @RequestMapping("/insertRecord")
+    public AdminBorrowFlowResponse insertRecord(@RequestBody  AdminBorrowFlowRequest adminRequest){
+        AdminBorrowFlowResponse result=new AdminBorrowFlowResponse();
+        // 插入
+       int cou= this.borrowFlowService.insertRecord(adminRequest);
+       if(cou > 0){
+           result.setRtn(Response.SUCCESS);
+           return result;
+       }
+        result.setRtn(Response.FAIL);
+        return result;
+    }
+    /**
+     * 修改
+     * @param adminRequest
+     * @return
+     */
+    @RequestMapping("/updateRecord")
+    public AdminBorrowFlowResponse updateRecord(@RequestBody  AdminBorrowFlowRequest adminRequest){
+        AdminBorrowFlowResponse result = new AdminBorrowFlowResponse();
+            // 修改
+       int cou= this.borrowFlowService.updateRecord(adminRequest);
+        if(cou > 0){
+            result.setRtn(Response.SUCCESS);
+            return result;
+        }
+        result.setRtn(Response.FAIL);
+        return result;
+    }
+    /**
+     * 删除
+     * @param adminRequest
+     * @return
+     */
+    @RequestMapping("/deleteRecord")
+    public AdminBorrowFlowResponse deleteRecord(@RequestBody  AdminBorrowFlowRequest adminRequest){
+        AdminBorrowFlowResponse resp = new AdminBorrowFlowResponse();
+        if(adminRequest.getId() != null){
+            int cou= this.borrowFlowService.deleteRecord(adminRequest.getId());
+            if(cou > 0){
+                resp.setRtn(Response.SUCCESS);
+                return resp;
+            }
+            resp.setRtn(Response.FAIL);
+            return resp;
+        }
+        resp.setRtn(Response.FAIL);
+        return resp;
+    }
+}
