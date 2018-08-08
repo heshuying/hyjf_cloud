d0fa348a0ff45a77ab6cbcfd4a2025f8909056e6
nixiaoling <nixiaoling@hyjf.com>
2018-08-08 15:23:02 +0800

    用户测评代码修改

---
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/beans/vo/AccountTradeCustomizeVO.java
@@ -0,0 +1,93 @@
+package com.hyjf.admin.beans.vo;
+
+import com.hyjf.am.vo.BaseVO;
+
+import java.io.Serializable;
+
+/**
+ * @author nxl
+ * @version AccountTradeVO, v0.1 2018/6/27 10:19
+ */
+public class AccountTradeCustomizeVO extends BaseVO implements Serializable {
+    private Short id;
+
+    private Integer typeId;
+
+    private String name;
+
+    private String nid;
+
+    private String value;
+
+    private Integer status;
+
+    private Short order;
+
+    private String operation;
+
+    private static final long serialVersionUID = 1L;
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getNid() {
+        return nid;
+    }
+
+    public void setNid(String nid) {
+        this.nid = nid == null ? null : nid.trim();
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value == null ? null : value.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Short getOrder() {
+        return order;
+    }
+
+    public void setOrder(Short order) {
+        this.order = order;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation == null ? null : operation.trim();
+    }
+}
