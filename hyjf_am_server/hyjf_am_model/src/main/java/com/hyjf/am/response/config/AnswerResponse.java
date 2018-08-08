9a277d903b386197a9479a6bee35b87108c9f8af
nixiaoling <nixiaoling@hyjf.com>
2018-08-08 15:21:32 +0800

    用户测评代码修改

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/config/AnswerResponse.java
@@ -0,0 +1,11 @@
+package com.hyjf.am.response.config;
+
+import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.config.AnswerVO;
+
+/**
+ * @author nxl
+ * @version AnswerResponse, v0.1 2018/6/20 10:52
+ */
+public class AnswerResponse extends Response<AnswerVO> {
+}
