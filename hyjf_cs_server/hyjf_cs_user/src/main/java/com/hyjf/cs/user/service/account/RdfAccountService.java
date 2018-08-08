8993ad57713a227fd13f85048f6e666cf9b8df0c
jun <jijunjun@hyjf.com>
2018-08-08 13:18:10 +0800

    添加代码注释

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/service/account/RdfAccountService.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/service/account/RdfAccountService.java
@@ -6,15 +6,30 @@ import java.util.Map;
 import com.hyjf.am.vo.user.BankCardVO;
 import com.hyjf.cs.user.service.BaseUserService;
 /**
- * surong账户相关查询
+ * 融东风用户账户接口
  * @author jun
  *
  */
 public interface RdfAccountService extends BaseUserService{
-	
+
+      /**
+       * 通过手机号获取账户余额
+       * @param mobile
+       * @return
+       */
       public String getBalance(String mobile);
-      
+
+      /**
+       * 通过手机号获取银行卡信息
+       * @param mobile
+       * @return
+       */
       public BankCardVO getBankCard(String mobile);
-      
+
+      /**
+       * 同步余额
+       * @param ids
+       * @return
+       */
       public List<Map<String, String>> balanceSync(List<Integer> ids);
 }
