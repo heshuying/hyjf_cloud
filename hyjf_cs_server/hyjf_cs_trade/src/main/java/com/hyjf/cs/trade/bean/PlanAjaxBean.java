63dce3e3b7e4c7a44131c5b4116aac206e3768ca
pangchengchao <pangchengchao@hyjf.com>
2018-08-10 18:54:17 +0800

    web端我的散标和我的计划修改

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/bean/PlanAjaxBean.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/bean/PlanAjaxBean.java
@@ -6,8 +6,9 @@ import java.util.List;
 import com.hyjf.am.vo.trade.assetmanage.CurrentHoldPlanListCustomizeVO;
 import com.hyjf.am.vo.trade.assetmanage.RepayMentPlanListCustomizeVO;
 import com.hyjf.cs.common.bean.result.WebResult;
+import com.hyjf.cs.common.util.Page;
 
-public class PlanAjaxBean extends WebResult {
+public class PlanAjaxBean {
 
 	private static final long serialVersionUID = 3278149257478770256L;
 	
@@ -19,6 +20,16 @@ public class PlanAjaxBean extends WebResult {
 	private Integer currentHoldPlanCount=0;
 	//已回款计划数量
 	private Integer repayMentPlanCount=0;
+    // 分页对象
+    private Page page;
+
+    public Page getPage() {
+        return page;
+    }
+
+    public void setPage(Page page) {
+        this.page = page;
+    }
 
     public List<CurrentHoldPlanListCustomizeVO> getCurrentHoldPlanList() {
         return currentHoldPlanList;
