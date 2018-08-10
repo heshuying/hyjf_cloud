bd023a4c8c0bc658b79b9fbf79be81b7bf471b65
tanyangyang <591628420@qq.com>
2018-08-10 16:39:32 +0800

    运营报告app拆分

--- hyjf_cs_server/hyjf_cs_message/src/main/java/com/hyjf/cs/message/controller/app/report/AppOperationReportController.java
+++ hyjf_cs_server/hyjf_cs_message/src/main/java/com/hyjf/cs/message/controller/app/report/AppOperationReportController.java
@@ -3,6 +3,7 @@
  */
 package com.hyjf.cs.message.controller.app.report;
 
+import com.alibaba.fastjson.JSONObject;
 import com.hyjf.am.response.message.OperationReportResponse;
 import com.hyjf.am.resquest.message.OperationReportRequest;
 import com.hyjf.am.vo.datacollect.OperationReportVO;
@@ -36,13 +37,13 @@ public class AppOperationReportController extends BaseController {
 
 	@ApiOperation(value = "获取已发布运营报告列表", notes = "获取已发布运营报告列表")
 	@GetMapping("/reportList")
-	public OperationReportResponse listByRelease(HttpServletRequest httpServletRequest) {
+	public JSONObject listByRelease(HttpServletRequest httpServletRequest) {
 		OperationReportRequest request = new OperationReportRequest();
 		String param1 =httpServletRequest.getParameter("isRelease");
 		String param2 =httpServletRequest.getParameter("paginatorPage");
 		request.setIsRelease(param1==null?null:Integer.valueOf(param1));
 		request.setCurrPage(param2==null?0:Integer.valueOf(param2));
-		OperationReportResponse response = operationReportService.getRecordListByReleaseJson(request);
+		JSONObject response = operationReportService.getRecordListByReleaseJson(request);
 		return response;
 
 	}
