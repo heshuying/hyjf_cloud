fc050b4c4794fd8f07c25359eb4f1e0d87e48f23
sunpeikai <spk123456>
2018-08-14 17:52:26 +0800

    银行卡异常

---
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/service/BankCardExceptionService.java
@@ -0,0 +1,40 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.admin.service;
+
+import com.hyjf.am.resquest.admin.BankCardExceptionRequest;
+import com.hyjf.am.vo.admin.AdminBankCardExceptionCustomizeVO;
+import com.hyjf.am.vo.trade.BankConfigVO;
+
+import java.util.List;
+
+/**
+ * @author: sunpeikai
+ * @version: BankCardExceptionService, v0.1 2018/8/14 14:01
+ */
+public interface BankCardExceptionService extends BaseAdminService {
+    /**
+     * 银行卡配置下拉框
+     * @auth sunpeikai
+     * @param
+     * @return
+     */
+    List<BankConfigVO> searchBankConfig();
+
+    /**
+     * 银行卡异常count
+     * @auth sunpeikai
+     * @param
+     * @return
+     */
+    int getBankCardExceptionCount(BankCardExceptionRequest request);
+
+    /**
+     * 银行卡异常列表
+     * @auth sunpeikai
+     * @param
+     * @return
+     */
+    List<AdminBankCardExceptionCustomizeVO> searchBankCardExceptionList(BankCardExceptionRequest request);
+}
