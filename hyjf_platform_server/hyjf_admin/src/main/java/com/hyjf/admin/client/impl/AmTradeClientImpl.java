afdb2ebec306574977caf9bc8201e2a197e9afcf
TonyLee <libin@hyjf.com>
2018-08-14 16:42:37 +0800

    汇计划加入明细发送协议代码修改

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/impl/AmTradeClientImpl.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/client/impl/AmTradeClientImpl.java
@@ -2491,7 +2491,9 @@ public class AmTradeClientImpl implements AmTradeClient {
 
     @Override
     public List<TenderAgreementVO> selectTenderAgreementByNid(String planOrderId) {
-        String url = "http://AM-TRADE/am-trade/tenderagreement/selectTenderAgreementByNid/" + planOrderId;
+        // 原 selectTenderAgreementByNid 方法返回的是 List<CurrentHoldObligatoryRightListCustomizeVO> 不能共用，换新方法
+    	/*String url = "http://AM-TRADE/am-trade/tenderagreement/selectTenderAgreementByNid/" + planOrderId;*/
+    	String url = "http://AM-TRADE/am-trade/tenderagreement/selectTenderAgreementByTenderNid/" + planOrderId;
         TenderAgreementResponse response = restTemplate.getForEntity(url, TenderAgreementResponse.class).getBody();
         if (response != null) {
             return response.getResultList();
