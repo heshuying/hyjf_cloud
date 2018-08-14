33efcd858531824338a184bc8f3a045ad3259f2c
limeng <limeng@shizitegong.com>
2018-08-14 14:51:53 +0800

    优化app获取分享信息方法获取当前登陆用户ID方式

--- hyjf_cs_server/hyjf_cs_market/src/main/java/com/hyjf/cs/market/controller/app/sharenews/ShareNewsController.java
+++ hyjf_cs_server/hyjf_cs_market/src/main/java/com/hyjf/cs/market/controller/app/sharenews/ShareNewsController.java
@@ -10,10 +10,7 @@ import com.hyjf.cs.market.controller.BaseMarketController;
 import com.hyjf.cs.market.service.ShareNewsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -34,44 +31,14 @@ public class ShareNewsController extends BaseMarketController {
 
     @ApiOperation(value = "分享信息", notes = "app-分享信息")
     @PostMapping("/getNews")
-    public ShareNewsResponse init(HttpServletRequest request, @RequestBody ShareNewsRequest shareNewsRequest){
+    public ShareNewsResponse init(@RequestHeader(value = "userId") Integer userId, HttpServletRequest request, @RequestBody ShareNewsRequest shareNewsRequest){
         ShareNewsResponse response = new ShareNewsResponse();
-
-// 检查参数正确性
-        if (Validator.isNull(shareNewsRequest.getVersion()) || Validator.isNull(shareNewsRequest.getNetStatus()) || Validator.isNull(shareNewsRequest.getPlatform())
-                || Validator.isNull(shareNewsRequest.getSign())) {
-            response.setStatus("1");
-            response.setStatusDesc("请求参数非法");
-            return response;
-        }
-
-        // // 判断sign是否存在
-        // boolean isSignExists = SecretUtil.isExists(sign);
-        // if (!isSignExists) {
-        // ret.put("status", "1");
-        // ret.put("statusDesc", "请求参数非法");
-        // return ret;
-        // }
-
-        // 取得加密用的Key
-        String key = SecretUtil.getKey(shareNewsRequest.getSign());
-        if (Validator.isNull(key)) {
-            response.setStatus("1");
-            response.setStatusDesc("请求参数非法");
-            return response;
-        }
 
         ShareNewsBeanVO shareNewsBean = this.shareNewsService.queryShareNews();
         if (shareNewsBean.getTitle() != null) {
-            if (null != SecretUtil.getToken(shareNewsRequest.getSign())) {// 如果用户是登陆状态 拼接用户编号
-                Integer userId = SecretUtil.getUserId(shareNewsRequest.getSign());
-                try {
-                    response.setLinkUrl(systemConfig.getWechatQrcodeUrl().replace("{userId}", userId+""));
-                } catch (Exception e) {
-                    response.setLinkUrl(systemConfig.getWechatQrcodeUrl());
-                }
-
-            } else {
+            try {
+                response.setLinkUrl(systemConfig.getWechatQrcodeUrl().replace("{userId}", userId+""));
+            } catch (Exception e) {
                 response.setLinkUrl(systemConfig.getWechatQrcodeUrl());
             }
             response.setStatus("0");
