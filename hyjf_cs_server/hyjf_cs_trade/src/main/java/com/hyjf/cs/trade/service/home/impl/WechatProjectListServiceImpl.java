3527f92e80b039a07447100079fef9c32b674980
zhangyunkai <zhangyunkai@hyjf.com>
2018-08-16 10:33:55 +0800

    ## 微信接口

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/home/impl/WechatProjectListServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/home/impl/WechatProjectListServiceImpl.java
@@ -335,7 +335,9 @@ public class WechatProjectListServiceImpl implements WechatProjectListService {
                             borrowRepayPlanBean.setTime(borrowRepayPlan.getRepayTime());
                         }
                         borrowRepayPlanBean.setNumber("第" + (i + 1) + "期");
-                        borrowRepayPlanBean.setAccount(DF_FOR_VIEW.format(borrowRepayPlan.getRepayTotal()));
+                        if (StringUtils.isNotBlank(borrowRepayPlan.getRepayTotal())){
+                            borrowRepayPlanBean.setAccount(DF_FOR_VIEW.format(new BigDecimal(borrowRepayPlan.getRepayTotal())));
+                        }
                         repayPlanList.add(borrowRepayPlanBean);
                     }
                 }
