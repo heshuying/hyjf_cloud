2892652f53aeab2c756f8d3eb785e0c673d59685
fuqiang <18251359382@163.com>
2018-08-17 09:52:56 +0800

    修改app接口请求方式

--- hyjf_cs_server/hyjf_cs_message/src/main/java/com/hyjf/cs/message/controller/app/msgpush/MessagePushController.java
+++ hyjf_cs_server/hyjf_cs_message/src/main/java/com/hyjf/cs/message/controller/app/msgpush/MessagePushController.java
@@ -104,7 +104,7 @@ public class MessagePushController extends BaseController {
 	}
 
 	@ApiOperation(value = "获取通知列表", notes = "获取通知列表")
-	@GetMapping("/getMsgListAction")
+	@PostMapping("/getMsgListAction")
 	public JSONObject getMsgListAction(@RequestParam(value = "page", defaultValue = "1") int page,
 			@RequestParam(value = "pageSize", defaultValue = "10") int pageSize, HttpServletRequest request) {
 		JSONObject ret = new JSONObject();
