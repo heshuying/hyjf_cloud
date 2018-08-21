a188ab15c9fda2988d41cb4e295f2f9211ee6847
wangjun <wangjun@hyjf.com>
2018-08-21 11:46:56 +0800

    代码优化

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/bean/MyCreditDetailBean.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/bean/MyCreditDetailBean.java
@@ -6,6 +6,7 @@ package com.hyjf.cs.trade.bean;
 import com.hyjf.am.vo.trade.BorrowCreditVO;
 import com.hyjf.am.vo.trade.assetmanage.TenderCreditAssignedCustomizeVO;
 import com.hyjf.am.vo.trade.assetmanage.TenderCreditAssignedStatisticCustomizeVO;
+import io.swagger.annotations.ApiModelProperty;
 
 import java.util.List;
 
@@ -14,10 +15,13 @@ import java.util.List;
  * @version MyCreditDetailBean, v0.1 2018/8/17 14:49
  */
 public class MyCreditDetailBean {
+    @ApiModelProperty("散标转让记录列表")
     private List<TenderCreditAssignedCustomizeVO> recordList;
 
+    @ApiModelProperty("散标转让记录详情")
     private TenderCreditAssignedStatisticCustomizeVO assignedStatistic;
 
+    @ApiModelProperty("转让标的信息")
     private BorrowCreditVO borrowCredit;
 
     public List<TenderCreditAssignedCustomizeVO> getRecordList() {
