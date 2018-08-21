d8401718844147bec00a2226a5a33e5cb69701cd
TonyLee <libin@hyjf.com>
2018-08-21 10:37:32 +0800

    App 债权转让提交短信发送 和 提交债权转让申请 接口做成

---
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/service/exception/BidApplyQueryService.java
@@ -0,0 +1,14 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.admin.service.exception;
+
+import com.hyjf.admin.service.BaseAdminService;
+
+/**
+ * @author libin
+ * @version BidApplyQueryService.java, v0.1 2018年8月21日 上午9:20:08
+ */
+public interface BidApplyQueryService extends BaseAdminService{
+
+}
