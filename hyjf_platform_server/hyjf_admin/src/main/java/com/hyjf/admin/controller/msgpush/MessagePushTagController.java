8c551f1aa9b8edd37a24cade2ad437b15fe834b4
yaoyong <yaoyong@shizitegong.com>
2018-08-23 11:41:47 +0800

    消息标签管理修改状态及画面校验

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/msgpush/MessagePushTagController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/msgpush/MessagePushTagController.java
@@ -3,7 +3,6 @@
  */
 package com.hyjf.admin.controller.msgpush;
 
-import com.alibaba.fastjson.JSONObject;
 import com.hyjf.admin.common.result.AdminResult;
 import com.hyjf.admin.controller.BaseController;
 import com.hyjf.admin.service.ActivityListService;
@@ -15,8 +14,6 @@ import com.hyjf.am.resquest.config.MessagePushTagRequest;
 import com.hyjf.am.vo.config.AdminSystemVO;
 import com.hyjf.am.vo.config.MessagePushTagVO;
 import com.hyjf.am.vo.config.ParamNameVO;
-import com.hyjf.am.vo.trade.borrow.BorrowCommonImageVO;
-import com.hyjf.common.file.UploadFileUtils;
 import com.hyjf.common.util.CustomConstants;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -24,17 +21,9 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.Serializable;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.LinkedList;
+
 import java.util.List;
 
 /**
@@ -58,7 +47,7 @@ public class MessagePushTagController extends BaseController {
     private ActivityListService activityListService;
 
     @ApiOperation(value = "初始化页面", notes = "标签管理初始化页面")
-    @RequestMapping(value = "/init",method = RequestMethod.POST)
+    @RequestMapping(value = "/init", method = RequestMethod.POST)
     public AdminResult<MessagePushTagResponse> init(@RequestBody MessagePushTagRequest request) {
         MessagePushTagResponse response = messagePushTagService.searchList(request);
         String nameClass = "MSG_PUSH_STATUS";
@@ -76,7 +65,7 @@ public class MessagePushTagController extends BaseController {
 
 
     @ApiOperation(value = "详情页", notes = "详情页")
-    @RequestMapping(value = "/infoAction",method = RequestMethod.GET)
+    @RequestMapping(value = "/infoAction", method = RequestMethod.GET)
     public AdminResult infoAction(@RequestParam Integer id) {
         MessagePushTagResponse response = messagePushTagService.getRecord(id);
         if (response == null) {
@@ -90,11 +79,12 @@ public class MessagePushTagController extends BaseController {
     }
 
     @ApiOperation(value = "添加信息", notes = "添加信息")
-    @RequestMapping(value = "insertAction",method = RequestMethod.POST)
+    @RequestMapping(value = "insertAction", method = RequestMethod.POST)
     public AdminResult insertAction(HttpServletRequest request, @RequestBody MessagePushTagRequest tagRequest) {
         AdminSystemVO user = getUser(request);
         String userName = user.getUsername();
         tagRequest.setCreateUserName(userName);
+        tagRequest.setCreateUserId(Integer.parseInt(user.getId()));
         String message = validatorFieldCheckAudit(tagRequest);
         if (message != null) {
             return new AdminResult<>(FAIL, "校验失败");
@@ -110,11 +100,12 @@ public class MessagePushTagController extends BaseController {
     }
 
     @ApiOperation(value = "修改信息", notes = "修改信息")
-    @RequestMapping(value = "/updateAction",method = RequestMethod.POST)
+    @RequestMapping(value = "/updateAction", method = RequestMethod.POST)
     public AdminResult updateAction(HttpServletRequest request, @RequestBody MessagePushTagRequest tagRequest) {
         AdminSystemVO user = getUser(request);
         String userName = user.getUsername();
-        tagRequest.setCreateUserName(userName);
+        tagRequest.setUpdateUserName(userName);
+        tagRequest.setUpdateUserId(Integer.parseInt(user.getId()));
         String message = validatorFieldCheckAudit(tagRequest);
         if (message != null) {
             return new AdminResult<>(FAIL, "校验失败");
@@ -130,7 +121,7 @@ public class MessagePushTagController extends BaseController {
     }
 
     @ApiOperation(value = "删除信息", notes = "删除信息")
-    @RequestMapping(value = "/deleteAction",method = RequestMethod.GET)
+    @RequestMapping(value = "/deleteAction", method = RequestMethod.GET)
     public AdminResult deleteAction(Integer id) {
         MessagePushTagResponse response = messagePushTagService.deleteAction(id);
         if (response == null) {
@@ -143,34 +134,31 @@ public class MessagePushTagController extends BaseController {
     }
 
     @ApiOperation(value = "修改状态", notes = "修改装态")
-    @RequestMapping(value = "/statusAction",method = RequestMethod.GET)
-    public AdminResult updateStatus(Integer id) {
-        if (id != null) {
-            MessagePushTagResponse response = messagePushTagService.getRecord(id);
-            MessagePushTagVO record = response.getResult();
-            // 新建状态只能启用，启用只能禁用，禁用只能启用
-            if (record.getStatus().intValue() == CustomConstants.MSG_PUSH_STATUS_0) {
-                record.setStatus(CustomConstants.MSG_PUSH_STATUS_1);
-            } else if (record.getStatus() == CustomConstants.MSG_PUSH_STATUS_1) {
-                record.setStatus(CustomConstants.MSG_PUSH_STATUS_2);
-            } else if (record.getStatus() == CustomConstants.MSG_PUSH_STATUS_2) {
-                record.setStatus(CustomConstants.MSG_PUSH_STATUS_1);
-            }
-            MessagePushTagResponse tagResponse = messagePushTagService.updatePushTag(record);
-            if (response == null) {
-                return new AdminResult<>(FAIL, FAIL_DESC);
-            }
-            if (!Response.isSuccess(response)) {
-                return new AdminResult<>(FAIL, response.getMessage());
-            }
-            return new AdminResult<>(response);
+    @RequestMapping(value = "/statusAction", method = RequestMethod.GET)
+    public AdminResult updateStatus(@RequestParam Integer id) {
+        MessagePushTagResponse response = messagePushTagService.getRecord(id);
+        MessagePushTagVO record = response.getResult();
+        // 新建状态只能启用，启用只能禁用，禁用只能启用
+        if (record.getStatus() == CustomConstants.MSG_PUSH_STATUS_0) {
+            record.setStatus(CustomConstants.MSG_PUSH_STATUS_1);
+        } else if (record.getStatus() == CustomConstants.MSG_PUSH_STATUS_1) {
+            record.setStatus(CustomConstants.MSG_PUSH_STATUS_2);
+        } else if (record.getStatus() == CustomConstants.MSG_PUSH_STATUS_2) {
+            record.setStatus(CustomConstants.MSG_PUSH_STATUS_1);
         }
-        return new AdminResult<>(FAIL, FAIL_DESC);
+        MessagePushTagResponse tagResponse = messagePushTagService.updatePushTag(record);
+        if (tagResponse == null) {
+            return new AdminResult<>(FAIL, FAIL_DESC);
+        }
+        if (!Response.isSuccess(tagResponse)) {
+            return new AdminResult<>(FAIL, tagResponse.getMessage());
+        }
+        return new AdminResult<>(tagResponse);
     }
 
 
     @ApiOperation(value = "检查名称唯一性", notes = "检查名称唯一")
-    @RequestMapping(value = "/checkAction",method = RequestMethod.POST)
+    @RequestMapping(value = "/checkAction", method = RequestMethod.POST)
     public AdminResult checkAction(@RequestBody MessagePushTagRequest request) {
         Integer id = request.getId();
         String tagCode = request.getTagCode();
@@ -189,8 +177,8 @@ public class MessagePushTagController extends BaseController {
         return new AdminResult<>(response);
     }
 
-    @ApiOperation(value = "文件上传",notes = "文件上传")
-    @RequestMapping(value = "/uploadFile",method = RequestMethod.POST)
+    @ApiOperation(value = "文件上传", notes = "文件上传")
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
     public AdminResult uploadFile(HttpServletRequest request, HttpServletResponse response) throws Exception {
         try {
             String s = activityListService.uploadFile(request, response);
