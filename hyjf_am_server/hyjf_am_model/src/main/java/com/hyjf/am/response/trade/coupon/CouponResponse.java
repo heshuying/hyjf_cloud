3867981821628a848d7803d71c582feb6c3c14f6
jun <jijunjun@hyjf.com>
2018-08-27 18:36:45 +0800

    添加类型

--- hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/trade/coupon/CouponResponse.java
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/trade/coupon/CouponResponse.java
@@ -1,8 +1,9 @@
 package com.hyjf.am.response.trade.coupon;
 
 import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.trade.coupon.CouponUserForAppCustomizeVO;
 
-public class CouponResponse extends Response {
+public class CouponResponse extends Response<CouponUserForAppCustomizeVO> {
     private Integer totalRecord;
 
     public Integer getTotalRecord() {
