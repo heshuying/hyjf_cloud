b205804586e910c92fca0c98835e98970e85b1f0
huanghui <huanghui@hyjf.com>
2018-08-28 16:48:27 +0800

    计划还款修改

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/admin/hjhplan/HjhRepayService.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/admin/hjhplan/HjhRepayService.java
@@ -5,6 +5,7 @@ import com.hyjf.am.trade.dao.model.auto.HjhRepay;
 import com.hyjf.am.vo.trade.hjh.HjhRepayVO;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 汇计划-订单退出 Service
@@ -14,12 +15,12 @@ public interface HjhRepayService {
 
     /**
      * 获取总数
-     * @param repayRequest
+     * @param params
      * @return
      */
-    Integer getRepayCount(HjhRepayRequest repayRequest);
+    Integer getRepayCount(Map<String, Object> params);
 
-    List<HjhRepayVO> selectByExample(HjhRepayRequest request);
+    List<HjhRepayVO> selectByExample(Map<String, Object> params);
 
     List<HjhRepayVO> selectByAccedeOrderId(String accedeOrderId);
 
