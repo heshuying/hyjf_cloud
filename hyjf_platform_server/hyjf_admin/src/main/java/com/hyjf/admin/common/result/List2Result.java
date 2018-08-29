433bd026c048a38ddcc878487d1bdc9f67d38fe5
liubin <liu'bin>
2018-08-29 17:16:42 +0800

    微服务 admin用提升版通用list2Result做成。

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/common/result/List2Result.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/common/result/List2Result.java
@@ -10,30 +10,41 @@ import java.util.List;
 
 /**
  * @author liubin
- * @version ListResult, v0.1 2018/7/2 19:42
+ * @version List2Result, v0.1 2018/7/2 19:42
  */
-public class ListResult<T> {
+public class List2Result<T, V> {
 
     @ApiModelProperty(value = "列表数据")
     private List<T> list;
-    @ApiModelProperty(value = "数据")
-    private T data;
     @ApiModelProperty(value = "列表count")
     private int count;
     @ApiModelProperty(value = "page")
     private Page page;
+    @ApiModelProperty(value = "列表总计")
+    private T sum;
+    @ApiModelProperty(value = "其他数据")
+    private V data;
 
-    public static <T> ListResult<T> build(List<T> list,int count) {
-        ListResult<T> result = new ListResult<>();
+    public static <T, V> List2Result<T, V> build(List<T> list, int count) {
+        List2Result<T, V> result = new List2Result<>();
         result.setCount(count);
         result.setList(list);
         return result;
     }
 
-    public static <T> ListResult<T> build2(List<T> list, int count, T data) {
-        ListResult<T> result = new ListResult<>();
+    public static <T, V> List2Result<T, V> build(List<T> list, int count, V data) {
+        List2Result<T, V> result = new List2Result<>();
         result.setCount(count);
         result.setList(list);
+        result.setData(data);
+        return result;
+    }
+
+    public static <T, V> List2Result<T, V> build(List<T> list, int count, T sum, V data) {
+        List2Result<T, V> result = new List2Result<>();
+        result.setCount(count);
+        result.setList(list);
+        result.setSum(sum);
         result.setData(data);
         return result;
     }
@@ -54,11 +65,11 @@ public class ListResult<T> {
         this.count = count;
     }
 
-    public T getData() {
+    public V getData() {
         return data;
     }
 
-    public void setData(T data) {
+    public void setData(V data) {
         this.data = data;
     }
 
@@ -70,13 +81,22 @@ public class ListResult<T> {
         this.page = page;
     }
 
+    public T getSum() {
+        return sum;
+    }
+
+    public void setSum(T sum) {
+        this.sum = sum;
+    }
+
     @Override
     public String toString() {
-        return "ListResult{" +
+        return "List2Result{" +
                 "list=" + list +
-                ", data=" + data +
                 ", count=" + count +
                 ", page=" + page +
+                ", sum=" + sum +
+                ", data=" + data +
                 '}';
     }
 }
