70d2eaa37c38166a2da2fc5ad5b0faa4224dff76
sunshuaishuai <sunshuaishuai@hyjf.com>
2018-08-29 15:13:31 +0800

    插入日志表错误

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/invest/impl/BorrowTenderServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/invest/impl/BorrowTenderServiceImpl.java
@@ -110,6 +110,7 @@ public class BorrowTenderServiceImpl extends BaseTradeServiceImpl implements Bor
         Integer userId = loginUser.getUserId();
         logger.info("开始检查散标投资参数,userId:{}", userId);
         request.setUser(loginUser);
+        request.setUserName(loginUser.getUsername());
         // 设置redis 用户正在投资
         String key = RedisConstants.BORROW_TENDER_REPEAT + userId;
         boolean checkTender = RedisUtils.tranactionSet(key, RedisConstants.TENDER_OUT_TIME);
