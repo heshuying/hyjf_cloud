f00d4745895ab702480e5ddd1b6d17188e7a4dfc
liubin <liu'bin>
2018-08-31 14:30:11 +0800

    微服务 aop 日志修改

--- hyjf_cs_server/hyjf_cs_common/src/main/java/com/hyjf/cs/common/aop/AOPHandler.java
+++ hyjf_cs_server/hyjf_cs_common/src/main/java/com/hyjf/cs/common/aop/AOPHandler.java
@@ -15,6 +15,7 @@ import com.hyjf.am.bean.result.BaseResult;
 import com.hyjf.common.exception.CheckException;
 import com.hyjf.cs.common.bean.result.ApiResult;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
@@ -34,137 +35,139 @@ public class AOPHandler {
 
 	private	static final Logger logger = LoggerFactory.getLogger(AOPHandler.class);
 
-	@Pointcut("execution(public * com.hyjf.cs..*Controller.*(..))")
-	public void controllerLog(){}
+//	@Pointcut("execution(public * com.hyjf.cs..*Controller.*(..)")
+//	public void controllerLog(){}
 
 
 	/**
-	 * api用日志处理
+	 * 组合层对外接口通用AOP处理
 	 * @param pjp
 	 * @return
 	 */
-	@Pointcut("controllerLog()")
-	public Object controllerLogAOPHandler(ProceedingJoinPoint pjp) {
+//	@Pointcut("controllerLog()")
+	@Around("(execution(public com.hyjf.am.bean.result.BaseResult+ com.hyjf.cs..*Controller.*(..))) && !(@annotation(org.springframework.web.bind.annotation.ExceptionHandler))")
+	public Object controllerAOPHandler(ProceedingJoinPoint pjp) {
 
 		long startTime = System.currentTimeMillis();
-		BaseResult<?> result;
+		Object result;
 		try {
-			result = (BaseResult<?>) pjp.proceed();
+			result = pjp.proceed();
 			logger.info(pjp.getSignature() + " 执行时间:" + (System.currentTimeMillis() - startTime));
 		}catch(Throwable e) {
-			result = apiExceptionHandler(pjp, e);
+			logger.error("组合层对外接口通用AOP处理异常！", e);
+			return null;
 		}
 		return result;
 	}
-
-	/**
-	 * api用错误处理
-	 * @param pjp
-	 * @param e
-	 * @return
-	 */
-	private BaseResult<?> apiExceptionHandler(ProceedingJoinPoint pjp, Throwable e) {
-
-		BaseResult<?> result = new BaseResult<>();
-
-		// 已知异常
-		if (e instanceof CheckException) {
-			result.setStatus(BaseResult.FAIL);
-			result.setStatusDesc(e.getLocalizedMessage());
-		} else {
-			logger.error(pjp.getSignature() + " 发生异常\r\n" + AOPUtil.getMethodArgs(pjp), e);
-			result.setStatus(BaseResult.ERROR);
-			result.setStatusDesc("接口调用发生异常，请联系服务方。");
-			//  发送邮件或者写到异常文件中
-		}
-		return result;
-	}
-
-	/**
-	 * api用日志处理（返回值加签）
-	 * @param pjp
-	 * @return
-	 */
-
-	@Pointcut("execution(public com.hyjf.cs.common.bean.result.ApiResult *(..))")
-	public Object apiResSignLogAOPHandler(ProceedingJoinPoint pjp) {
-
-		long startTime = System.currentTimeMillis();
-		ApiResult<?> result;
-		try {
-			result = (ApiResult<?>) pjp.proceed();
-			logger.info(pjp.getSignature() + " 执行时间:" + (System.currentTimeMillis() - startTime));
-		}catch(Throwable e) {
-			result = apiResSignExceptionHandler(pjp, e);
-		}
-		return result;
-	}
-
-	/**
-	 * api用错误处理（返回值加签）
-	 * @param pjp
-	 * @param e
-	 * @return
-	 */
-	private ApiResult<?> apiResSignExceptionHandler(ProceedingJoinPoint pjp, Throwable e) {
-
-		ApiResult<?> result = new ApiResult<>();
-
-		// 已知异常
-		if (e instanceof CheckException) {
-			result.setStatus(((CheckException) e).getCode());
-			result.setStatusDesc(e.getLocalizedMessage());
-		} else {
-			logger.error(pjp.getSignature() + " 发生异常\r\n" + AOPUtil.getMethodArgs(pjp), e);
-			result.setStatus(BaseResult.ERROR);
-			result.setStatusDesc("接口调用发生异常，请联系服务方。");
-			//  发送邮件或者写到异常文件中
-		}
-		return result;
-	}
-
-	/**
-	 * webApi用日志处理
-	 * @param pjp
-	 * @return
-	 * @throws Throwable
-	 */
-	@Pointcut("execution(public com.hyjf.cs.common.bean.result.WebResult *(..))")
-	public Object webApiLogAOPHandler(ProceedingJoinPoint pjp) throws Throwable {
-
-		long startTime = System.currentTimeMillis();
-		ModelAndView result = null;
-		try {
-			result = (ModelAndView) pjp.proceed();
-			logger.info(pjp.getSignature() + " 执行时间:" + (System.currentTimeMillis() - startTime));
-		}catch(Throwable e) {
-			result = webApiExceptionHandler(pjp, e);
-		}
-		return result;
-	}
-
-	/**
-	 * webApi用错误日志处理
-	 * @param pjp
-	 * @param e
-	 * @return
-	 * @throws Throwable
-	 */
-	private ModelAndView webApiExceptionHandler(ProceedingJoinPoint pjp, Throwable e) throws Throwable {
-
-//		ModelAndView result = new ModelAndView();
-
-		// 已知异常
-		if (e instanceof CheckException) {
-			logger.info(pjp.getSignature() + " " + e.getLocalizedMessage() + "\r\n" + AOPUtil.getMethodArgs(pjp));
-			throw e;
-		} else {
-			logger.error(pjp.getSignature() + " 发生异常\r\n" + AOPUtil.getMethodArgs(pjp), e);
-			throw e;
-			//  发送邮件或者写到异常文件中
-		}
+//
+//	/**
+//	 * api用错误处理
+//	 * @param pjp
+//	 * @param e
+//	 * @return
+//	 */
+//	private BaseResult<?> apiExceptionHandler(ProceedingJoinPoint pjp, Throwable e) {
+//
+//		BaseResult<?> result = new BaseResult<>();
+//
+//		// 已知异常
+//		if (e instanceof CheckException) {
+//			result.setStatus(BaseResult.FAIL);
+//			result.setStatusDesc(e.getLocalizedMessage());
+//		} else {
+//			logger.error(pjp.getSignature() + " 发生异常\r\n" + AOPUtil.getMethodArgs(pjp), e);
+//			result.setStatus(BaseResult.ERROR);
+//			result.setStatusDesc("接口调用发生异常，请联系服务方。");
+//			//  发送邮件或者写到异常文件中
+//		}
 //		return result;
-	}
+//	}
+//
+//	/**
+//	 * api用日志处理（返回值加签）
+//	 * @param pjp
+//	 * @return
+//	 */
+//
+//	@Pointcut("execution(public com.hyjf.cs.common.bean.result.ApiResult *(..))")
+//	public Object apiResSignLogAOPHandler(ProceedingJoinPoint pjp) {
+//
+//		long startTime = System.currentTimeMillis();
+//		ApiResult<?> result;
+//		try {
+//			result = (ApiResult<?>) pjp.proceed();
+//			logger.info(pjp.getSignature() + " 执行时间:" + (System.currentTimeMillis() - startTime));
+//		}catch(Throwable e) {
+//			result = apiResSignExceptionHandler(pjp, e);
+//		}
+//		return result;
+//	}
+//
+//	/**
+//	 * api用错误处理（返回值加签）
+//	 * @param pjp
+//	 * @param e
+//	 * @return
+//	 */
+//	private ApiResult<?> apiResSignExceptionHandler(ProceedingJoinPoint pjp, Throwable e) {
+//
+//		ApiResult<?> result = new ApiResult<>();
+//
+//		// 已知异常
+//		if (e instanceof CheckException) {
+//			result.setStatus(((CheckException) e).getCode());
+//			result.setStatusDesc(e.getLocalizedMessage());
+//		} else {
+//			logger.error(pjp.getSignature() + " 发生异常\r\n" + AOPUtil.getMethodArgs(pjp), e);
+//			result.setStatus(BaseResult.ERROR);
+//			result.setStatusDesc("接口调用发生异常，请联系服务方。");
+//			//  发送邮件或者写到异常文件中
+//		}
+//		return result;
+//	}
+//
+//	/**
+//	 * webApi用日志处理
+//	 * @param pjp
+//	 * @return
+//	 * @throws Throwable
+//	 */
+//	@Pointcut("execution(public com.hyjf.cs.common.bean.result.WebResult *(..))")
+//	public Object webApiLogAOPHandler(ProceedingJoinPoint pjp) throws Throwable {
+//
+//		long startTime = System.currentTimeMillis();
+//		ModelAndView result = null;
+//		try {
+//			result = (ModelAndView) pjp.proceed();
+//			logger.info(pjp.getSignature() + " 执行时间:" + (System.currentTimeMillis() - startTime));
+//		}catch(Throwable e) {
+//			result = webApiExceptionHandler(pjp, e);
+//		}
+//		return result;
+//	}
+//
+//	/**
+//	 * webApi用错误日志处理
+//	 * @param pjp
+//	 * @param e
+//	 * @return
+//	 * @throws Throwable
+//	 */
+//	private ModelAndView webApiExceptionHandler(ProceedingJoinPoint pjp, Throwable e) throws Throwable {
+//
+////		ModelAndView result = new ModelAndView();
+//
+//		// 已知异常
+//		if (e instanceof CheckException) {
+//			logger.info(pjp.getSignature() + " " + e.getLocalizedMessage() + "\r\n" + AOPUtil.getMethodArgs(pjp));
+//			throw e;
+//		} else {
+//			logger.error(pjp.getSignature() + " 发生异常\r\n" + AOPUtil.getMethodArgs(pjp), e);
+//			throw e;
+//			//  发送邮件或者写到异常文件中
+//		}
+////		return result;
+//	}
 
 	/**
 	 * 原子层返回99时，往组合层抛出异常
