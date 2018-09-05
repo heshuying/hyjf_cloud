bdfbf3550ec358a33f0dbf2cbabd35a28c8988b0
wenxin <wenxin@hyjf.com>
2018-09-05 13:46:29 +0800

    还款计划接口迁移

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/repay/RepayService.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/repay/RepayService.java
@@ -2,14 +2,21 @@ package com.hyjf.cs.trade.service.repay;
 
 import com.hyjf.am.resquest.admin.BatchBorrowRecoverRequest;
 import com.hyjf.am.vo.admin.BatchBorrowRecoverVo;
+import com.hyjf.am.vo.trade.repay.WebUserRepayProjectListCustomizeVO;
+import com.hyjf.am.vo.user.BankOpenAccountVO;
+import com.hyjf.cs.common.service.BaseService;
+import com.hyjf.cs.trade.bean.repay.ProjectBean;
+import com.hyjf.cs.trade.bean.repay.RepayProjectListBean;
 
+import java.text.ParseException;
 import java.util.List;
+
 
 /**
  * @version RepayService, v0.1 2018/8/27 15:39
  * @Author: Zha Daojian
  */
-public interface RepayService {
+public interface RepayService extends BaseService {
 
     /**
      * 获取批次放款列表
@@ -29,16 +36,6 @@ public interface RepayService {
     * @return
     **/
     Integer countBatchCenter(BatchBorrowRecoverRequest request);
-import com.hyjf.am.vo.trade.repay.WebUserRepayProjectListCustomizeVO;
-import com.hyjf.am.vo.user.BankOpenAccountVO;
-import com.hyjf.cs.common.service.BaseService;
-import com.hyjf.cs.trade.bean.repay.ProjectBean;
-import com.hyjf.cs.trade.bean.repay.RepayProjectListBean;
-
-import java.text.ParseException;
-import java.util.List;
-
-public interface RepayService extends BaseService {
 
 	/**
 	 * 获取用户还款列表
