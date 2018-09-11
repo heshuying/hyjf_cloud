64fc9ac380409001628d0ccf1ccb60e42abc1472
jun <jijunjun@hyjf.com>
2018-09-11 10:03:13 +0800

    代码重构

--- hyjf_platform_server/hyjf_pay/hyjf_pay_server/src/main/java/com/hyjf/pay/controller/DzqzCallController.java
+++ hyjf_platform_server/hyjf_pay/hyjf_pay_server/src/main/java/com/hyjf/pay/controller/DzqzCallController.java
@@ -1,19 +1,5 @@
 package com.hyjf.pay.controller;
 
-import java.lang.reflect.Method;
-import java.util.TreeMap;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.hyjf.common.constants.MQConstant;
@@ -26,8 +12,19 @@ import com.hyjf.pay.lib.fadada.call.impl.DzqzCallApiImpl;
 import com.hyjf.pay.mq.FddProducer;
 import com.hyjf.pay.mq.Producer;
 import com.hyjf.pay.service.DzqzPayLogService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.TreeMap;
+
+@RestController
 @RequestMapping(DzqzCallDefine.FDD_REQUEST_MAPPING)
 public class DzqzCallController extends BaseController {
 
@@ -46,8 +43,7 @@ public class DzqzCallController extends BaseController {
      * @return
      * @throws Exception
      */
-    @ResponseBody
-    @RequestMapping(method = RequestMethod.POST, value = DzqzCallDefine.FDD_CALL_APIBG)
+    @PostMapping(value = DzqzCallDefine.FDD_CALL_APIBG)
     public String callApiBg(HttpServletRequest request, @ModelAttribute DzqzCallBean bean) throws Exception {
         log.info("--------------开始调用pay工程-------------");
         log.info("-------fdd-------ca参数-----[{}]",JSON.toJSONString(bean));
@@ -101,8 +97,7 @@ public class DzqzCallController extends BaseController {
      * @return
      * @throws Exception
      */
-    @ResponseBody
-    @RequestMapping(method = RequestMethod.POST, value = DzqzCallDefine.FDD_CALL_API_SIGNNODIFY)
+    @PostMapping(value = DzqzCallDefine.FDD_CALL_API_SIGNNODIFY)
     public String callApiSignNodify(HttpServletRequest request, @ModelAttribute DzqzCallBean bean) throws Exception {
         String orderId = bean.getTransaction_id();
         String transType = request.getParameter("transType");
