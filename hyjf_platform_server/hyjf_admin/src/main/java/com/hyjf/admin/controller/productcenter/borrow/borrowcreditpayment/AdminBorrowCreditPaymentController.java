e281c365eaad519f664b8bcc4da4b0e70b0fc431
zhangyunkai <zhangyunkai@hyjf.com>
2018-09-14 09:20:15 +0800

    ##  修正admin请求链接

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/borrow/borrowcreditpayment/AdminBorrowCreditPaymentController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/borrow/borrowcreditpayment/AdminBorrowCreditPaymentController.java
@@ -20,7 +20,7 @@ import java.io.UnsupportedEncodingException;
  */
 @Api(value = "产品中心-汇转让-还款信息",tags ="产品中心-汇转让-还款信息")
 @RestController
-@RequestMapping("/borrow/creditPayment")
+@RequestMapping("/hyjf-admin/borrow/creditPayment")
 public class AdminBorrowCreditPaymentController {
 
 
