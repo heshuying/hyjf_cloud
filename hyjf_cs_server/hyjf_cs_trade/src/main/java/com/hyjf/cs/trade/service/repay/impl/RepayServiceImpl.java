892fa54903e2525d0c522ac628b8ea2e8ceb78ba
wenxin <wenxin@hyjf.com>
2018-09-16 17:13:00 +0800

    还款计划接口问题调整

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/repay/impl/RepayServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/repay/impl/RepayServiceImpl.java
@@ -192,7 +192,7 @@ public class RepayServiceImpl implements RepayService {
      * 查询用户的还款详情
      */
     @Override
-    public BankOpenAccountVO getBankOpenAccount(String bankAccount) {
+    public BankOpenAccountVO getBankOpenAccount(Integer bankAccount) {
         return borrowClient.getBankOpenAccount(bankAccount);
     }
 }
