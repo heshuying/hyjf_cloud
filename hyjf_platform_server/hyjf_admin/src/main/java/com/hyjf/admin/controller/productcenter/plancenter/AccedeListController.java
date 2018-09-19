3486d4e9d5f0ad84971359c162eb2bd55d55471a
nixiaoling <nixiaoling@hyjf.com>
2018-09-19 10:12:28 +0800

    汇计划投资服务协议->智投服务协议

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/plancenter/AccedeListController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/plancenter/AccedeListController.java
@@ -665,11 +665,8 @@ public class AccedeListController extends BaseController{
 				String[] emails = { email };
 				 _log.info("sendMail***************************下载法大大协议--投资filePath:"+filePath + fileName);
 
-				 // mod by nxl 汇计划投资服务协议->智投服务协议
-/*                MailMessage mailMessage = new MailMessage(Integer.valueOf(userid), msg, "汇计划投资服务协议", null, new String[] { filePath + "/" + fileName }, emails, CustomConstants.EMAITPL_EMAIL_LOCK_REPAY,
-                		MessageConstant.MAIL_SEND_FOR_MAILING_ADDRESS);*/
-				MailMessage mailMessage = new MailMessage(Integer.valueOf(userid), msg, "智投服务协议", null, new String[] { filePath + "/" + fileName }, emails, CustomConstants.EMAITPL_EMAIL_LOCK_REPAY,
-						MessageConstant.MAIL_SEND_FOR_MAILING_ADDRESS);
+                MailMessage mailMessage = new MailMessage(Integer.valueOf(userid), msg, "汇计划投资服务协议", null, new String[] { filePath + "/" + fileName }, emails, CustomConstants.EMAITPL_EMAIL_LOCK_REPAY,
+                		MessageConstant.MAIL_SEND_FOR_MAILING_ADDRESS);
 				// 发送邮件
 				mailProducer.messageSend(new MessageContent(MQConstant.MAIL_TOPIC, UUID.randomUUID().toString(),JSON.toJSONBytes(mailMessage)));
 				// 邮件发送成功后修改DB发邮件的状态
