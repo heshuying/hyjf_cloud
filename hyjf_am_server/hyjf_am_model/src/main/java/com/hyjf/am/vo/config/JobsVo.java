b6c41af5bde639f7f9dbbbad57bdeaeb7f9694a3
fuqiang <18251359382@163.com>
2018-09-25 14:54:37 +0800

    修改返回前端时间格式

--- hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/vo/config/JobsVo.java
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/vo/config/JobsVo.java
@@ -1,8 +1,10 @@
 package com.hyjf.am.vo.config;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hyjf.am.vo.BaseVO;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author lisheng
@@ -20,15 +22,19 @@ public class JobsVo extends BaseVO implements Serializable {
 
     private String addAdmin;
 
-    private Integer createTime;
-
-    private Integer updateTime;
-
     private Integer order;
 
     private Integer persons;
 
     private String email;
+
+    private Integer createUserId;
+
+    private Integer updateUserId;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    private Date createTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    private Date updateTime;
 
     private String content;
 
@@ -74,19 +80,19 @@ public class JobsVo extends BaseVO implements Serializable {
         this.addAdmin = addAdmin == null ? null : addAdmin.trim();
     }
 
-    public Integer getCreateTime() {
+    public Date getCreateTime() {
         return createTime;
     }
 
-    public void setCreateTime(Integer createTime) {
+    public void setCreateTime(Date createTime) {
         this.createTime = createTime;
     }
 
-    public Integer getUpdateTime() {
+    public Date getUpdateTime() {
         return updateTime;
     }
 
-    public void setUpdateTime(Integer updateTime) {
+    public void setUpdateTime(Date updateTime) {
         this.updateTime = updateTime;
     }
 
@@ -120,5 +126,21 @@ public class JobsVo extends BaseVO implements Serializable {
 
     public void setContent(String content) {
         this.content = content == null ? null : content.trim();
+    }
+
+    public Integer getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(Integer createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Integer getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public void setUpdateUserId(Integer updateUserId) {
+        this.updateUserId = updateUserId;
     }
 }
