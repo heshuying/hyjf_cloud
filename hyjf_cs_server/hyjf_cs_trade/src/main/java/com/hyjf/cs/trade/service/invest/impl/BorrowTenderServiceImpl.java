24d74d3c3cd6055f13e9b346f74e68307228cdf8
sunshuaishuai <sunshuaishuai@hyjf.com>
2018-09-29 09:56:37 +0800

    新手标问题

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/invest/impl/BorrowTenderServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/invest/impl/BorrowTenderServiceImpl.java
@@ -368,8 +368,8 @@ public class BorrowTenderServiceImpl extends BaseTradeServiceImpl implements Bor
             }
         }
         // 新手标
-        logger.info("investUserType: "+borrowProjectType.getInvestUserType());
-        if ("1".equals(borrowProjectType.getInvestUserType())) {
+        logger.info("investUserType:{}  projectType:{} ",borrowProjectType.getInvestUserType(),projectType);
+        if (borrowProjectType.getInvestUserType().equals(1)) {
             boolean isNew = this.checkIsNewUserCanInvest(userId);
             // 该项目只能新手投资
             if (!isNew) {
