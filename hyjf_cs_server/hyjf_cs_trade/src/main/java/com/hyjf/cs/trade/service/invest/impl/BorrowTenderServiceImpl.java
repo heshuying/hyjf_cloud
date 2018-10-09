7db28b1eddafc6223be50e41aa9bb643f40c3eb5
sunshuaishuai <sunshuaishuai@hyjf.com>
2018-10-09 11:57:52 +0800

    散标详情-->优惠券，没有优惠券时，显示无可用状态并不可点击，当前是显示了“暂无可用”并能点击

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/invest/impl/BorrowTenderServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/invest/impl/BorrowTenderServiceImpl.java
@@ -827,6 +827,7 @@ public class BorrowTenderServiceImpl extends BaseTradeServiceImpl implements Bor
             investInfo.setCouponAvailableCount(couponAvailableCount + "");
             // 优惠券总张数
             recordTotal = amTradeClient.getUserCouponCount(loginUser.getUserId(), "0");
+            logger.info("recordTotal:{}  , couponAvailableCount:{}",recordTotal,couponAvailableCount);
             investInfo.setBorrowApr(borrow.getBorrowApr() + "%");
             investInfo.setPaymentOfInterest("");
             // 是否使用优惠券
@@ -861,10 +862,10 @@ public class BorrowTenderServiceImpl extends BaseTradeServiceImpl implements Bor
                 investInfo.setCouponQuota("");
                 investInfo.setCouponId("-1");
 
-                if (recordTotal > 0) {
+                if (couponAvailableCount > 0) {
                     investInfo.setIsThereCoupon("1");
                     investInfo.setCouponDescribe("请选择");
-                } else if (recordTotal == 0) {
+                } else if (recordTotal > 0 && couponAvailableCount == 0) {
                     investInfo.setIsThereCoupon("1");
                     investInfo.setCouponDescribe("暂无可用");
                 } else {
