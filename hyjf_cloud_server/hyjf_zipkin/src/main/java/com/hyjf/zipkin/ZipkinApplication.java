09244843e861ebbdf7e338f0810d7efc5248c17a
dengxiaojiang <dengxiaojiang@hyjf.com>
2018-10-12 21:03:15 +0800

    zipkin mysql

--- hyjf_cloud_server/hyjf_zipkin/src/main/java/com/hyjf/zipkin/ZipkinApplication.java
+++ hyjf_cloud_server/hyjf_zipkin/src/main/java/com/hyjf/zipkin/ZipkinApplication.java
@@ -1,9 +1,14 @@
 package com.hyjf.zipkin;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
 import zipkin2.server.internal.EnableZipkinServer;
+import zipkin2.storage.mysql.v1.MySQLStorage;
 
 /**
  * ZipKin Server
@@ -17,5 +22,10 @@ import zipkin2.server.internal.EnableZipkinServer;
 public class ZipkinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkinApplication.class, args);
+	}
+
+	@Bean
+	public MySQLStorage mySQLStorage(DataSource datasource) {
+		return MySQLStorage.newBuilder().datasource(datasource).executor(Runnable::run).build();
 	}
 }
