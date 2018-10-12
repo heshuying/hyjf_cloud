eb8cc69b1fd1ff818c9a942dff37bb1e79cddaf6
heshuying <heshuying@hyjf.com>
2018-10-12 20:31:07 +0800

    受托支付回调修改

--- hyjf_platform_server/hyjf_pay/hyjf_pay_server/src/main/java/com/hyjf/pay/controller/BankCallController.java
+++ hyjf_platform_server/hyjf_pay/hyjf_pay_server/src/main/java/com/hyjf/pay/controller/BankCallController.java
@@ -1,5 +1,6 @@
 package com.hyjf.pay.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -450,6 +451,7 @@ public class BankCallController extends BaseController {
                                 notifyUrl = StringEscapeUtils.unescapeHtml(notifyUrl);
 //                                content = HttpDeal.post(notifyUrl, bean.getAllParams());
                                 logger.info("开始请求回调地址：" + notifyUrl);
+                                logger.info("参数：" + JSON.toJSONString(bean.getAllParams()));
                                 content = restTemplate
                                         .postForEntity(notifyUrl, bean.getAllParams(), String.class).getBody();
                                 logger.info("返回值content：" + content);
