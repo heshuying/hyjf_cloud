9d95fab8e5611a80b8d33fa5d31f3a1cefc28a44
yangchangwei <ycw123456>
2018-10-16 16:20:55 +0800

    自动审核保证金增加延时避免事务读不到

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/mq/consumer/hjh/issuerecover/AutoRecordMessageConsumer.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/mq/consumer/hjh/issuerecover/AutoRecordMessageConsumer.java
@@ -194,7 +194,8 @@ public class AutoRecordMessageConsumer extends Consumer {
                                         try {
                                             JSONObject params = new JSONObject();
                                             params.put("borrowNid", borrow.getBorrowNid());
-                                            autoBailMessageProducer.messageSend(new MessageContent(MQConstant.ROCKETMQ_BORROW_BAIL_TOPIC, UUID.randomUUID().toString(), JSONObject.toJSONBytes(params)));
+                                            //modify by yangchangwei 防止队列触发太快，导致无法获得本事务变泵的数据，延时级别为2 延时5秒
+                                            autoBailMessageProducer.messageSendDelay(new MessageContent(MQConstant.ROCKETMQ_BORROW_BAIL_TOPIC, UUID.randomUUID().toString(), JSONObject.toJSONBytes(params)),2);
                                         } catch (MQException e) {
                                             logger.error("发送【审核保证金队列】MQ失败...");
                                         }
