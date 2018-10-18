75e5f9ec2e9bb54bae38139fbffae18950752cca
sunshuaishuai <sunshuaishuai@hyjf.com>
2018-10-18 15:37:35 +0800

    文字内容修改

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/invest/impl/BorrowCreditTenderServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/invest/impl/BorrowCreditTenderServiceImpl.java
@@ -306,7 +306,7 @@ public class BorrowCreditTenderServiceImpl extends BaseTradeServiceImpl implemen
         if (money == null || "".equals(money) || (new BigDecimal(money).compareTo(BigDecimal.ZERO) == 0)) {
             money = "0";
             result.setRealAmount("");
-            result.setButtonWord("实际支付0.00元");
+            result.setButtonWord("确认");
         } else {
             result.setRealAmount("");
             result.setButtonWord("确认投资"+CommonUtils.formatAmount(null, money)+"元");
