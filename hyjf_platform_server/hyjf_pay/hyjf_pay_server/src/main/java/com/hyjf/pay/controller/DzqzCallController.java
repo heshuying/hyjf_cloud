fa1622ed2d2edbc9638c68f2538501283ff40dfc
yangchangwei <ycw123456>
2018-10-18 11:19:21 +0800

    法大大协议不通日志调试

--- hyjf_platform_server/hyjf_pay/hyjf_pay_server/src/main/java/com/hyjf/pay/controller/DzqzCallController.java
+++ hyjf_platform_server/hyjf_pay/hyjf_pay_server/src/main/java/com/hyjf/pay/controller/DzqzCallController.java
@@ -33,6 +33,9 @@ public class DzqzCallController extends BaseController {
     private FddProducer fddProducer;
 
     @Autowired
+    private Producer producer;
+
+    @Autowired
     SystemConfig systemConfig;
 
 
@@ -117,8 +120,8 @@ public class DzqzCallController extends BaseController {
                 fddProducer.messageSend(new Producer.MassageContent(MQConstant.FDD_TOPIC,MQConstant.FDD_AUTO_SIGN_TAG,JSON.toJSONBytes(bean)));
             }
         } catch (Exception e) {
-            log.info("---------------合同签署异步返回异常，txcode:" + bean.getResult_code() + ",logordid:" + orderId);
             e.printStackTrace();
+            log.info("---------------合同签署异步返回异常，txcode:" + bean.getResult_code() + ",logordid:" + orderId);
         }
         return "success";
     }
