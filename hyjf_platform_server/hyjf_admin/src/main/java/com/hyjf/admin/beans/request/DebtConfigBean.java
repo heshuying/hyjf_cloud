c51234e5e879c0b1ab38f19500d895577f37d267
tanyangyang <591628420@qq.com>
2018-10-18 19:21:08 +0800

    swwager

---
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/beans/request/DebtConfigBean.java
@@ -0,0 +1,13 @@
+package com.hyjf.admin.beans.request;
+
+import com.hyjf.admin.beans.BaseRequest;
+
+/**
+ *
+ * @author tanyy
+ * @date 2018/08/09 17:00
+ * @version V1.0  
+ */
+public class DebtConfigBean extends BaseRequest {
+
+}
