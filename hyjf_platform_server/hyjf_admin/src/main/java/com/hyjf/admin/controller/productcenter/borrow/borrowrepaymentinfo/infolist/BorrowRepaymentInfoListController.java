876b8ab1b5dd99dd7808589dbe5b8a40c8813b27
zhangqingqing <zhangqingqing@hyjf.com>
2018-10-19 17:25:48 +0800

    产品中心-汇直投-还款明细列表导出修改

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/borrow/borrowrepaymentinfo/infolist/BorrowRepaymentInfoListController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/borrow/borrowrepaymentinfo/infolist/BorrowRepaymentInfoListController.java
@@ -1,13 +1,17 @@
 package com.hyjf.admin.controller.productcenter.borrow.borrowrepaymentinfo.infolist;
 
+import com.google.common.collect.Maps;
 import com.hyjf.admin.beans.BorrowRepaymentInfoListBean;
 import com.hyjf.admin.beans.request.BorrowRepaymentInfoListRequestBean;
 import com.hyjf.admin.common.result.AdminResult;
 import com.hyjf.admin.common.util.ExportExcel;
 import com.hyjf.admin.common.util.ShiroConstants;
+import com.hyjf.admin.config.SystemConfig;
 import com.hyjf.admin.interceptor.AuthorityAnnotation;
 import com.hyjf.admin.service.BorrowRepaymentInfoListService;
 import com.hyjf.admin.utils.ConvertUtils;
+import com.hyjf.admin.utils.exportutils.DataSet2ExcelSXSSFHelper;
+import com.hyjf.admin.utils.exportutils.IValueFormatter;
 import com.hyjf.am.resquest.admin.BorrowRepaymentInfoListRequset;
 import com.hyjf.am.vo.admin.BorrowRepaymentInfoListCustomizeVO;
 import com.hyjf.am.vo.user.HjhInstConfigVO;
@@ -21,6 +25,7 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,8 +37,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author pangchengchao
@@ -48,6 +55,9 @@ public class BorrowRepaymentInfoListController {
     private BorrowRepaymentInfoListService borrowRepaymentInfoListService;
     /** 查看权限 */
     public static final String PERMISSIONS = "borrowrepayment";
+
+    @Autowired
+    SystemConfig systemConfig;
     /**
      * 画面初始化
      *
@@ -75,9 +85,9 @@ public class BorrowRepaymentInfoListController {
      * @Version v0.1
      * @Date
      */
-    @ApiOperation(value = "数据导出--还款详情导出数据", notes = "带条件导出EXCEL")
-    @PostMapping(value = "/exportAction")
-    @AuthorityAnnotation(key = PERMISSIONS, value = ShiroConstants.PERMISSION_EXPORT)
+//    @ApiOperation(value = "数据导出--还款详情导出数据", notes = "带条件导出EXCEL")
+//    @PostMapping(value = "/exportAction")
+//    @AuthorityAnnotation(key = PERMISSIONS, value = ShiroConstants.PERMISSION_EXPORT)
     public void exportAction(HttpServletRequest request, HttpServletResponse response, @RequestBody @Valid BorrowRepaymentInfoListRequestBean form) throws Exception {
         BorrowRepaymentInfoListRequset copyForm=new BorrowRepaymentInfoListRequset();
         BeanUtils.copyProperties(form, copyForm);
@@ -248,5 +258,150 @@ public class BorrowRepaymentInfoListController {
         }
         // 导出
         ExportExcel.writeExcelFile(response, workbook, titles, fileName);
+    }
+
+    /**
+     * @Description 数据导出--还款计划
+     * @Author pangchengchao
+     * @Version v0.1
+     * @Date
+     */
+    @ApiOperation(value = "数据导出--还款详情导出数据", notes = "带条件导出EXCEL")
+    @PostMapping(value = "/exportAction")
+    @AuthorityAnnotation(key = PERMISSIONS, value = ShiroConstants.PERMISSION_EXPORT)
+    public void exportToExcel(HttpServletRequest request, HttpServletResponse response, @RequestBody @Valid BorrowRepaymentInfoListRequestBean form) throws Exception {
+
+        BorrowRepaymentInfoListRequset copyForm=new BorrowRepaymentInfoListRequset();
+        BeanUtils.copyProperties(form, copyForm);
+
+        //sheet默认最大行数
+        int defaultRowMaxCount = Integer.valueOf(systemConfig.getDefaultRowMaxCount());
+        // 表格sheet名称
+        String sheetName = "还款详情导出数据";
+        // 文件名称
+        String fileName = URLEncoder.encode(sheetName, CustomConstants.UTF8) + StringPool.UNDERLINE + GetDate.getServerDateTime(8, new Date()) + ".xls";
+        // 声明一个工作薄
+        SXSSFWorkbook workbook = new SXSSFWorkbook(SXSSFWorkbook.DEFAULT_WINDOW_SIZE);
+        DataSet2ExcelSXSSFHelper helper = new DataSet2ExcelSXSSFHelper();
+        //请求第一页5000条
+        copyForm.setPageSize(defaultRowMaxCount);
+        copyForm.setCurrPage(1);
+        // 查询
+        List<BorrowRepaymentInfoListCustomizeVO> resultList = this.borrowRepaymentInfoListService.selectExportBorrowRepaymentInfoListList(copyForm);
+        Integer totalCount = resultList.size();
+
+        int sheetCount = (totalCount % defaultRowMaxCount) == 0 ? totalCount / defaultRowMaxCount : totalCount / defaultRowMaxCount + 1;
+        Map<String, String> beanPropertyColumnMap = buildMap();
+        Map<String, IValueFormatter> mapValueAdapter = buildValueAdapter();
+        String sheetNameTmp = sheetName + "_第1页";
+        if (totalCount == 0) {
+
+            helper.export(workbook, sheetNameTmp, beanPropertyColumnMap, mapValueAdapter, new ArrayList());
+        }else {
+            helper.export(workbook, sheetNameTmp, beanPropertyColumnMap, mapValueAdapter, resultList);
+        }
+        for (int i = 1; i < sheetCount; i++) {
+
+            copyForm.setPageSize(defaultRowMaxCount);
+            copyForm.setCurrPage(i+1);
+            List<BorrowRepaymentInfoListCustomizeVO> resultList2 = this.borrowRepaymentInfoListService.selectExportBorrowRepaymentInfoListList(copyForm);
+            if (resultList2 != null && resultList2.size()> 0) {
+                sheetNameTmp = sheetName + "_第" + (i + 1) + "页";
+                helper.export(workbook, sheetNameTmp, beanPropertyColumnMap, mapValueAdapter,  resultList2);
+            } else {
+                break;
+            }
+        }
+        DataSet2ExcelSXSSFHelper.write2Response(request, response, fileName, workbook);
+    }
+
+    private Map<String, String> buildMap() {
+        Map<String, String> map = Maps.newLinkedHashMap();
+        map.put("borrowNid","借款编号");
+        map.put("instName","资产来源");
+        map.put("userId","借款人ID");
+        map.put("borrowUserName","借款人用户名");
+        map.put("borrowName","借款标题");
+        map.put("projectTypeName","项目类型");
+        map.put("borrowPeriod","借款期限");
+        map.put("borrowApr","年化收益");
+        map.put("borrowAccount","借款金额");
+        map.put("borrowAccountYes","借到金额");
+        map.put("repayType","还款方式");
+        map.put("recoverPeriod","还款期次");
+        map.put("recoverUserName","投资人用户名");
+        map.put("recoverUserId","投资人ID");
+        map.put("recoverTotal","投资金额");
+        map.put("recoverCapital","应还本金");
+        map.put("recoverInterest","应还利息");
+        map.put("recoverAccountCopy","应还本息");
+        map.put("recoverFee","还款服务费");
+        map.put("chargeDays","提前天数");
+        map.put("chargeInterest","少还利息");
+        map.put("delayDays","延期天数");
+        map.put("delayInterest","延期利息");
+        map.put("lateDays","逾期天数");
+        map.put("lateInterest","逾期利息");
+        map.put("recoverAccount","应还总额");
+        map.put("nid","还款订单号");
+        map.put("recoverAccountYes","实还总额");
+        map.put("status","还款状态");
+        map.put("recoverActionTime","实际还款日期");
+        map.put("recoverLastTime","应还日期");
+        return map;
+    }
+
+    private Map<String, IValueFormatter> buildValueAdapter() {
+        Map<String, IValueFormatter> mapAdapter = Maps.newHashMap();
+        IValueFormatter borrowPeriodAdapter = new IValueFormatter() {
+            @Override
+            public String format(Object object) {
+                return object+"个月";
+            }
+        };
+        IValueFormatter borrowAprAdapter = new IValueFormatter() {
+            @Override
+            public String format(Object object) {
+                return object + "%";
+            }
+        };
+        IValueFormatter valueFormatAdapter = new IValueFormatter() {
+            @Override
+            public String format(Object object) {
+                return object==null||"".equals(object) ? "0" : object.toString();
+            }
+        };
+        IValueFormatter recoverPeriodAdapter = new IValueFormatter() {
+            @Override
+            public String format(Object object) {
+                return "第" + object + "期";
+            }
+        };
+        IValueFormatter statusAdapter = new IValueFormatter() {
+            @Override
+            public String format(Object object) {
+                if (null!=object) {
+                    return"0".equals(object) ? "还款中" : "已还款";
+                }
+                return null;
+            }
+        };
+        mapAdapter.put("borrowPeriod", borrowPeriodAdapter);
+        mapAdapter.put("borrowApr", borrowAprAdapter);
+        mapAdapter.put("borrowAccount", valueFormatAdapter);
+        mapAdapter.put("borrowAccountYes", valueFormatAdapter);
+        mapAdapter.put("recoverPeriod", recoverPeriodAdapter);
+        mapAdapter.put("recoverTotal", valueFormatAdapter);
+        mapAdapter.put("recoverCapital", valueFormatAdapter);
+        mapAdapter.put("recoverInterest", valueFormatAdapter);
+        mapAdapter.put("recoverFee", valueFormatAdapter);
+        mapAdapter.put("chargeInterest", valueFormatAdapter);
+        mapAdapter.put("delayInterest", valueFormatAdapter);
+        mapAdapter.put("lateInterest", valueFormatAdapter);
+        mapAdapter.put("recoverAccount", valueFormatAdapter);
+        mapAdapter.put("recoverAccountCopy", valueFormatAdapter);
+        mapAdapter.put("recoverAccountYes", valueFormatAdapter);
+        mapAdapter.put("status", statusAdapter);
+        return mapAdapter;
     }
 }
