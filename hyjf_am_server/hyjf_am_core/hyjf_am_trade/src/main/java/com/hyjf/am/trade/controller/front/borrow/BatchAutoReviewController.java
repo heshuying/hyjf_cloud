155d4429ad8a10169f794a0b3f3ee6ccc575ffd2
PC-LIUSHOUYI <liushouyi@hyjf.com>
2018-10-24 15:01:31 +0800

    14696 bug修复  查询复审的sql修改

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/front/borrow/BatchAutoReviewController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/front/borrow/BatchAutoReviewController.java
@@ -1,8 +1,8 @@
 package com.hyjf.am.trade.controller.front.borrow;
 
+import com.hyjf.am.trade.dao.model.auto.Borrow;
 import com.hyjf.am.trade.service.task.bank.autoreview.BatchAutoReviewService;
 import com.hyjf.am.trade.utils.constant.BorrowSendTypeEnum;
-import com.hyjf.am.vo.task.issuerecover.BorrowWithBLOBs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,9 +41,9 @@ public class BatchAutoReviewController {
             }
             // 获取过期时间
             Integer afterTime = batchAutoReviewService.getAfterTime(BorrowSendTypeEnum.FUSHENSEND_CD);
-            List<BorrowWithBLOBs> borrowList = this.batchAutoReviewService.selectAutoReview();
+            List<Borrow> borrowList = this.batchAutoReviewService.selectAutoReview();
             if (borrowList != null && borrowList.size() > 0) {
-                for (BorrowWithBLOBs borrow : borrowList) {
+                for (Borrow borrow : borrowList) {
                     try {
                         batchAutoReviewService.updateBorrow(borrow, afterTime);
                     } catch (Exception e) {
