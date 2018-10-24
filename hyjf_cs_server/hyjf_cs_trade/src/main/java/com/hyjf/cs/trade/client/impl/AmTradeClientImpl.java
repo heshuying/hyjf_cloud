0990a760cfe4f3ab25a1b4c170b90145eca5fb6a
liubin <liu'bin>
2018-10-24 19:34:31 +0800

    微服务 自动投资修改 修正

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/impl/AmTradeClientImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/client/impl/AmTradeClientImpl.java
@@ -397,7 +397,7 @@ public class AmTradeClientImpl implements AmTradeClient {
      */
     @Override
     public boolean updateCreditForAutoTender(String creditNid, String accedeOrderId, String planNid, BankCallBean bean, String tenderUsrcustid, String sellerUsrcustid, Map<String, Object> resultMap) {
-        String url = "autoTenderController/updateCreditForAutoTender";
+        String url = urlBase + "autoTenderController/updateCreditForAutoTender";
         BankCallBeanVO bankCallBeanVO = new BankCallBeanVO();
         BeanUtils.copyProperties(bean, bankCallBeanVO);
         UpdateCreditForAutoTenderRequest request = new UpdateCreditForAutoTenderRequest(creditNid, accedeOrderId, planNid, bankCallBeanVO, tenderUsrcustid, sellerUsrcustid, resultMap);
