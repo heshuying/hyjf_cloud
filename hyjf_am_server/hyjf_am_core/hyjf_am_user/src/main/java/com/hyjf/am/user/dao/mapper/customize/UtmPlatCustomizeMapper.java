6517b51cb2c6b73989802da5a544c44258576fd4
heshuying <heshuying@hyjf.com>
2018-10-25 18:45:39 +0800

    报错修改

--- hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/dao/mapper/customize/UtmPlatCustomizeMapper.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/dao/mapper/customize/UtmPlatCustomizeMapper.java
@@ -6,5 +6,6 @@ import com.hyjf.am.user.dao.model.auto.UtmPlat;
  * 渠道相关
  */
 public interface UtmPlatCustomizeMapper {
+    UtmPlat selectUtmPlatByUserId(Integer userId);
     UtmPlat selectUtmPlatBySourceIds(Integer userId);
 }
