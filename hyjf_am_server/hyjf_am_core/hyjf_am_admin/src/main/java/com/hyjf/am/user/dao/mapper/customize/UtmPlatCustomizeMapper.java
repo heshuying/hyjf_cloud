4b5863fa278ec514b9b3555a7482b7e26f00aec9
yinhui <yinhui@shizitegong.com>
2018-10-25 17:49:21 +0800

    app渠道统计定时任务

--- hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/user/dao/mapper/customize/UtmPlatCustomizeMapper.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/user/dao/mapper/customize/UtmPlatCustomizeMapper.java
@@ -1,10 +1,19 @@
 package com.hyjf.am.user.dao.mapper.customize;
 
 import com.hyjf.am.user.dao.model.auto.UtmPlat;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 渠道相关
  */
 public interface UtmPlatCustomizeMapper {
     UtmPlat selectUtmPlatByUserId(Integer userId);
+
+    UtmPlat selectUtmPlatBySourceIds(Integer sourceId);
+
+    List<Integer> selectUsersInfo();
+
+    List<Integer> selectUsers(@Param("sourceIdSrch") String sourceIdSrch);
 }
