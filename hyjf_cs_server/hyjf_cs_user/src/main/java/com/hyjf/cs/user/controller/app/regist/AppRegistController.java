4e75920b3bcb1759348810aa63aff5178263f335
zhangqingqing <zhangqingqing@hyjf.com>
2018-10-25 15:52:05 +0800

    调试

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/controller/app/regist/AppRegistController.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/controller/app/regist/AppRegistController.java
@@ -124,6 +124,7 @@ public class AppRegistController extends BaseUserController {
                 register.getReffer(), CommonConstant.HYJF_INST_CODE, register.getUtmId(), platform, GetCilentIP.getIpAddr(request));
 
         //发送mq同步推广表
+        logger.info("发送mq同步推广表=============");
         registService.sendMqToSaveAppChannel(version,webViewUserVO);
         String statusDesc = "注册成功";
         boolean active = false;
