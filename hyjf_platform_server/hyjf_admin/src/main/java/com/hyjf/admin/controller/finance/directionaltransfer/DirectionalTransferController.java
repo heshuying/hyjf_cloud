e3ac3f169bb30b8b6059eaeb549385f7c43c5f4c
zhangyunkai <zhangyunkai@hyjf.com>
2018-10-26 16:52:21 +0800

    ##  导出方法修改12

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/finance/directionaltransfer/DirectionalTransferController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/finance/directionaltransfer/DirectionalTransferController.java
@@ -68,7 +68,7 @@ public class DirectionalTransferController extends BaseController {
         //sheet默认最大行数
         int defaultRowMaxCount = Integer.valueOf(systemConfig.getDefaultRowMaxCount());
         // 表格sheet名称
-        String sheetName = "绑定日志列表";
+        String sheetName = "定向转账列表";
         // 文件名称
         String fileName = URLEncoder.encode(sheetName, CustomConstants.UTF8) + StringPool.UNDERLINE + GetDate.getServerDateTime(8, new Date()) + ".xlsx";
         // 声明一个工作薄
