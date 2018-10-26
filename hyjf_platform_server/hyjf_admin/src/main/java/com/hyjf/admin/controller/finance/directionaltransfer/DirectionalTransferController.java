e480ae0d4c8062211ceb1aa63ed98fa828708f65
zhangyunkai <zhangyunkai@hyjf.com>
2018-10-26 16:51:10 +0800

    ##  导出方法修改12

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/finance/directionaltransfer/DirectionalTransferController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/finance/directionaltransfer/DirectionalTransferController.java
@@ -1,10 +1,12 @@
 package com.hyjf.admin.controller.finance.directionaltransfer;
 
+import com.google.common.collect.Maps;
 import com.hyjf.admin.common.result.AdminResult;
 import com.hyjf.admin.common.result.ListResult;
-import com.hyjf.admin.common.util.ExportExcel;
 import com.hyjf.admin.controller.BaseController;
 import com.hyjf.admin.service.DirectionalTransferService;
+import com.hyjf.admin.utils.exportutils.DataSet2ExcelSXSSFHelper;
+import com.hyjf.admin.utils.exportutils.IValueFormatter;
 import com.hyjf.am.resquest.admin.DirectionalTransferListRequest;
 import com.hyjf.am.vo.admin.AccountDirectionalTransferVO;
 import com.hyjf.common.util.CustomConstants;
@@ -12,21 +14,23 @@ import com.hyjf.common.util.GetDate;
 import com.hyjf.common.util.StringPool;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Api(value = "资金中心-定向转账-定向转账",tags = "资金中心-定向转账-定向转账")
 @RestController
@@ -59,74 +63,105 @@ public class DirectionalTransferController extends BaseController {
      */
     @ApiOperation(value = "导出定向转账列表",notes = "导出定向转账列表")
     @PostMapping(value = "/directionaltransferlistexport")
-    public void exportDirectionalTransferListExcel(@RequestBody DirectionalTransferListRequest request,HttpServletResponse response) throws Exception {
-        // currPage<0 为全部,currPage>0 为具体某一页
-        request.setCurrPage(-1);
+    public void exportDirectionalTransferListExcel(@RequestBody DirectionalTransferListRequest request, HttpServletRequest httpRequest, HttpServletResponse response) throws Exception {
 
+        //sheet默认最大行数
+        int defaultRowMaxCount = Integer.valueOf(systemConfig.getDefaultRowMaxCount());
         // 表格sheet名称
-        String sheetName = "定向转账列表";
+        String sheetName = "绑定日志列表";
         // 文件名称
-        String fileName =
-                URLEncoder.encode(sheetName, "UTF-8") + StringPool.UNDERLINE + GetDate.getServerDateTime(8, new Date()) + CustomConstants.EXCEL_EXT;
-
-        // 检索列表
-        List<AccountDirectionalTransferVO> resultList = directionaltransferService.searchDirectionalTransferList(request);
-        String[] titles = new String[] { "序号", "转出账户", "转入账户", "转账订单号", "转账金额", "转账状态", "转账时间", "说明" };
+        String fileName = URLEncoder.encode(sheetName, CustomConstants.UTF8) + StringPool.UNDERLINE + GetDate.getServerDateTime(8, new Date()) + ".xlsx";
         // 声明一个工作薄
-        HSSFWorkbook workbook = new HSSFWorkbook();
-        // 生成一个表格
-        HSSFSheet sheet = ExportExcel.createHSSFWorkbookTitle(workbook, titles, sheetName + "_第1页");
+        SXSSFWorkbook workbook = new SXSSFWorkbook(SXSSFWorkbook.DEFAULT_WINDOW_SIZE);
+        DataSet2ExcelSXSSFHelper helper = new DataSet2ExcelSXSSFHelper();
 
-        if (resultList != null && resultList.size() > 0) {
-            int sheetCount = 1;
-            int rowNum = 0;
-            for (int i = 0; i < resultList.size(); i++) {
-                rowNum++;
-                if (i != 0 && i % 60000 == 0) {
-                    sheetCount++;
-                    sheet =
-                            ExportExcel
-                                    .createHSSFWorkbookTitle(workbook, titles, (sheetName + "_第" + sheetCount + "页"));
-                    rowNum = 1;
-                }
-                // 新建一行
-                Row row = sheet.createRow(rowNum);
+        int sheetCount = 0;
+        String sheetNameTmp = sheetName + "_第1页";
+        Map<String, String> beanPropertyColumnMap = buildMap();
+        Map<String, IValueFormatter> mapValueAdapter = buildValueAdapter();
+        request.setCurrPage(1);
+        request.setPageSize(defaultRowMaxCount);
 
-                // 循环数据
-                for (int celLength = 0; celLength < titles.length; celLength++) {
-                    AccountDirectionalTransferVO accountDirectionalTransfer = resultList.get(i);
-                    // 创建相应的单元格
-                    Cell cell = row.createCell(celLength);
-                    if (celLength == 0) {// 序号
-                        cell.setCellValue(i + 1);
-                    } else if (celLength == 1) { // 转出账户
-                        cell.setCellValue(accountDirectionalTransfer.getTurnOutUsername());
-                    } else if (celLength == 2) { // 转入账户
-                        cell.setCellValue(accountDirectionalTransfer.getShiftToUsername());
-                    } else if (celLength == 3) { // 转账订单号
-                        cell.setCellValue(accountDirectionalTransfer.getOrderId());
-                    } else if (celLength == 4) {// 转账金额
-                        cell.setCellValue(String.valueOf(accountDirectionalTransfer.getTransferAccountsMoney()));
-                    } else if (celLength == 5) {// 转账状态
-                        if (accountDirectionalTransfer.getTransferAccountsState() == 0) {
-                            cell.setCellValue("转账中");
-                        } else if (accountDirectionalTransfer.getTransferAccountsState() == 1) {
-                            cell.setCellValue("成功");
-                        } else if (accountDirectionalTransfer.getTransferAccountsState() == 2) {
-                            cell.setCellValue("失败");
-                        }
-                    } else if (celLength == 6) {// 转账时间
-                        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                        cell.setCellValue(df.format(accountDirectionalTransfer.getTransferAccountsTime()));
-                    } else if (celLength == 7) {// 说明
-                        cell.setCellValue(accountDirectionalTransfer.getRemark());
-                    }
-                }
-            }
+
+        Integer count = directionaltransferService.getDirectionalTransferCount(request);
+
+        if (count == null || count <= 0 ){
+            helper.export(workbook, sheetNameTmp, beanPropertyColumnMap, mapValueAdapter, new ArrayList());
+        }else{
+            int totalCount = count;
+            // 检索列表
+            List<AccountDirectionalTransferVO> resultList = directionaltransferService.searchDirectionalTransferList(request);
+            sheetCount = (totalCount % defaultRowMaxCount) == 0 ? totalCount / defaultRowMaxCount : totalCount / defaultRowMaxCount + 1;
+            helper.export(workbook, sheetNameTmp, beanPropertyColumnMap, mapValueAdapter, resultList);
         }
 
-        // 导出
-        ExportExcel.writeExcelFile(response, workbook, titles, fileName);
+        for (int i = 1; i < sheetCount; i++) {
+            request.setCurrPage(i+1);
+            List<AccountDirectionalTransferVO> resultList2 = directionaltransferService.searchDirectionalTransferList(request);
+            if (!CollectionUtils.isEmpty(resultList2)) {
+                sheetNameTmp = sheetName + "_第" + (i + 1) + "页";
+                helper.export(workbook, sheetNameTmp, beanPropertyColumnMap, mapValueAdapter,  resultList2);
+            } else {
+                break;
+            }
+        }
+        DataSet2ExcelSXSSFHelper.write2Response(httpRequest, response, fileName, workbook);
+    }
 
+    private Map<String, String> buildMap() {
+        Map<String, String> map = Maps.newLinkedHashMap();
+        map.put("turnOutUsername", "转出账户");
+        map.put("shiftToUsername", "转入账户");
+        map.put("orderId", "转账订单号");
+        map.put("transferAccountsMoney", "转账金额");
+        map.put("transferAccountsState", "转账状态");
+        map.put("transferAccountsTime", "转账时间");
+        map.put("remark", "说明");
+        return map;
+    }
+
+    private Map<String, IValueFormatter> buildValueAdapter() {
+        Map<String, IValueFormatter> mapAdapter = Maps.newHashMap();
+        IValueFormatter transferAccountsMoneyAdapter = new IValueFormatter() {
+            @Override
+            public String format(Object object) {
+                BigDecimal transferAccountsMoney = (BigDecimal) object;
+                return String.valueOf(transferAccountsMoney);
+            }
+        };
+
+        IValueFormatter transferAccountsStateAdapter = new IValueFormatter() {
+            @Override
+            public String format(Object object) {
+                Integer transferAccountsState = (Integer) object;
+                String transferAccountsStateStr = "";
+                switch (transferAccountsState){
+                    case 0:
+                        transferAccountsStateStr = "转账中";
+                        break;
+                    case 1:
+                        transferAccountsStateStr = "成功";
+                        break;
+                    case 2:
+                        transferAccountsStateStr = "失败";
+                        break;
+                    default:
+                }
+
+                return transferAccountsStateStr;
+            }
+        };
+        IValueFormatter transferAccountsTimeAdapter = new IValueFormatter() {
+            @Override
+            public String format(Object object) {
+                Date transferAccountsTime = (Date) object;
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                return df.format(transferAccountsTime);
+            }
+        };
+        mapAdapter.put("transferAccountsMoney",transferAccountsMoneyAdapter);
+        mapAdapter.put("transferAccountsState",transferAccountsStateAdapter);
+        mapAdapter.put("transferAccountsTime",transferAccountsTimeAdapter);
+        return mapAdapter;
     }
 }
