b4fb92bbca45e764daf8c77071c4c0992d129af4
zhangyunkai <zhangyunkai@hyjf.com>
2018-10-29 18:20:56 +0800

    ##  全局拦截整理

--- hyjf_cs_server/hyjf_cs_common/src/main/java/com/hyjf/cs/common/controller/BaseController.java
+++ hyjf_cs_server/hyjf_cs_common/src/main/java/com/hyjf/cs/common/controller/BaseController.java
@@ -3,21 +3,9 @@
  */
 package com.hyjf.cs.common.controller;
 
-import com.hyjf.am.bean.result.BaseResult;
-import com.hyjf.common.enums.MsgEnum;
-import com.hyjf.common.exception.AMException;
-import com.hyjf.common.exception.CheckException;
-import com.hyjf.common.exception.ReturnMessageException;
-import com.hyjf.common.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -34,86 +22,6 @@ public class BaseController {
         return "---《"+ex.getMessage()+"》";
     }
 
-    /**
-     * 组合层Exception错误异常处理（共用）
-     * @param request
-     * @param response
-     * @param ex
-     * @return
-     */
-    @ExceptionHandler(value = Exception.class)
-    @ResponseBody
-    public BaseResult<?> defaultErrorHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        logger.error("组合层发生异常(CS)", ex);
-        return new BaseResult<>(MsgEnum.ERR_SYSTEM.getCode(), StringUtil.getEnumMessage(MsgEnum.ERR_SYSTEM) + getMessageForTest(ex));
-    }
-
-    /**
-     * 原子层Exception错误异常处理（共用）
-     * @param request
-     * @param response
-     * @param ex
-     * @return
-     */
-    @ExceptionHandler(value = AMException.class)
-    @ResponseBody
-    public BaseResult<?> amExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        logger.error("原子层发生异常(AM)", ex);
-        return new BaseResult<>(MsgEnum.ERR_AM_SYSTEM.getCode(), StringUtil.getEnumMessage(MsgEnum.ERR_AM_SYSTEM) + getMessageForTest(ex));
-    }
-
-    /**
-     * 传入参数类型错误异常处理
-     * @param request
-     * @param response
-     * @param ex
-     * @return
-     * @author liubin
-     */
-    @ExceptionHandler(BindException.class)
-    @ResponseBody
-    public BaseResult<?> bindExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        return new BaseResult<>(MsgEnum.ERR_PARAM_TYPE.getCode(), StringUtil.getEnumMessage(MsgEnum.ERR_PARAM_TYPE));
-    }
-
-    /**
-     * 传入JSON错误异常处理
-     * @param request
-     * @param response
-     * @param ex
-     * @return
-     * @author liubin
-     */
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    @ResponseBody
-    public BaseResult<?> httpMessageNotReadableExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        return new BaseResult<>(MsgEnum.ERR_JSON.getCode(), StringUtil.getEnumMessage(MsgEnum.ERR_JSON));
-    }
-
-    /**
-     * 传入信息验证错误异常处理
-     * @param request
-     * @param response
-     * @param ex
-     * @return
-     * @author liubin
-     */
-    @ExceptionHandler(CheckException.class)
-    @ResponseBody
-    public BaseResult<?> CheckExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        CheckException e = (CheckException)ex;
-        BaseResult<?> result = new BaseResult<>(e.getData());
-        result.setStatusInfo(e.getCode(), ex.getLocalizedMessage());
-        return result;
-    }
-
-    @ExceptionHandler(value = ReturnMessageException.class)
-    @ResponseBody
-    public BaseResult defaultReturnErrorHandler(HttpServletRequest req, ReturnMessageException e) {
-        BaseResult result = new BaseResult<>();
-        result.setStatusInfo(e.getError().getCode(), e.getError().getMsg());
-        return result;
-    }
 
     /**
      *
--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/common/controller/BaseController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/common/controller/BaseController.java
@@ -4,19 +4,8 @@
 package com.hyjf.admin.common.controller;
 
 
-import com.hyjf.admin.common.result.BaseResult;
-import com.hyjf.common.enums.MsgEnum;
-import com.hyjf.common.exception.CheckException;
-import com.hyjf.common.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * 组合层共用Controller基类
@@ -26,62 +15,4 @@ import javax.servlet.http.HttpServletResponse;
 public class BaseController {
     private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
 
-    /**
-     * Exception错误异常处理
-     * @param request
-     * @param response
-     * @param ex
-     * @return
-     */
-    @ExceptionHandler(value = Exception.class)
-    @ResponseBody
-    public BaseResult<?> defaultErrorHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        logger.error("system error", ex);
-        return new BaseResult<>(BaseResult.ERROR, BaseResult.ERROR_DESC);
-    }
-
-    /**
-     * 传入参数类型错误异常处理
-     * @param request
-     * @param response
-     * @param ex
-     * @return
-     * @author liubin
-     */
-    @ExceptionHandler(BindException.class)
-    @ResponseBody
-    public BaseResult<?> bindExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        return new BaseResult<>(MsgEnum.ERR_PARAM_TYPE.getCode(), StringUtil.getEnumMessage(MsgEnum.ERR_PARAM_TYPE));
-    }
-
-    /**
-     * 传入JSON错误异常处理
-     * @param request
-     * @param response
-     * @param ex
-     * @return
-     * @author liubin
-     */
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    @ResponseBody
-    public BaseResult<?> httpMessageNotReadableExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        return new BaseResult<>(MsgEnum.ERR_JSON.getCode(), StringUtil.getEnumMessage(MsgEnum.ERR_JSON));
-    }
-
-    /**
-     * 传入信息验证错误异常处理
-     * @param request
-     * @param response
-     * @param ex
-     * @return
-     * @author liubin
-     */
-    @ExceptionHandler(CheckException.class)
-    @ResponseBody
-    public BaseResult<?> CheckExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        CheckException e = (CheckException)ex;
-        BaseResult<?> result = new BaseResult<>(e.getData());
-        result.setStatusInfo(e.getCode(), ex.getLocalizedMessage());
-        return result;
-    }
 }
