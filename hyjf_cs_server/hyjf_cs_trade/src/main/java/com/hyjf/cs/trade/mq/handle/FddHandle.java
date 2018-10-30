65d2d1908f498f75ddfafe41d5f07336d31588fd
yangchangwei <ycw123456>
2018-10-30 11:21:52 +0800

    法大大协议异常处理

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/handle/FddHandle.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/handle/FddHandle.java
@@ -1930,7 +1930,7 @@ public class FddHandle {
 		}
 		String output = imageSavePath;
 		String source = imageFilePath;    //签章源图片路径
-		String fileUploadTempPath = UploadFileUtils.getDoPath(systemConfig.getFddFileUpload());
+		String fileUploadTempPath = systemConfig.getFddFileUpload();
 		String fileParent = imghost + fileUploadTempPath;
 		logger.info("-----------开始下载脱敏，获得签章图片父级别路径" + fileParent);
 		String signIcon = fileParent + borrowSigntmImage; //签章覆盖图片路径
