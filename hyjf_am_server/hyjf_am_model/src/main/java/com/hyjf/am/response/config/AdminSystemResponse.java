422cc8692b99c5967dfcd9652245bf35b9d70dbd
dongzeshan <dongzeshan@hyjf.com>
2018-11-05 18:07:12 +0800

    代码提交

--- hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/config/AdminSystemResponse.java
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/config/AdminSystemResponse.java
@@ -1,6 +1,7 @@
 package com.hyjf.am.response.config;
 
 import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.config.AdminMenuVO;
 import com.hyjf.am.vo.config.AdminSystemVO;
 
 /**
@@ -8,4 +9,14 @@ import com.hyjf.am.vo.config.AdminSystemVO;
  * @version AccountListResponse, v0.1 2018/6/20 10:52
  */
 public class AdminSystemResponse extends Response<AdminSystemVO> {
+	private AdminMenuVO adminMenu;
+
+	public AdminMenuVO getAdminMenu() {
+		return adminMenu;
+	}
+
+	public void setAdminMenu(AdminMenuVO adminMenu) {
+		this.adminMenu = adminMenu;
+	}
+	
 }
