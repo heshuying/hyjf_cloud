aa317e8e5ebc6dec5ac82ca138d9d0d3c6ba2185
xiasq <mx2012xshq@126.com>
2018-11-06 09:22:23 +0800

    增加测试类

---
+++ hyjf_cs_server/hyjf_cs_message/src/test/java/com/hyjf/cs/message/mongo/ic/AppChannelStatisticsDetailDaoTest.java
@@ -0,0 +1,8 @@
+package com.hyjf.cs.message.mongo.ic;
+
+/**
+ * @author xiasq
+ * @version AppChannelStatisticsDetailDaoTest, v0.1 2018/11/6 9:21
+ */
+public class AppChannelStatisticsDetailDaoTest {
+}
