248ecf7467da44bea74896860e6b1124ae648b96
limeng <limeng@shizitegong.com>
2018-11-06 11:45:27 +0800

    修复优惠券放款BUG

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/consumer/impl/CouponLoansServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/consumer/impl/CouponLoansServiceImpl.java
@@ -447,13 +447,8 @@ public class CouponLoansServiceImpl implements CouponLoansService {
         // 年利率
         BigDecimal borrowApr = borrow.getBorrowApr();
         // 借款成功时间
-        String borrowSuccessTimeString = borrow.getVerifyTime();
-        logger.info("标的："+borrowNid + "放款开始，借款满标时间："+borrowSuccessTimeString);
+        Integer borrowSuccessTime = borrow.getVerifyTimeInteger();
         logger.info("标的："+borrowNid + "放款开始，借款满标时间："+borrow.getVerifyTimeInteger());
-        if (null == borrowSuccessTimeString || "0".equals(borrowSuccessTimeString)){
-            return retMsgList;
-        }
-        Integer borrowSuccessTime = Integer.parseInt(borrowSuccessTimeString);
         // 项目类型
         Integer projectType = borrow.getProjectType();
         // 是否月标(true:月标, false:天标)
