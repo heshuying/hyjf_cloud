94951c815fa857ab18f2ac751ff1d435b7687087
zhangyunkai <zhangyunkai@hyjf.com>
2018-11-09 16:40:07 +0800

    ## 承接信息导出优化

--- hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/vo/admin/BorrowCreditTenderVO.java
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/vo/admin/BorrowCreditTenderVO.java
@@ -68,6 +68,285 @@ public class BorrowCreditTenderVO extends BaseVO implements Serializable {
     /*原来承接订单号*/
     private String creditTenderNid;
 
+
+
+    /**
+     * 出让人用户属性及部门信息
+     */
+    private String recommendAttrCreditSelf;
+    private String regionNameCreditSelf;
+    private String branchNameCreditSelf;
+    private String departmentNameCreditSelf;
+
+
+    /**
+     * 承接人用户属性及部门信息
+     */
+    private String recommendAttrSelf;
+    private String regionNameSelf;
+    private String branchNameSelf;
+    private String departmentNameSelf;
+
+    /**
+     * 承接用户推荐人属性及部门信息
+     */
+    private String recommendName;
+    private String recommendAttr;
+    private String regionName;
+    private String branchName;
+    private String departmentName;
+
+    /**
+     * 出让用户推荐人用户名及部门信息
+     */
+    private String recommendNameCredit;
+    private String recommendAttrCredit;
+    private String regionNameCredit;
+    private String branchNameCredit;
+    private String departmentNameCredit;
+
+    /**
+     * 承接人承接时推荐人信息
+     */
+    private String inviteUserName;
+    private String inviteUserAttribute;
+    private String inviteUseRegionname;
+    private String inviteUserBranchname;
+    private String inviteUserDepartmentName;
+    /**
+     * 出让人承接时推荐人信息
+     */
+    private String inviteUserCreditName;
+    private String inviteUserCreditAttribute;
+    private String inviteUserCreditRegionName;
+    private String inviteUserCreditBranchName;
+    private String inviteUserCreditDepartmentName;
+
+
+    public String getRecommendAttrCreditSelf() {
+        return recommendAttrCreditSelf;
+    }
+
+    public void setRecommendAttrCreditSelf(String recommendAttrCreditSelf) {
+        this.recommendAttrCreditSelf = recommendAttrCreditSelf;
+    }
+
+    public String getRegionNameCreditSelf() {
+        return regionNameCreditSelf;
+    }
+
+    public void setRegionNameCreditSelf(String regionNameCreditSelf) {
+        this.regionNameCreditSelf = regionNameCreditSelf;
+    }
+
+    public String getBranchNameCreditSelf() {
+        return branchNameCreditSelf;
+    }
+
+    public void setBranchNameCreditSelf(String branchNameCreditSelf) {
+        this.branchNameCreditSelf = branchNameCreditSelf;
+    }
+
+    public String getDepartmentNameCreditSelf() {
+        return departmentNameCreditSelf;
+    }
+
+    public void setDepartmentNameCreditSelf(String departmentNameCreditSelf) {
+        this.departmentNameCreditSelf = departmentNameCreditSelf;
+    }
+
+    public String getRecommendAttrSelf() {
+        return recommendAttrSelf;
+    }
+
+    public void setRecommendAttrSelf(String recommendAttrSelf) {
+        this.recommendAttrSelf = recommendAttrSelf;
+    }
+
+    public String getRegionNameSelf() {
+        return regionNameSelf;
+    }
+
+    public void setRegionNameSelf(String regionNameSelf) {
+        this.regionNameSelf = regionNameSelf;
+    }
+
+    public String getBranchNameSelf() {
+        return branchNameSelf;
+    }
+
+    public void setBranchNameSelf(String branchNameSelf) {
+        this.branchNameSelf = branchNameSelf;
+    }
+
+    public String getDepartmentNameSelf() {
+        return departmentNameSelf;
+    }
+
+    public void setDepartmentNameSelf(String departmentNameSelf) {
+        this.departmentNameSelf = departmentNameSelf;
+    }
+
+    public String getRecommendName() {
+        return recommendName;
+    }
+
+    public void setRecommendName(String recommendName) {
+        this.recommendName = recommendName;
+    }
+
+    public String getRecommendAttr() {
+        return recommendAttr;
+    }
+
+    public void setRecommendAttr(String recommendAttr) {
+        this.recommendAttr = recommendAttr;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getRecommendNameCredit() {
+        return recommendNameCredit;
+    }
+
+    public void setRecommendNameCredit(String recommendNameCredit) {
+        this.recommendNameCredit = recommendNameCredit;
+    }
+
+    public String getRecommendAttrCredit() {
+        return recommendAttrCredit;
+    }
+
+    public void setRecommendAttrCredit(String recommendAttrCredit) {
+        this.recommendAttrCredit = recommendAttrCredit;
+    }
+
+    public String getRegionNameCredit() {
+        return regionNameCredit;
+    }
+
+    public void setRegionNameCredit(String regionNameCredit) {
+        this.regionNameCredit = regionNameCredit;
+    }
+
+    public String getBranchNameCredit() {
+        return branchNameCredit;
+    }
+
+    public void setBranchNameCredit(String branchNameCredit) {
+        this.branchNameCredit = branchNameCredit;
+    }
+
+    public String getDepartmentNameCredit() {
+        return departmentNameCredit;
+    }
+
+    public void setDepartmentNameCredit(String departmentNameCredit) {
+        this.departmentNameCredit = departmentNameCredit;
+    }
+
+    public String getInviteUserName() {
+        return inviteUserName;
+    }
+
+    public void setInviteUserName(String inviteUserName) {
+        this.inviteUserName = inviteUserName;
+    }
+
+    public String getInviteUserAttribute() {
+        return inviteUserAttribute;
+    }
+
+    public void setInviteUserAttribute(String inviteUserAttribute) {
+        this.inviteUserAttribute = inviteUserAttribute;
+    }
+
+    public String getInviteUseRegionname() {
+        return inviteUseRegionname;
+    }
+
+    public void setInviteUseRegionname(String inviteUseRegionname) {
+        this.inviteUseRegionname = inviteUseRegionname;
+    }
+
+    public String getInviteUserBranchname() {
+        return inviteUserBranchname;
+    }
+
+    public void setInviteUserBranchname(String inviteUserBranchname) {
+        this.inviteUserBranchname = inviteUserBranchname;
+    }
+
+    public String getInviteUserDepartmentName() {
+        return inviteUserDepartmentName;
+    }
+
+    public void setInviteUserDepartmentName(String inviteUserDepartmentName) {
+        this.inviteUserDepartmentName = inviteUserDepartmentName;
+    }
+
+    public String getInviteUserCreditName() {
+        return inviteUserCreditName;
+    }
+
+    public void setInviteUserCreditName(String inviteUserCreditName) {
+        this.inviteUserCreditName = inviteUserCreditName;
+    }
+
+    public String getInviteUserCreditAttribute() {
+        return inviteUserCreditAttribute;
+    }
+
+    public void setInviteUserCreditAttribute(String inviteUserCreditAttribute) {
+        this.inviteUserCreditAttribute = inviteUserCreditAttribute;
+    }
+
+    public String getInviteUserCreditRegionName() {
+        return inviteUserCreditRegionName;
+    }
+
+    public void setInviteUserCreditRegionName(String inviteUserCreditRegionName) {
+        this.inviteUserCreditRegionName = inviteUserCreditRegionName;
+    }
+
+    public String getInviteUserCreditBranchName() {
+        return inviteUserCreditBranchName;
+    }
+
+    public void setInviteUserCreditBranchName(String inviteUserCreditBranchName) {
+        this.inviteUserCreditBranchName = inviteUserCreditBranchName;
+    }
+
+    public String getInviteUserCreditDepartmentName() {
+        return inviteUserCreditDepartmentName;
+    }
+
+    public void setInviteUserCreditDepartmentName(String inviteUserCreditDepartmentName) {
+        this.inviteUserCreditDepartmentName = inviteUserCreditDepartmentName;
+    }
+
     public String getUserId() {
         return userId;
     }
