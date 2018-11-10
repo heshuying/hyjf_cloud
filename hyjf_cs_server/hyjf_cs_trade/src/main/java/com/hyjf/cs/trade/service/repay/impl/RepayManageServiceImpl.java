2fa168f6a8768acb981a7183faf185636ce3aba4
heshuying <heshuying@hyjf.com>
2018-11-10 14:05:27 +0800

    isallreapy添加

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/repay/impl/RepayManageServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/repay/impl/RepayManageServiceImpl.java
@@ -392,10 +392,11 @@ public class RepayManageServiceImpl extends BaseTradeServiceImpl implements Repa
      * @date: 2018/7/10
      */
     @Override
-    public Boolean updateForRepayRequest(RepayBean repayBean, BankCallBean bankCallBean){
+    public Boolean updateForRepayRequest(RepayBean repayBean, BankCallBean bankCallBean, boolean isAllRepay){
         RepayRequestUpdateRequest requestBean = new RepayRequestUpdateRequest();
         requestBean.setRepayBeanData(JSON.toJSONString(repayBean));
         requestBean.setBankCallBeanData(JSON.toJSONString(bankCallBean));
+        requestBean.setAllRepay(isAllRepay);
         logger.info("updateForRepayRequest 请求参数：" + JSON.toJSONString(repayBean));
         return amTradeClient.repayRequestUpdate(requestBean);
     }
@@ -861,7 +862,7 @@ public class RepayManageServiceImpl extends BaseTradeServiceImpl implements Repa
      * @date: 2018/10/16
      */
     @Override
-    public  WebResult getBalanceFreeze(WebViewUserVO userVO, String borrowNid, RepayBean repayBean, String orderId, String account, WebResult webResult) {
+    public  WebResult getBalanceFreeze(WebViewUserVO userVO, String borrowNid, RepayBean repayBean, String orderId, String account, WebResult webResult, boolean isAllRepay) {
         Integer userId = userVO.getUserId();
         String userName = userVO.getUsername();
         String ip = repayBean.getIp();
@@ -893,7 +894,7 @@ public class RepayManageServiceImpl extends BaseTradeServiceImpl implements Repa
             return webResult;
         }
         //还款后变更数据
-        boolean updateResult = this.updateForRepayRequest(repayBean, callBackBean);
+        boolean updateResult = this.updateForRepayRequest(repayBean, callBackBean, isAllRepay);
         if(updateResult){
             updateResult = this.updateBorrowCreditStautus(borrowNid);
             if(!updateResult){
