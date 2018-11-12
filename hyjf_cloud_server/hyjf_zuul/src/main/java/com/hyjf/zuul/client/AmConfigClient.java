aef7e542ee44ff0a8939029d095610723db7d0b4
xiasq <mx2012xshq@126.com>
2018-11-12 10:15:36 +0800

    添加zuul方法注释

--- hyjf_cloud_server/hyjf_zuul/src/main/java/com/hyjf/zuul/client/AmConfigClient.java
+++ hyjf_cloud_server/hyjf_zuul/src/main/java/com/hyjf/zuul/client/AmConfigClient.java
@@ -9,5 +9,9 @@ import java.util.List;
  * @version AmConfigClient, v0.1 2018/4/19 17:59
  */
 public interface AmConfigClient {
+    /**
+     * 查询动态路由url配置
+     * @return
+     */
     List<GatewayApiConfigVO> findGatewayConfigs();
 }
