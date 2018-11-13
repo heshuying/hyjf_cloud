ba2f8fbad60c745286ca72d8cec8b6d6167f6777
jun <jijunjun@hyjf.com>
2018-11-13 12:53:19 +0800

    日志输出

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/plancenter/AccedeListController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/plancenter/AccedeListController.java
@@ -1109,6 +1109,7 @@ public class AccedeListController extends BaseController{
 		tenderAgreement = tenderAgreementList.get(0);
 		if(tenderAgreement != null && tenderAgreement.getStatus() == 2){
 			// PDF下载加脱敏
+			_log.info("==========汇计划-计划订单PDF签署发送法大大MQ(PDF下载脱敏)=========");
 			this.accedeListService.updateSaveSignInfo(tenderAgreement, "", FddGenerateContractConstant.PROTOCOL_TYPE_PLAN, accede.getDebtPlanNid());
 		} else {
 			FddGenerateContractBeanVO bean = new FddGenerateContractBeanVO();
@@ -1123,7 +1124,7 @@ public class AccedeListController extends BaseController{
 			bean.setTenderInterestFmt(String.valueOf(accede.getWaitTotal()));
 			// 法大大生成合同接口
 			/*this.rabbitTemplate.convertAndSend(RabbitMQConstants.EXCHANGES_NAME, RabbitMQConstants.ROUTINGKEY_GENERATE_CONTRACT, JSONObject.toJSONString(bean));*/
-            _log.info("==========汇计划-计划订单PDF签署发送法大大MQ=========");
+            _log.info("==========汇计划-计划订单PDF签署发送法大大MQ(生成合同)=========");
             fddProducer.messageSend(new MessageContent(MQConstant.FDD_TOPIC,MQConstant.FDD_GENERATE_CONTRACT_TAG, UUID.randomUUID().toString(), JSON.toJSONBytes(bean)));
 		}
 		ret.put("result", "操作成功,签署MQ已发送");
