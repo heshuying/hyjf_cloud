f100875c1826aa521ebfa2120cc5ff52bebcc70a
PC-LIUSHOUYI <liushouyi@hyjf.com>
2018-11-14 19:16:58 +0800

    导出查询语句修改

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/exception/assetexception/AssetExceptionController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/exception/assetexception/AssetExceptionController.java
@@ -313,7 +313,7 @@ public class AssetExceptionController extends BaseController {
 	        // 表格sheet名称
 	        String sheetName = "项目异常列表";
 	        // 文件名称
-	        String fileName = URLEncoder.encode(sheetName, CustomConstants.UTF8) + StringPool.UNDERLINE + GetDate.getServerDateTime(8, new Date()) + ".xls";
+	        String fileName = URLEncoder.encode(sheetName, CustomConstants.UTF8) + StringPool.UNDERLINE + GetDate.getServerDateTime(8, new Date()) + ".xlsx";
 	        // 声明一个工作薄
 	        SXSSFWorkbook workbook = new SXSSFWorkbook(SXSSFWorkbook.DEFAULT_WINDOW_SIZE);
 	        DataSet2ExcelSXSSFHelper helper = new DataSet2ExcelSXSSFHelper();
@@ -361,8 +361,8 @@ public class AssetExceptionController extends BaseController {
 	        IValueFormatter exceptionTypeAdapter = new IValueFormatter() {
 	            @Override
 	            public String format(Object object) {
-	                String exceptionType = (String) object;
-	                if("0".equals(exceptionType)) {
+	                Integer exceptionType = (Integer) object;
+	                if(exceptionType == 0) {
 	                	return "流标";
 	                }else {
 	                	return "删标";
