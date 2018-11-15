3695b38a1ae44bd96513e7f0ceee04fb656048fc
yangchangwei <ycw123456>
2018-11-15 17:20:13 +0800

    法大大服务拆分

--- hyjf_platform_server/hyjf_signatures/src/main/java/com/hyjf/signatrues/SignApplication.java
+++ hyjf_platform_server/hyjf_signatures/src/main/java/com/hyjf/signatrues/SignApplication.java
@@ -2,9 +2,11 @@ package com.hyjf.signatrues;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +17,8 @@ import org.springframework.web.client.RestTemplate;
 @CrossOrigin(origins = "*")
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
+@ComponentScan(basePackages = "com.hyjf")
 public class SignApplication {
 
     public static void main(String[] args) {
