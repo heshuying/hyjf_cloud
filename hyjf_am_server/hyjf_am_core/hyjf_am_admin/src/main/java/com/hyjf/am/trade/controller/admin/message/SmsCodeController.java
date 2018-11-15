c521ae400594900c0e3c5972e608ea26e3e0f8c5
lisheng <lisheng1919@outlook.com>
2018-11-15 14:40:20 +0800

    迁移千乐数据统计到admin工程

--- hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/trade/controller/admin/message/SmsCodeController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/trade/controller/admin/message/SmsCodeController.java
@@ -3,8 +3,12 @@
  */
 package com.hyjf.am.trade.controller.admin.message;
 
+import com.alibaba.fastjson.JSONObject;
+import com.hyjf.am.response.IntegerResponse;
 import com.hyjf.am.response.admin.SmsCodeCustomizeResponse;
+import com.hyjf.am.response.user.SmsCodeResponse;
 import com.hyjf.am.resquest.admin.SmsCodeUserRequest;
+import com.hyjf.am.resquest.user.SmsCodeRequest;
 import com.hyjf.am.trade.controller.BaseController;
 import com.hyjf.am.user.dao.model.customize.SmsCodeCustomize;
 import com.hyjf.am.user.service.admin.message.SmsCodeService;
@@ -14,8 +18,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -41,6 +47,36 @@ public class SmsCodeController extends BaseController {
             List<SmsCodeCustomizeVO> voList = CommonUtils.convertBeanList(list, SmsCodeCustomizeVO.class);
             response.setResultList(voList);
         }
+        return response;
+    }
+    /**
+     * 校验千乐登录验证码
+     * @param request
+     * @return
+     */
+    @RequestMapping("/qianle_check")
+    public IntegerResponse qianleCheckMobileCode(@RequestBody @Valid SmsCodeRequest request) {
+        IntegerResponse response = new IntegerResponse();
+        logger.info("onlyCheckMobileCode...param is :{}", JSONObject.toJSONString(request));
+        String mobile = request.getMobile();
+        String verificationCode = request.getVerificationCode();
+        int i = smsCodeService.checkQianleMobileCode(mobile, verificationCode);
+        response.setResultInt(i);
+        return response;
+    }
+
+    /**
+     * 短信验证码保存
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public SmsCodeResponse saveSmsCode(@RequestBody @Valid SmsCodeRequest request) {
+        SmsCodeResponse response = new SmsCodeResponse();
+        int cnt = smsCodeService.save(request.getMobile(), request.getVerificationType(), request.getVerificationCode(),
+                request.getPlatform(), request.getStatus());
+        response.setCnt(cnt);
         return response;
     }
 }
