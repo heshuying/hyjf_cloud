5628ff442fbefd976b8c182b5c51da2bc6d6456d
fuqiang <18251359382@163.com>
2018-11-19 09:34:37 +0800

    修改短信群发

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/message/SmsCodeController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/message/SmsCodeController.java
@@ -175,23 +175,6 @@ public class SmsCodeController extends BaseController {
                 }
             }
 
-            // 在筛选条件下查询出用户
-            List<SmsCodeCustomizeVO> smslist = smsCodeService.queryUser(form);
-            if (!CollectionUtils.isEmpty(smslist)) {
-                String phones = "";
-                for (int z = 0; z < smslist.size(); z++) {
-                    if (StringUtils.isNotEmpty(smslist.get(z).getUser_phones())
-                            && Validator.isPhoneNumber(smslist.get(z).getUser_phones())) {
-                        if (z < smslist.size() - 1) {
-                            phones += smslist.get(z).getUser_phones() + ",";
-                        } else {
-                            phones += smslist.get(z).getUser_phones();
-                        }
-                    }
-                }
-                form.setUser_phones(phones);
-            }
-
             flag = smsCodeService.sendSmsOntime(form);
             if (flag) {
                 jsonObject.put("success", true);
