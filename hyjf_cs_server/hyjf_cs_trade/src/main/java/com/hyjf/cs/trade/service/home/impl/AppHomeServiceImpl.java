699946455348891cf4012c1976dd3e09dd8b2f32
yinhui <yinhui@shizitegong.com>
2018-11-21 13:56:58 +0800

    协议添加 《服务协议》/《隐私政策》

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/home/impl/AppHomeServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/home/impl/AppHomeServiceImpl.java
@@ -251,6 +251,9 @@ public class AppHomeServiceImpl implements AppHomeService {
         this.createPopImgPage(info, uniqueIdentifier);
         this.createForceUpdateInfo(info, platform, version, HOST);
 
+        info.put("serviceAgreementUrl","/public/agreement?name=fwxy");//服务协议
+        info.put("privacyPolicyUrl","/public/agreement?name=yszc");//隐私政策
+
         info.put(CustomConstants.APP_STATUS, CustomConstants.APP_STATUS_SUCCESS);
         info.put(CustomConstants.APP_STATUS_DESC, CustomConstants.APP_STATUS_DESC_SUCCESS);
         info.put(CustomConstants.APP_REQUEST,
