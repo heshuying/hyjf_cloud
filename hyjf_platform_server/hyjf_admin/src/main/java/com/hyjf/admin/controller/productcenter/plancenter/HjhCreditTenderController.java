ea878a985e371ceff70ebc626d3c546d3f3b011d
jun <jijunjun@hyjf.com>
2018-12-05 11:52:30 +0800

    项目期数

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/plancenter/HjhCreditTenderController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/plancenter/HjhCreditTenderController.java
@@ -364,7 +364,7 @@ public class HjhCreditTenderController extends BaseController{
 		map.put("assignServiceApr","债转服务费率");
 		map.put("assignServiceFee","债转服务费(元)");
 		map.put("tenderType", "复投承接(是/否)");
-		map.put("borrowPeriod", "项目期数");
+		map.put("projectPeriod", "项目期数");
 
 		return map;
 	}
