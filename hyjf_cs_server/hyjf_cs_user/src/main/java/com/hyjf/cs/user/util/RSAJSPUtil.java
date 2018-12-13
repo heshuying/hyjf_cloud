7b565d6cfffb3fab81247257439ee8745b2ad8a2
xiashuqing <xiashuqing@shizitegong.com>
2018-12-13 10:25:00 +0800

    删除多余工具类

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/util/RSAJSPUtil.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/util/RSAJSPUtil.java
@@ -134,24 +134,32 @@ public class RSAJSPUtil {
 		}
 	}
 
+	private static volatile Cipher encryptCipher;
+
 	public static byte[] encrypt(PublicKey pk, byte[] data) throws Exception {
+
+		synchronized (RSAJSPUtil.class){
+			if(encryptCipher == null){
+				encryptCipher = Cipher.getInstance("RSA", new org.bouncycastle.jce.provider.BouncyCastleProvider());
+			}
+		}
+
 		try {
-			Cipher cipher = Cipher.getInstance("RSA", new org.bouncycastle.jce.provider.BouncyCastleProvider());
-			cipher.init(Cipher.ENCRYPT_MODE, pk);
-			int blockSize = cipher.getBlockSize();// 获得加密块大小，如：加密前数据为128个byte，而key_size=1024
+			encryptCipher.init(Cipher.ENCRYPT_MODE, pk);
+			int blockSize = encryptCipher.getBlockSize();// 获得加密块大小，如：加密前数据为128个byte，而key_size=1024
 			// 加密块大小为127
 			// byte,加密后为128个byte;因此共有2个加密块，第一个127
 			// byte第二个为1个byte
-			int outputSize = cipher.getOutputSize(data.length);// 获得加密块加密后块大小
+			int outputSize = encryptCipher.getOutputSize(data.length);// 获得加密块加密后块大小
 			int leavedSize = data.length % blockSize;
 			int blocksSize = leavedSize != 0 ? data.length / blockSize + 1 : data.length / blockSize;
 			byte[] raw = new byte[outputSize * blocksSize];
 			int i = 0;
 			while (data.length - i * blockSize > 0) {
 				if (data.length - i * blockSize > blockSize) {
-					cipher.doFinal(data, i * blockSize, blockSize, raw, i * outputSize);
+					encryptCipher.doFinal(data, i * blockSize, blockSize, raw, i * outputSize);
 				} else {
-                    cipher.doFinal(data, i * blockSize, data.length - i * blockSize, raw, i * outputSize);
+					encryptCipher.doFinal(data, i * blockSize, data.length - i * blockSize, raw, i * outputSize);
                 }
 				// 这里面doUpdate方法不可用，查看源代码后发现每次doUpdate后并没有什么实际动作除了把byte[]放到
 				// ByteArrayOutputStream中，而最后doFinal的时候才将所有的byte[]进行加密，可是到了此时加密块大小很可能已经超出了
