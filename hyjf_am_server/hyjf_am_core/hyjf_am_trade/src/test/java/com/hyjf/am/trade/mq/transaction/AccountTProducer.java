93525ac3f7b550aa8247e0d91fa347b4648b861b
yinhui <yinhui@shizitegong.com>
2018-12-17 16:43:11 +0800

    mq删除

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/test/java/com/hyjf/am/trade/mq/transaction/AccountTProducer.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/test/java/com/hyjf/am/trade/mq/transaction/AccountTProducer.java
@@ -1,51 +1,51 @@
-package com.hyjf.am.trade.mq.transaction;
-
-import com.alibaba.fastjson.JSON;
-import com.hyjf.common.exception.MQException;
-import org.apache.rocketmq.client.producer.LocalTransactionState;
-import org.apache.rocketmq.common.message.Message;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-/**
- * @author xiasq
- * @version AccountTProducer, v0.1 2018/6/26 14:59
- */
-@Component
-public class AccountTProducer extends TransactionProducer {
-	private Logger logger = LoggerFactory.getLogger(AccountTProducer.class);
-
-	@Override
-	protected ProducerFieldsWrapper getFieldsWrapper() {
-		ProducerFieldsWrapper wrapper = new ProducerFieldsWrapper();
-		wrapper.setGroup("user_transaction_group_test");
-		wrapper.setInstanceName(String.valueOf(System.currentTimeMillis()));
-		return wrapper;
-	}
-
-	public boolean send(MassageContent messageContent) throws MQException {
-		LocalTransactionState localTransactionState = super.messageSend(messageContent);
-		if (localTransactionState == LocalTransactionState.COMMIT_MESSAGE) {
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	protected LocalTransactionState doExecuteLocalTransaction(Message message, Object obj) {
-		Integer userId = JSON.parseObject(message.getBody(), Integer.class);
-		if (userId == null) {
-			logger.error("事务消息发送失败， 参数解析错误...");
-			return LocalTransactionState.ROLLBACK_MESSAGE;
-		}
-		try {
-			// 执行本地事务
-			//transactionService.updateAmount(userId);
-		} catch (Exception e) {
-			logger.error("本地事务执行失败....", e);
-			return LocalTransactionState.ROLLBACK_MESSAGE;
-		}
-		return LocalTransactionState.COMMIT_MESSAGE;
-	}
-}
+//package com.hyjf.am.trade.mq.transaction;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.hyjf.common.exception.MQException;
+//import org.apache.rocketmq.client.producer.LocalTransactionState;
+//import org.apache.rocketmq.common.message.Message;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author xiasq
+// * @version AccountTProducer, v0.1 2018/6/26 14:59
+// */
+//@Component
+//public class AccountTProducer extends TransactionProducer {
+//	private Logger logger = LoggerFactory.getLogger(AccountTProducer.class);
+//
+//	@Override
+//	protected ProducerFieldsWrapper getFieldsWrapper() {
+//		ProducerFieldsWrapper wrapper = new ProducerFieldsWrapper();
+//		wrapper.setGroup("user_transaction_group_test");
+//		wrapper.setInstanceName(String.valueOf(System.currentTimeMillis()));
+//		return wrapper;
+//	}
+//
+//	public boolean send(MassageContent messageContent) throws MQException {
+//		LocalTransactionState localTransactionState = super.messageSend(messageContent);
+//		if (localTransactionState == LocalTransactionState.COMMIT_MESSAGE) {
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	@Override
+//	protected LocalTransactionState doExecuteLocalTransaction(Message message, Object obj) {
+//		Integer userId = JSON.parseObject(message.getBody(), Integer.class);
+//		if (userId == null) {
+//			logger.error("事务消息发送失败， 参数解析错误...");
+//			return LocalTransactionState.ROLLBACK_MESSAGE;
+//		}
+//		try {
+//			// 执行本地事务
+//			//transactionService.updateAmount(userId);
+//		} catch (Exception e) {
+//			logger.error("本地事务执行失败....", e);
+//			return LocalTransactionState.ROLLBACK_MESSAGE;
+//		}
+//		return LocalTransactionState.COMMIT_MESSAGE;
+//	}
+//}
