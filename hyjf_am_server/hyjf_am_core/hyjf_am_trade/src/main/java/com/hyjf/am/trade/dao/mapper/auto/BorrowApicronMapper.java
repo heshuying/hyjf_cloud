ab9b4998b967240b5e3ea947201aeee3a2be1633
yangchangwei <ycw123456>
2018-12-26 10:11:27 +0800

    合规协议2.0提交自动mapper

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/dao/mapper/auto/BorrowApicronMapper.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/dao/mapper/auto/BorrowApicronMapper.java
@@ -2,9 +2,8 @@ package com.hyjf.am.trade.dao.mapper.auto;
 
 import com.hyjf.am.trade.dao.model.auto.BorrowApicron;
 import com.hyjf.am.trade.dao.model.auto.BorrowApicronExample;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface BorrowApicronMapper {
     int countByExample(BorrowApicronExample example);
