62875fa219b3368357e93e11a06d58bdc808df51
wangjun <wangjun@hyjf.com>
2018-12-27 15:58:37 +0800

    MQ幂等性测试

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/consumer/CrmInvestMessageConsumer.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/consumer/CrmInvestMessageConsumer.java
@@ -11,7 +11,6 @@ import com.hyjf.am.vo.trade.borrow.RightBorrowVO;
 import com.hyjf.am.vo.trade.hjh.HjhAccedeVO;
 import com.hyjf.am.vo.trade.hjh.HjhPlanVO;
 import com.hyjf.am.vo.user.UserInfoVO;
-import com.hyjf.common.constants.MQConstant;
 import com.hyjf.common.util.CustomConstants;
 import com.hyjf.common.util.GetDate;
 import com.hyjf.cs.common.service.BaseClient;
@@ -30,7 +29,6 @@ import org.apache.http.util.EntityUtils;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
-import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.apache.rocketmq.spring.core.RocketMQPushConsumerLifecycleListener;
 import org.slf4j.Logger;
@@ -42,7 +40,7 @@ import java.io.IOException;
 import java.util.Map;
 
 @Service
-@RocketMQMessageListener(topic = MQConstant.CRM_TENDER_INFO_TOPIC, selectorExpression = "*", consumerGroup = MQConstant.BORROW_GROUP)
+//@RocketMQMessageListener(topic = MQConstant.CRM_TENDER_INFO_TOPIC, selectorExpression = "*", consumerGroup = MQConstant.BORROW_GROUP)
 public class CrmInvestMessageConsumer implements RocketMQListener<MessageExt>, RocketMQPushConsumerLifecycleListener {
 
     private static final Logger logger = LoggerFactory.getLogger(CrmInvestMessageConsumer.class);
