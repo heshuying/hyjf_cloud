63f300478a8245995fdd37ddf4e3ec8c982fae68
yangchangwei <ycw123456>
2018-12-29 15:57:01 +0800

    合规协议2.0

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/batch/FddPushController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/batch/FddPushController.java
@@ -9,10 +9,7 @@ import com.hyjf.am.vo.trade.BorrowRecoverPlanVO;
 import com.hyjf.am.vo.trade.borrow.BorrowApicronVO;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,13 +36,13 @@ public class FddPushController {
         return response;
     }
 
-    @GetMapping("/updateFddPush")
+    @PostMapping("/updateFddPush")
     public void updateFddPush(@RequestBody BorrowApicronRequest request){
 
         fddPushService.updateFddPush(request);
     }
 
-    @GetMapping("/getBorrowRecoverPlanByNidandPeriod")
+    @PostMapping("/getBorrowRecoverPlanByNidandPeriod")
     public BorrowRecoverPlanResponse getBorrowRecoverPlanByNidandPeriod(@RequestBody BorrowRecoverPlanVO planinfo){
 
         BorrowRecoverPlanResponse response = new BorrowRecoverPlanResponse();
