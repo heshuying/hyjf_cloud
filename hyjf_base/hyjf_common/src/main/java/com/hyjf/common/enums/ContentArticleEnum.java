25958040b33433fb4a3c90fb099758f6d3e1932f
xiashuqing <xiashuqing@shizitegong.com>
2019-01-07 09:44:46 +0800

    优化: 删除无效引用、代码

--- hyjf_base/hyjf_common/src/main/java/com/hyjf/common/enums/ContentArticleEnum.java
+++ hyjf_base/hyjf_common/src/main/java/com/hyjf/common/enums/ContentArticleEnum.java
@@ -1,5 +1,7 @@
 package com.hyjf.common.enums;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * 文章分类
  * @author：yinhui
@@ -7,29 +9,30 @@ package com.hyjf.common.enums;
  */
 public enum ContentArticleEnum {
     //2-网站公告，3-网贷知识，5-关于我们，101-风险教育，8-联系我们，20-公司动态
-    WZGG("2","网站公告"),
-    WDZS("3","网贷知识"),
-    GYWM("5","关于我们"),
-    FXJY("101","风险教育"),
-    LXWM("8","联系我们"),
-    GSDT("20","公司动态");
+    WEB_NOTICE("2","网站公告"),
+    NET_LOAN_TIPS("3","网贷知识"),
+    ABOUT_UP("5","关于我们"),
+    CONTACT_US("8","联系我们"),
+    COMPANY_NEWS("20","公司动态"),
+    RISK_TIPS("101","风险教育");
 
-    private String type;//
-    private String name;//
+    private String type;
+    private String name;
 
     ContentArticleEnum(String type, String name) {
         this.type = type;
         this.name = name;
     }
 
-    public static String getName(String type){
+	public static String getName(String type) {
+		if (StringUtils.isNotBlank(type)) {
+			for (ContentArticleEnum e : ContentArticleEnum.values()) {
 
-        for (ContentArticleEnum e:ContentArticleEnum.values()){
-
-            if(type.equals(e.type)){
-                return e.name;
-            }
-        }
-        return null;
-    }
+				if (type.equals(e.type)) {
+					return e.name;
+				}
+			}
+		}
+		return "";
+	}
 }
