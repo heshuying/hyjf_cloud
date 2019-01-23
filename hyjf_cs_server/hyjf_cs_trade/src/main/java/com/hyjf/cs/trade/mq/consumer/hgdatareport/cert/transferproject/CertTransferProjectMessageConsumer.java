3eb944ad0318f1de9d2737728c72b51765f2b389
pangchengchao <pangchengchao@hyjf.com>
2019-01-23 10:38:02 +0800

    cert转让项目信息上报

---
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/consumer/hgdatareport/cert/transferproject/CertTransferProjectMessageConsumer.java
@@ -0,0 +1,109 @@
+package com.hyjf.cs.trade.mq.consumer.hgdatareport.cert.transferproject;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.hyjf.am.vo.hgreportdata.cert.CertReportEntityVO;
+import com.hyjf.common.constants.MQConstant;
+import com.hyjf.common.util.CustomConstants;
+import com.hyjf.cs.trade.mq.consumer.hgdatareport.cert.common.CertCallConstant;
+import com.hyjf.cs.trade.service.consumer.hgdatareport.cert.transferproject.CertTransferProjectService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.apache.rocketmq.spring.core.RocketMQPushConsumerLifecycleListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description 
+ * @Author pangchengchao
+ * @Version v0.1
+ * @Date
+ */
+@Service
+@RocketMQMessageListener(topic = MQConstant.HJH_COUPON_LOAN_TOPIC, selectorExpression = "*", consumerGroup = MQConstant.HJH_COUPON_LOAN_GROUP)
+public class CertTransferProjectMessageConsumer implements RocketMQListener<MessageExt>, RocketMQPushConsumerLifecycleListener {
+    Logger logger = LoggerFactory.getLogger(CertTransferProjectMessageConsumer.class);
+
+    private String thisMessName = "转让项目信息";
+    private String logHeader = "【" + CustomConstants.HG_DATAREPORT + CustomConstants.UNDERLINE + CustomConstants.HG_DATAREPORT_CERT + " " + thisMessName + "】";
+
+
+    @Autowired
+    private CertTransferProjectService certTransferProjectService;
+
+    @Override
+    public void prepareStart(DefaultMQPushConsumer defaultMQPushConsumer) {
+        // 设置Consumer第一次启动是从队列头部开始消费还是队列尾部开始消费
+        // 如果非第一次启动，那么按照上次消费的位置继续消费
+        defaultMQPushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+        // 设置为集群消费(区别于广播消费)
+        defaultMQPushConsumer.setMessageModel(MessageModel.CLUSTERING);
+        logger.info("====CertTransferProjectMessageConsumer start=====");
+    }
+
+    @Override
+    public void onMessage(MessageExt paramBean) {
+        logger.info(logHeader + " 开始。");
+        String msgBody = new String(paramBean.getBody());
+
+        JSONObject jsonObject;
+        try {
+            jsonObject = JSONObject.parseObject(msgBody);
+        } catch (Exception e) {
+            logger.error(logHeader + "解析消息体失败！！！", e);
+            return;
+        }
+
+        String creditNid = jsonObject.getString("creditNid");
+        String flag = jsonObject.getString("flag");
+        String tradeDate = jsonObject.getString("tradeDate");
+
+        if (StringUtils.isBlank(creditNid)) {
+            logger.error(logHeader + "通知参数不全！！！");
+            return;
+        }
+
+        // 检查redis的值是否允许运行 允许返回true  不允许返回false
+        boolean canRun = certTransferProjectService.checkCanRun();
+        if(!canRun){
+            logger.info(logHeader + "redis不允许上报！");
+            return;
+        }
+
+        // --> 消息处理
+        try {
+            // --> 增加防重校验（根据不同平台不同上送方式校验不同）
+
+            // --> 调用service组装数据
+            JSONArray data =certTransferProjectService.createDate(creditNid,flag);
+            // 上送数据
+            CertReportEntityVO entity = new CertReportEntityVO(thisMessName, CertCallConstant.CERT_INF_TYPE_TRANSFER_PROJECT, creditNid, data);
+            try {
+                // 掉单用
+                if(tradeDate!=null&&!"".equals(tradeDate)){
+                    entity.setTradeDate(tradeDate);
+                }
+                entity = certTransferProjectService.insertAndSendPost(entity);
+            } catch (Exception e) {
+                throw e;
+            }
+            logger.info(logHeader + " 处理成功。" + msgBody);
+        } catch (Exception e) {
+            // 错误时，以下日志必须出力（预警捕捉点）
+            logger.error(logHeader + " 处理失败！！" + msgBody, e);
+        } finally {
+            logger.info(logHeader + " 结束。");
+        }
+    }
+}
