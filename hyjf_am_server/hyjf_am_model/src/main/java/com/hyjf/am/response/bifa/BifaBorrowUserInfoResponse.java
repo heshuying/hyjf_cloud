d07846be1f9c9a79dc1e98def45949f93284faf2
jun <jijunjun@hyjf.com>
2019-01-25 16:53:21 +0800

    北互金上報相關

---
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/bifa/BifaBorrowUserInfoResponse.java
@@ -0,0 +1,14 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.am.response.bifa;
+
+import com.hyjf.am.response.Response;
+import com.hyjf.am.vo.trade.bifa.BifaBorrowUserInfoVO;
+
+/**
+ * @author jun
+ * @version BifaBorrowUserInfoResponse, v0.1 2019/1/16 10:45
+ */
+public class BifaBorrowUserInfoResponse extends Response<BifaBorrowUserInfoVO> {
+}
