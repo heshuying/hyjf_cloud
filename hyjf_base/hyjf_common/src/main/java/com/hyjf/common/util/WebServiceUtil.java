6f019472db73f15d1ed4103ed8c523de7b3454e4
jun <jijunjun@hyjf.com>
2019-01-26 09:47:48 +0800

    北互金删除证书地址

--- hyjf_base/hyjf_common/src/main/java/com/hyjf/common/util/WebServiceUtil.java
+++ hyjf_base/hyjf_common/src/main/java/com/hyjf/common/util/WebServiceUtil.java
@@ -22,15 +22,13 @@ public class WebServiceUtil {
 
     /**
      * webService调用接口
-     *
-     * @param crtPath https协议问题证书存放地址
      * @param endPoint wsdl地址
      * @param nameSpace wsdl返回xml中的namespace
      * @param methodName wsdl中的方法名称
      * @param params 参数集合
      * @param objects 需要传入的参数
      */
-     public static String webService(String crtPath,String endPoint,String nameSpace,String methodName,String[] params,Object[] objects){
+     public static String webService(String endPoint,String nameSpace,String methodName,String[] params,Object[] objects){
         try {
 
             // wsdl地址
@@ -89,15 +87,14 @@ public class WebServiceUtil {
     }
 
     public static void main(String[] args) throws IOException {
-        String crtPath = "D:\\Cloud\\WebService\\jssecacerts";
         String endPoint = "https://test.bjp2p.com.cn:8443/platformService?wsdl";
         String nameSpace = "http://supervise.service.app.mp.zkbc.net/";
         String[] params = {"arg0", "arg1", "arg2"};
         String methodName = "productRegistration";
         String loginName = "huiyingjingfu@20181101";
         String passWord = "huiyingjinfu@#20181101";
-        String encMsg = "{\"loan_type\": \"信用/抵质押 \",\"remark\": \"测试标\",\"product_name\": \"心意贷\",\"collateral_info\": \"3月 \",\"project_source\": \"线上\",\"amount\": \"80000\",\"rate\": \"0.001 \",\"pay_type\": \"1\",\"product_reg_type\": \"01\",\"service_cost\": \"1000 \",\"loan_credit_rating\": \"A-F \",\"amount_limmts\": \"1\",\"product_mark\": \"企业经营贷\",\"amount_limmtl\": \"10000\",\"allow_transfer\": \"0 \",\"close_limmit\": \"12\",\"source_product_code\": \"XMF0011\",\"term_type\": \"年/月/天 \",\"source_code\": \"BJ20170831001\",\"bad_debt_limmit\": \"4月 \",\"term\": \"12 \",\"collateral_desc\": \"押品价值很高 \",\"borrow_sex\": \"女\",\"security_info\": \"4月 \",\"source_product_url\": \"http://www.nmyjd.com/invest/a20151100034.html\",\"security_type\": \"抵押担保 \",\"borrow_name_idcard_digest\": \"9113667bb95e7f109a9df2fef304bd5b223e188fd306f3a20d6e89e6be08424e\",\"overdue_limmit\": \"4月 \",\"risk_margin\": \"4000 \"}\n";
-        String result = webService(crtPath, endPoint, nameSpace, methodName, params, new Object[]{loginName, passWord, encMsg});
+        String encMsg = "{\"allow_transfer\":\"0\",\"amount\":\"50000.00\",\"amount_limmtl\":\"100000000\",\"amount_limmts\":\"100\",\"bad_debt_limmit\":\"3月\",\"borrow_name_idcard_digest\":\"75259f1748aeabd2fbc8c4e072ee9ab9ba9da742f01606891213a3bd4d51af96\",\"borrow_sex\":\"男\",\"close_limmit\":\"0\",\"collateral_desc\":\"\",\"collateral_info\":\"\",\"createTime\":1548409429077,\"loan_credit_rating\":\"AA\",\"loan_type\":\"抵质押\",\"overdue_limmit\":\"到期还款日当天24点未提交还款的标的\",\"pay_type\":\"4\",\"product_mark\":\"散标\",\"product_name\":\"放款异常测试1\",\"product_reg_type\":\"01\",\"project_source\":\"合作机构推荐\",\"rate\":\"0.005833\",\"risk_margin\":\"0\",\"security_info\":\"\",\"security_type\":\"抵押\",\"service_cost\":\"2300.00\",\"source_code\":\"BJ20181101001\",\"source_product_code\":\"HCD19010005\",\"source_product_url\":\"https://www.hyjf.com/bank/web/borrow/getBorrowDetail.do?borrowNid=HCD19010005\",\"term\":\"4\",\"term_type\":\"月\",\"updateTime\":1548409429077}";
+        String result = webService(endPoint, nameSpace, methodName, params, new Object[]{loginName, passWord, encMsg});
         System.out.println(result);
     }
 
