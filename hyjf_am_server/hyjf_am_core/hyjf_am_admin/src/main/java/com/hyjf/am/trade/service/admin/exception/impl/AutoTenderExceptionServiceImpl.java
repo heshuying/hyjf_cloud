75d242e3272abeb84557865eeaf38cc24d71e638
heshuying <heshuying@hyjf.com>
2019-02-01 10:49:35 +0800

    自动投标债转标加入异常测试代码模拟异常情况回退

--- hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/trade/service/admin/exception/impl/AutoTenderExceptionServiceImpl.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/trade/service/admin/exception/impl/AutoTenderExceptionServiceImpl.java
@@ -910,6 +910,8 @@ public class AutoTenderExceptionServiceImpl extends BaseServiceImpl implements A
             return creditTenderFlag;
         }
 
+        int a = 1/0;
+
         // 删除临时表 OK
         deleteHjhPlanBorrowTmpByAccedeBorrow(credit.getCreditNid(), hjhAccede.getAccedeOrderId());
 
