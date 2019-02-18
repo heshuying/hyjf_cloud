26b5a91ac8c337cfd75b1fccdf05f0184b172e0d
limeng <466965272@qq.com>
2019-02-18 14:44:14 +0800

    优化微服务获取优惠券代码

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/front/coupon/impl/MyCouponListServiceImpl.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/front/coupon/impl/MyCouponListServiceImpl.java
@@ -507,7 +507,7 @@ public class MyCouponListServiceImpl extends BaseServiceImpl implements MyCoupon
         Borrow borrow = borrowService.getBorrowByNid(borrowNid);
         BorrowInfo borrowInfo = borrowInfoService.getBorrowInfo(borrowNid);
         BorrowProjectType borrowProjectType = borrowProjectTypeService.getProjectTypeByBorrowNid(borrowNid);
-        String style = borrow.getBorrowStyle();
+        String style = borrow != null?borrow.getBorrowStyle():null;
         // 加息券是否启用 0禁用 1启用
         Integer couponFlg = borrowInfo.getBorrowInterestCoupon();
         // 体验金是否启用 0禁用 1启用
