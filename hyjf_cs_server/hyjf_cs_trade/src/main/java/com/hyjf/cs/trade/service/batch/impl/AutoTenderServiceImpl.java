167da8f2da82aeb989c12aa6fd1c69c23fb8e592
liubin <liu'bin>
2019-02-24 14:38:48 +0800

    【智投自动出借任务】test9

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/batch/impl/AutoTenderServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/batch/impl/AutoTenderServiceImpl.java
@@ -351,7 +351,7 @@ public class AutoTenderServiceImpl extends BaseTradeServiceImpl implements AutoT
                         throw new Exception(logMsgHeader + "/finally推回队列/ 计算计划债转实际金额 和 保存creditTenderLog表失败，计划订单号：" + hjhAccede.getAccedeOrderId());
                     }
 
-                    resultVO.setServiceApr(new BigDecimal(1.01)); // testtesttest
+                    resultVO.setAssignPay(new BigDecimal(103.26)); // testtesttest
 
                     //承接支付金额
                     BigDecimal assignPay = resultVO.getAssignPay();
