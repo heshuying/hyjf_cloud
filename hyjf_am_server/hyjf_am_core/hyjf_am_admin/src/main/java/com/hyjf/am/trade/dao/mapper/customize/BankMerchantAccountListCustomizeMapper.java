878f57e419542adfb92b0ca75989e8a917856ea3
zhangqingqing <zhangqingqing@hyjf.com>
2019-02-28 17:33:17 +0800

    红包账户sql修改

--- hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/trade/dao/mapper/customize/BankMerchantAccountListCustomizeMapper.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/trade/dao/mapper/customize/BankMerchantAccountListCustomizeMapper.java
@@ -1,32 +1,13 @@
 package com.hyjf.am.trade.dao.mapper.customize;
 
-import com.hyjf.am.trade.dao.model.auto.BankMerchantAccountList;
-import com.hyjf.am.trade.dao.model.auto.BankMerchantAccountListExample;
+import com.hyjf.am.resquest.admin.BankRedPacketAccountListRequest;
 import com.hyjf.am.trade.dao.model.customize.BankMerchantAccountListCustomize;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface BankMerchantAccountListCustomizeMapper {
-    int countByExample(BankMerchantAccountListExample example);
+    List<BankMerchantAccountListCustomize> selectRecordList(BankRedPacketAccountListRequest form);
 
-    int deleteByExample(BankMerchantAccountListExample example);
+    int countByExample(BankRedPacketAccountListRequest form);
 
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(BankMerchantAccountList record);
-
-    int insertSelective(BankMerchantAccountList record);
-
-    List<BankMerchantAccountListCustomize> selectByExample(BankMerchantAccountListExample example);
-
-    BankMerchantAccountListCustomize selectByPrimaryKey(Integer id);
-
-    int updateByExampleSelective(@Param("record") BankMerchantAccountList record, @Param("example") BankMerchantAccountListExample example);
-
-    int updateByExample(@Param("record") BankMerchantAccountList record, @Param("example") BankMerchantAccountListExample example);
-
-    int updateByPrimaryKeySelective(BankMerchantAccountList record);
-
-    int updateByPrimaryKey(BankMerchantAccountList record);
 }
