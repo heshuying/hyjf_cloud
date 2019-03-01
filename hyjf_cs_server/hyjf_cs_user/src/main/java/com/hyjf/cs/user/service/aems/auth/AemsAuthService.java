ee7c983f89341e60776511f0319c6b5d94aa146e
dangzewen <dangzewen@shizitegong.com>
2019-03-01 16:10:59 +0800

    AEMS多合一授权改造

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/service/aems/auth/AemsAuthService.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/service/aems/auth/AemsAuthService.java
@@ -1,5 +1,6 @@
 package com.hyjf.cs.user.service.aems.auth;
 
+import com.hyjf.cs.common.bean.result.WebResult;
 import com.hyjf.cs.user.bean.AemsMergeAuthPagePlusRequestBean;
 import com.hyjf.cs.user.bean.AuthBean;
 import com.hyjf.cs.user.service.BaseUserService;
@@ -69,4 +70,11 @@ public interface AemsAuthService extends BaseUserService {
      * @param authType
      */
     void updateUserAuth(Integer userId, BankCallBean bean, String authType);
+
+    /**
+     * 查询授权错误信息
+     * @param orderId
+     * @return
+     */
+    WebResult<Object> seachUserAuthErrorMessgae(String orderId);
 }
