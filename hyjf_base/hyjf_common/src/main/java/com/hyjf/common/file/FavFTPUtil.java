e1c62ce07915e7dcee3e05f7191d2d3ad9494c23
zhadaojian <zhadaojian123>
2019-03-05 19:30:30 +0800

    删除多余日志输出

--- hyjf_base/hyjf_common/src/main/java/com/hyjf/common/file/FavFTPUtil.java
+++ hyjf_base/hyjf_common/src/main/java/com/hyjf/common/file/FavFTPUtil.java
@@ -252,7 +252,6 @@ public class FavFTPUtil {
         try {
             int reply;
             ftp.connect(para.hostName, para.port);
-            logger.info("----------------------------开始下载FTP协议服务器："+para.hostName + "-" +para.port);
             // 如果采用默认端口，可以使用ftp.connect(host)的方式直接连接FTP服务器
             ftp.login(para.userName, para.passWord);// 登录
             reply = ftp.getReplyCode();
@@ -262,14 +261,12 @@ public class FavFTPUtil {
             }
             ftp.enterLocalPassiveMode();
             ftp.changeWorkingDirectory(para.downloadPath);// 转移到FTP服务器目录
-            logger.info("----------------------------开始下载FTP协议服务器downloadPath："+para.downloadPath);
             FTPFile[] fs = ftp.listFiles();
-            logger.info("----------------------------开始下载FTP协议服务器fs："+fs.length);
+            System.out.println(fs.length);
             ftp.setFileType(FTP.BINARY_FILE_TYPE);
             for (FTPFile ff : fs) {
-                logger.info("----------------------------开始下载FTP协议--------："+ff.getName()+ "===" +para.sftpKeyFile);
                 if (ff.getName().equals(para.sftpKeyFile)) {
-                    logger.info("----------------------------开始下载FTP协议："+para.savePath + "/" +para.fileName+"_"+ff.getName());
+
                     localFile = new File(para.savePath + "/" +para.fileName+"_"+ff.getName());
                     if (localFile.exists()) {
                         localFile.delete();
