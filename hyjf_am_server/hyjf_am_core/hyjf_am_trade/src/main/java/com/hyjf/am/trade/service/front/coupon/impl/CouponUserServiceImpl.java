dedfe705d40eb59c3d4e02ed716b85d2bc5cce80
limeng <466965272@qq.com>
2019-03-09 08:55:22 +0800

    修改APP优惠券失效推送逻辑

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/front/coupon/impl/CouponUserServiceImpl.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/front/coupon/impl/CouponUserServiceImpl.java
@@ -35,7 +35,7 @@ public class CouponUserServiceImpl implements CouponUserService {
     private CouponUserListCustomizeMapper couponUserListCustomizeMapper;
 
     @Override
-    public List<CouponUser> selectCouponUser(int nowBeginDate, int nowEndDate) {
+    public List<CouponUser> selectCouponUser(int nowBeginDate, int nowEndDate,Integer type) {
 
         int BeginDate = GetDate.strYYYYMMDD2Timestamp2(GetDate.getDataString(GetDate.date_sdf));
         int EndDate = GetDate.strYYYYMMDD2Timestamp2(GetDate.getDataString(GetDate.date_sdf, 1));
@@ -61,30 +61,35 @@ public class CouponUserServiceImpl implements CouponUserService {
         criteria.andEndTimeGreaterThanOrEqualTo(nowBeginDate);
         criteria.andEndTimeLessThan(nowEndDate);
 
-        CouponUserExample.Criteria criteria2 = example.createCriteria();
-        criteria2.andDelFlagEqualTo(0);
-        // 未使用
-        criteria2.andUsedFlagEqualTo(0);
-        criteria2.andEndTimeGreaterThanOrEqualTo(towBeginDate);
-        criteria2.andEndTimeLessThan(towEndDate);
-        example.or(criteria2);
+        //type 1:即将失效，2：已经失效
+        if(1 == type){
+            CouponUserExample.Criteria criteria2 = example.createCriteria();
+            criteria2.andDelFlagEqualTo(0);
+            // 未使用
+            criteria2.andUsedFlagEqualTo(0);
+            criteria2.andEndTimeGreaterThanOrEqualTo(towBeginDate);
+            criteria2.andEndTimeLessThan(towEndDate);
+            example.or(criteria2);
 
-        CouponUserExample.Criteria criteria3 = example.createCriteria();
-        criteria3.andDelFlagEqualTo(0);
-        // 未使用
-        criteria3.andUsedFlagEqualTo(0);
-        criteria3.andEndTimeGreaterThanOrEqualTo(threeBeginDate);
-        criteria3.andEndTimeLessThan(threeEndDate);
-        example.or(criteria3);
+            CouponUserExample.Criteria criteria3 = example.createCriteria();
+            criteria3.andDelFlagEqualTo(0);
+            // 未使用
+            criteria3.andUsedFlagEqualTo(0);
+            criteria3.andEndTimeGreaterThanOrEqualTo(threeBeginDate);
+            criteria3.andEndTimeLessThan(threeEndDate);
+            example.or(criteria3);
 
-        CouponUserExample.Criteria criteria7 = example.createCriteria();
-        criteria7.andDelFlagEqualTo(0);
-        // 未使用
-        criteria7.andUsedFlagEqualTo(0);
-        // 截止日小于当前时间
-        criteria7.andEndTimeGreaterThanOrEqualTo(sevenBeginDate);
-        criteria7.andEndTimeLessThan(sevenEndDate);
-        example.or(criteria7);
+            CouponUserExample.Criteria criteria7 = example.createCriteria();
+            criteria7.andDelFlagEqualTo(0);
+            // 未使用
+            criteria7.andUsedFlagEqualTo(0);
+            // 截止日小于当前时间
+            criteria7.andEndTimeGreaterThanOrEqualTo(sevenBeginDate);
+            criteria7.andEndTimeLessThan(sevenEndDate);
+            example.or(criteria7);
+        }
+
+
 
         List<CouponUser> couponUserList = couponUserMapper.selectByExample(example);
         return couponUserList;
