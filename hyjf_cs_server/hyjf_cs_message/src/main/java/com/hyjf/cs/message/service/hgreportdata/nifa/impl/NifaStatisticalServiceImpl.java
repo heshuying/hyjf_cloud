55b910f8edb0d1daa387fc3a9b55bbc27ea5f10f
PC-LIUSHOUYI <liushouyi@hyjf.com>
2019-03-11 13:45:03 +0800

    中互金-batch上送数据修复

--- hyjf_cs_server/hyjf_cs_message/src/main/java/com/hyjf/cs/message/service/hgreportdata/nifa/impl/NifaStatisticalServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_message/src/main/java/com/hyjf/cs/message/service/hgreportdata/nifa/impl/NifaStatisticalServiceImpl.java
@@ -252,7 +252,7 @@ public class NifaStatisticalServiceImpl extends BaseServiceImpl implements NifaS
         query.addCriteria(criteria);
         Update update = new Update();
         update.set("reportStatus", "1").set("updateTime", new Date());
-        nifaBorrowInfoDao.update(query, update);
+        nifaTenderInfoDao.update(query, update);
     }
 
     /**
