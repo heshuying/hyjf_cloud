bec5bb5352d3a874a9211548078119f07efb5959
tanyangyang <591628420@qq.com>
2019-03-15 11:59:05 +0800

    websocket  demo提交

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/controller/api/websocket/ApiSocketMessageController.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/controller/api/websocket/ApiSocketMessageController.java
@@ -5,12 +5,14 @@ package com.hyjf.cs.user.controller.api.websocket;
 
 import com.alibaba.fastjson.JSONObject;
 import com.hyjf.cs.common.bean.result.ApiResult;
-import com.hyjf.cs.user.config.SystemConfig;
-import com.hyjf.cs.user.controller.BaseUserController;
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,13 +25,13 @@ import java.io.IOException;
 @Api(value = "api端-用户画像大屏幕显示",tags = "api端-用户画像大屏幕显示")
 @Controller
 @RequestMapping(value = "/hyjf-api/api/user_picture")
-public class ApiSocketMessageController extends BaseUserController {
-
-
+public class ApiSocketMessageController{
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
     //页面请求
     @GetMapping("/socket/{cid}")
     public ModelAndView socket(@PathVariable String cid,HttpServletRequest request) {
-        ModelAndView mav=new ModelAndView("/socket/user_picture");
+        logger.info("进入接口=====");
+        ModelAndView mav=new ModelAndView("socket/user_picture");
         StringBuffer bf = new StringBuffer();
         bf.append("ws://").append(request.getServerName()).append(":").append(request.getServerPort()).append("/hyjf-api/websocket/").append(cid);
         logger.info(bf.toString());
