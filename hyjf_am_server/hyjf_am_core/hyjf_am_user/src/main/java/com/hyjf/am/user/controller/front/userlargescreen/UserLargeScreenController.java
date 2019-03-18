a1278984334c6ffedf78f93c492cc8d4932ca85a
tanyangyang <591628420@qq.com>
2019-03-18 16:35:13 +0800

    接口提交

--- hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/controller/front/userlargescreen/UserLargeScreenController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/controller/front/userlargescreen/UserLargeScreenController.java
@@ -3,14 +3,14 @@
  */
 package com.hyjf.am.user.controller.front.userlargescreen;
 
-import com.hyjf.am.response.user.ScreenConfigResponse;
+import com.hyjf.am.response.user.UserCustomerTaskConfigResponse;
+import com.hyjf.am.response.user.UserScreenConfigResponse;
+import com.hyjf.am.resquest.admin.UserLargeScreenRequest;
 import com.hyjf.am.user.service.front.userlargescreen.UserLargeScreenService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author tanyy
@@ -25,9 +25,16 @@ public class UserLargeScreenController {
     private UserLargeScreenService userLargeScreenService;
 
     @ApiOperation(value = " 大屏幕运营部配置获取",notes = " 大屏幕运营部配置获取")
-    @GetMapping(value = "/screenconfig")
-    public ScreenConfigResponse getScreenConfig() {
-        ScreenConfigResponse response = userLargeScreenService.getScreenConfig();
+    @PostMapping(value = "/screenconfig")
+    public UserScreenConfigResponse getScreenConfig(@RequestBody UserLargeScreenRequest request) {
+        UserScreenConfigResponse response = userLargeScreenService.getScreenConfig(request);
+        return response;
+    }
+
+    @ApiOperation(value = " 坐席月任务配置",notes = " 坐席月任务配置")
+    @PostMapping(value = "/customertaskconfig")
+    public UserCustomerTaskConfigResponse getCustomerTaskConfig(@RequestBody UserLargeScreenRequest request) {
+        UserCustomerTaskConfigResponse response = userLargeScreenService.getCustomerTaskConfig(request);
         return response;
     }
 }
