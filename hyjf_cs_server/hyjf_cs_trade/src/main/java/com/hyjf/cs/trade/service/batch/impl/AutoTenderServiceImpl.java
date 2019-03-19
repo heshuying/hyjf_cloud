6d6ef4f59fbdf55b28df4f8ac8ee51e2b623711d
liubin <liu'bin>
2019-03-19 11:34:11 +0800

    【智投自动出借任务】智投投资的金额校验（承接支付金额 > 承接本金+利息）修改2

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/batch/impl/AutoTenderServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/batch/impl/AutoTenderServiceImpl.java
@@ -650,7 +650,8 @@ public class AutoTenderServiceImpl extends BaseTradeServiceImpl implements AutoT
         logger.debug(logHeader + "校验债转用的计算金额是否有异常数据 开启！");
 
         // 承接的应该大于等于支付的
-        if(resultVO.getAssignPay().compareTo(resultVO.getAssignAccount().subtract(new BigDecimal(0.1))) > 0){
+        if(resultVO.getAssignPay().compareTo(resultVO.getAssignAccount().subtract(new BigDecimal(0.1))) >= 0){
+            logger.info(resultVO.getAssignPay() +">=" +resultVO.getAssignAccount() +"-0.1");
             return new CheckResult(false, "校验债转用的计算金额：承接支付金额 > 承接本金+利息-0.1");
         }
         // 承接服务率不能大于1
