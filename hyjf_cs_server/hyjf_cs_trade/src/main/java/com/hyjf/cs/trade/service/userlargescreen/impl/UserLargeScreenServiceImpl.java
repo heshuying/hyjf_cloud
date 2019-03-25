0d4b5bdbe5cbacc2270a375146fda020e7be7b58
tanyangyang <591628420@qq.com>
2019-03-25 17:41:12 +0800

    不保留小数

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/userlargescreen/impl/UserLargeScreenServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/service/userlargescreen/impl/UserLargeScreenServiceImpl.java
@@ -57,8 +57,8 @@ public class UserLargeScreenServiceImpl  implements UserLargeScreenService {
         UserLargeScreenVO userCapitalDetailsVo = amTradeClient.getUserCapitalDetails();
        // UserCustomerTaskConfigResponse userCustomerTaskConfigResponse = amUserClient.getCustomerTaskConfig(request);
         ScreenConfigVO vo = userScreenConfigResponse.getResult();
-        bean.setNewPassengerGoal(vo.getNewPassengerGoal().divide(new BigDecimal(10000),2,BigDecimal.ROUND_HALF_UP));
-        bean.setOldPassengerGoal(vo.getOldPassengerGoal().divide(new BigDecimal(10000),2,BigDecimal.ROUND_HALF_UP));
+        bean.setNewPassengerGoal(vo.getNewPassengerGoal().divide(new BigDecimal(10000),0,BigDecimal.ROUND_HALF_UP));
+        bean.setOldPassengerGoal(vo.getOldPassengerGoal().divide(new BigDecimal(10000),0,BigDecimal.ROUND_HALF_UP));
         bean.setScalePerformanceNew(scalePerformanceVo.getScalePerformanceNew());
         bean.setScalePerformanceOld(scalePerformanceVo.getScalePerformanceOld());
         bean.setMonthScalePerformanceListNew(monthScalePerformanceListVo.getMonthScalePerformanceListNew());
