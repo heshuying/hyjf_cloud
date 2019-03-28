5664ae7caa1ebeadcf57707bf68dd41c87faee04
dongzeshan <dongzeshan@hyjf.com>
2019-03-28 14:01:04 +0800

    还款接口开发

--- hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/trade/dao/model/customize/AdminRepayDelayCustomize.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/trade/dao/model/customize/AdminRepayDelayCustomize.java
@@ -18,8 +18,27 @@ public class AdminRepayDelayCustomize {
     private String repayLastTime;
     private String borrowStyle;
     private String borrowStyleName;
+    private String repayAccountWait;
+    private String interestCapitalWait;
 
-    public String getBorrowNid() {
+    
+    public String getRepayAccountWait() {
+		return repayAccountWait;
+	}
+
+	public void setRepayAccountWait(String repayAccountWait) {
+		this.repayAccountWait = repayAccountWait;
+	}
+
+	public String getInterestCapitalWait() {
+		return interestCapitalWait;
+	}
+
+	public void setInterestCapitalWait(String interestCapitalWait) {
+		this.interestCapitalWait = interestCapitalWait;
+	}
+
+	public String getBorrowNid() {
         return borrowNid;
     }
 
--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/borrow/borrowrepayment/plan/BorrowRepaymentPlanController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/borrow/borrowrepayment/plan/BorrowRepaymentPlanController.java
@@ -131,6 +131,8 @@ public class BorrowRepaymentPlanController extends BaseController {
         map.put("instName","资产来源");
         map.put("userId","借款人ID");
         map.put("borrowUserName","借款人用户名");
+        map.put("repayOrgName","担保人");
+        map.put("borrowName","项目名称");
         map.put("projectTypeName","项目类型");
         map.put("borrowPeriod","借款期限");
         map.put("borrowApr","出借利率");
@@ -154,6 +156,11 @@ public class BorrowRepaymentPlanController extends BaseController {
         map.put("repayActionTime","实际还款日期");
         map.put("repayLastTime","应还日期");
         map.put("repayMoneySource","还款来源");
+        map.put("userName","还款人");
+        map.put("submitter","发起人");
+        map.put("verifyTime","发标时间");
+        map.put("borrowFullTime","满标时间");
+        map.put("recoverLastTime","放款时间");
         map.put("repayAccountCapitalWait","剩余待还本金");
         map.put("repayAccountInterestWait","剩余待还利息");
         return map;
