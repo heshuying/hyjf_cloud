5a830485875e9ad7c26bd300bb39f697e650ee21
zhangqingqing <zhangqingqing@hyjf.com>
2019-04-09 14:58:55 +0800

    wbs添加启动注解，删除feign包

--- hyjf_platform_server/hyjf_wbs/src/main/java/com/hyjf/wbs/WbsApplication.java
+++ hyjf_platform_server/hyjf_wbs/src/main/java/com/hyjf/wbs/WbsApplication.java
@@ -2,9 +2,9 @@ package com.hyjf.wbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WbsApplication {
 
