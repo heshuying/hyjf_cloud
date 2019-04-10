78937b3e567754d71478d2faddce31fae82bf441
wenxin <wenxin@hyjf.com>
2019-04-10 10:20:10 +0800

    【青岛】 资金计划3.3.0 预计当日复投额字段計算方法實現 controller

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/front/hjh/PlanCapitalController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/controller/front/hjh/PlanCapitalController.java
@@ -6,10 +6,13 @@ package com.hyjf.am.trade.controller.front.hjh;
 import com.hyjf.am.response.admin.HjhPlanCapitalPredictionResponse;
 import com.hyjf.am.response.admin.HjhPlanCapitalResponse;
 import com.hyjf.am.trade.service.front.trade.PlanCapitalService;
+import com.hyjf.am.trade.utils.TransUtil;
 import com.hyjf.am.vo.trade.HjhPlanCapitalPredictionVO;
 import com.hyjf.am.vo.trade.HjhPlanCapitalVO;
 import com.hyjf.common.util.GetDate;
 import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -27,6 +33,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/am-trade/planCapitalController")
 public class PlanCapitalController {
+    // logger
+    private static final Logger logger = LoggerFactory.getLogger(PlanCapitalController.class);
+
     @Autowired
     private PlanCapitalService planCapitalService;
 
@@ -72,9 +81,21 @@ public class PlanCapitalController {
     public HjhPlanCapitalPredictionResponse getPlanCapitalPredictionForProformaList(@PathVariable(value = "fromDate") String fromDate,
                                                                                     @PathVariable(value = "toDate") String toDate){
         HjhPlanCapitalPredictionResponse response = new HjhPlanCapitalPredictionResponse();
-        List<HjhPlanCapitalPredictionVO> list = this.planCapitalService.getPlanCapitalPredictionForProformaList(GetDate.stringToDate2(fromDate), GetDate.stringToDate2(toDate));
-        if (!CollectionUtils.isEmpty(list)) {
-            response.setResultList(list);
+        List<HjhPlanCapitalPredictionVO> listAll = new ArrayList<HjhPlanCapitalPredictionVO>();
+        try {
+            // 拆解時間段list
+            List<Date> dates = TransUtil.findDates(GetDate.stringToDate2(fromDate),GetDate.stringToDate2(toDate),0);
+            for(Date date:dates){
+                // 根據日期獲取預計日期的新增復投額
+                List<HjhPlanCapitalPredictionVO> list = this.planCapitalService.getPlanCapitalPredictionForProformaList(date);
+                listAll.addAll(list);
+            }
+        } catch (ParseException e) {
+            logger.info("获取该期间的预计当日新增复投额CONTROLLER，拆解時間LIST時發生錯誤："+e.toString());
+        }
+        // 返回預估時間段內的所有計劃預估值（預計當日新增復投額）
+        if (!CollectionUtils.isEmpty(listAll)) {
+            response.setResultList(listAll);
         }
         return response;
     }
