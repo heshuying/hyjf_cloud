5e6e7e0f1681877820c952f67e4bc3ae18719237
wanggongxi <wgx123456>
2019-04-17 11:00:35 +0800

    带逾期的提前还款利息计算

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/front/repay/impl/RepayManageServiceImpl.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/front/repay/impl/RepayManageServiceImpl.java
@@ -1977,6 +1977,8 @@ public class RepayManageServiceImpl extends BaseServiceImpl implements RepayMana
                         }
                     }
                 } else if (repayPeriod > nowPeriod) {// 当前期之后的数据
+                    // 当前期也算的话，需要加上当前期
+                    totalPeriod = borrow.getBorrowPeriod() - nowPeriod + 1;
                     // 计算还款期的数据
                     BeanUtils.copyProperties(borrowRepayPlan, repayPlanDetail);
                     this.calculateRecoverPlanAll(repayPlanDetail, borrow, totalPeriod, borrowRecoverList);
