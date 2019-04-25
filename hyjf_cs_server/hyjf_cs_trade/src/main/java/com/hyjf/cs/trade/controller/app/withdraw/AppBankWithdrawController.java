b54d6717c19cd2a4b5d83c815d93e7195c8d30b4
liuyang <964107171@qq.com>
2019-04-25 11:22:02 +0800

    APP提现画面进入时,getcash 为空,获取配置失败解决

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/app/withdraw/AppBankWithdrawController.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/app/withdraw/AppBankWithdrawController.java
@@ -160,6 +160,7 @@ public class AppBankWithdrawController extends BaseTradeController {
         }
         // add by liuyang 20190422 节假日提现修改 start
         // 获取提现规则配置
+        logger.info(getcash);
         String withdrawMoney = getcash == null ? "0" : getcash;
         WithdrawRuleConfigVO withdrawRuleConfigVO = this.bankWithdrawService.getWithdrawRuleConfig(userId, withdrawMoney);
         if (withdrawRuleConfigVO == null){
