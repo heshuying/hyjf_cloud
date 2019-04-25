1dd94edf9285ff93c24dd344738234a5d9ca7474
dongzeshan <dongzeshan@hyjf.com>
2019-04-25 16:32:38 +0800

    利率优化

--- hyjf_base/hyjf_common/src/main/java/com/hyjf/common/util/FormatRateUtil.java
+++ hyjf_base/hyjf_common/src/main/java/com/hyjf/common/util/FormatRateUtil.java
@@ -34,15 +34,15 @@ public class FormatRateUtil {
             return "0.0";
         }
         //整数或只有一位小数的情况下，格式化利率
-        if (stringArr.length == 1 || stringArr[1].length() == 1) {
-            return borrowApr;
-        }
-        String spitArr = stringArr[1];
+//        if (stringArr.length == 1 || stringArr[1].length() == 1) {
+//            return borrowApr;
+//        }
+ //       String spitArr = stringArr[1];
         //两位小数的情况下，判断最后一位是否为0
-        String subBorr = spitArr.substring(spitArr.length() - 1, spitArr.length());
-        if (subBorr.equals("0")) {
+    //    String subBorr = spitArr.substring(spitArr.length() - 1, spitArr.length());
+        if (borrowApr.substring( borrowApr.length()-1,borrowApr.length()).equals("0")) {
 //            BigDecimal big = new BigDecimal(borrowApr).setScale(1);
-            borrowApr =borrowApr.substring(0, borrowApr.length()-1);
+        	return borrowApr.substring(0, borrowApr.length()-1);
         }
         return borrowApr;
     }
