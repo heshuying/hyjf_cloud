a7d9b5af29cc2b339aa6b65c216450944f7fde7e
liuyang <964107171@qq.com>
2019-04-25 15:12:02 +0800

    PC端提现参数修改

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/web/wirhdraw/WebBankWithdrawController.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/web/wirhdraw/WebBankWithdrawController.java
@@ -73,15 +73,15 @@ public class WebBankWithdrawController extends BaseTradeController {
      * 用户银行提现校验
      *
      * @param userId
-     * @param withdrawMoney
      * @param request
      * @return
      */
     @ApiOperation(value = "用户银行提现校验", notes = "用户银行提现校验")
     @PostMapping("/userBankWithdrawCheck")
     @RequestLimit(seconds = 3)
-    public WebResult<Object> userBankWithdrawCheck(@RequestHeader(value = "userId") Integer userId, @RequestHeader(value = "withdrawMoney") String withdrawMoney, HttpServletRequest request) {
+    public WebResult<Object> userBankWithdrawCheck(@RequestHeader(value = "userId") Integer userId, HttpServletRequest request) {
         WebViewUserVO user = bankWithdrawService.getUserFromCache(userId);
+        String withdrawMoney = request.getParameter("withdrawMoney");
         CheckUtil.check(null != user, MsgEnum.ERR_OBJECT_GET, "用户信息");
         WebResult<Object> objectWebResult = bankWithdrawService.userBankWithdrawCheck(userId, withdrawMoney);
         return objectWebResult;
