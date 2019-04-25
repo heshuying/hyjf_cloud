1c187dfe9dda1c411626be01e79157ce4ecbe86e
sunshuaishuai <sunshuaishuai@hyjf.com>
2019-04-25 19:42:36 +0800

    【青岛】单点登录代码还原

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/LoginController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/LoginController.java
@@ -90,10 +90,10 @@ public class LoginController extends BaseController {
 		String uuid=UUID.randomUUID().toString();
 		// 1. 登录成功将登陆密码错误次数的key删除
 		RedisUtils.del(RedisConstants.PASSWORD_ERR_COUNT_ADMIN + username);
-		if (!hyjfEnvTest) {
+		/*if (!hyjfEnvTest) {
 			// 生产环境   单点登录
 			RedisUtils.set(RedisConstants.ADMIN_UNIQUE_ID+username, uuid, 3600);
-		}
+		}*/
 		this.setUser(request, prs.getResult());
 		Map<String,Object> result =new HashMap<String, Object>();;
 		result.put("uuid", uuid);
