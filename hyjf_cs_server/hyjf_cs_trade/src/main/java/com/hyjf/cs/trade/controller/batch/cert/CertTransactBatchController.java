fa6af7e983ddede033eeec231a884d79838a6c9e
pangchengchao <pangchengchao@hyjf.com>
2019-04-28 11:08:27 +0800

    Revert "打印消息"
    This reverts commit 08ae58b

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/batch/cert/CertTransactBatchController.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/controller/batch/cert/CertTransactBatchController.java
@@ -49,30 +49,32 @@ public class CertTransactBatchController {
     @GetMapping("/certTransact")
     public StringResponse certTransact() {
         logger.info(logHeader + "CertTransactBatchController execute start...");
-        CertAccountListIdCustomizeVO certTransactCustomize=new CertAccountListIdCustomizeVO();
-        Integer transactPage=1;
+        Integer page=1;
         Integer size=500;
+        CertAccountListIdCustomizeVO customize=new CertAccountListIdCustomizeVO();
+
         // 检查redis的值是否允许运行 允许返回true  不允许返回false
         boolean canRun = certTransactService.checkCanRun();
         if(!canRun){
             logger.info(logHeader + "redis不允许上报！");
             return new StringResponse("success");
         }
+
         do {
             String certTransactMaxId = RedisUtils.get(RedisConstants.CERT_TRANSACT_OTHER_MAXID);
             Map<String, Object> param =  new HashMap<String, Object>();
             param.put("minId", certTransactMaxId==null?"0":certTransactMaxId);
-            param.put("limitStart", (transactPage-1)*size);
+            param.put("limitStart", (page-1)*size);
             param.put("limitEnd", size);
-            certTransactCustomize=certTransactService.queryCertAccountListId(param);
+            customize=certTransactService.queryCertAccountListId(param);
             if(certTransactMaxId==null||"".equals(certTransactMaxId)){
-                RedisUtils.set(RedisConstants.CERT_TRANSACT_OTHER_MAXID, certTransactCustomize.getMaxId()+"");
+                RedisUtils.set(RedisConstants.CERT_TRANSACT_OTHER_MAXID, customize.getMaxId()+"");
                 logger.info(logHeader + "CertTransactBatchController execute end...");
                 return new StringResponse("success");
             }
-            String minId=certTransactCustomize.getLimitMinId()+"";
-            String maxId=certTransactCustomize.getLimitMaxId()+"";
-            String sumCount=certTransactCustomize.getSumCount()+"";
+            String minId=customize.getLimitMinId()+"";
+            String maxId=customize.getLimitMaxId()+"";
+            String sumCount=customize.getSumCount()+"";
             if("0".equals(sumCount)){
                 logger.info(logHeader + "CertTransactBatchController execute end...");
                 return new StringResponse("success");
@@ -86,51 +88,12 @@ public class CertTransactBatchController {
                 commonProducer.messageSendDelay2(new MessageContent(MQConstant.HYJF_TOPIC, MQConstant.CERT_TRANSACT_TAG, UUID.randomUUID().toString(), params),
                         MQConstant.HG_REPORT_DELAY_LEVEL);
 
+
             }catch (Exception e){}
             RedisUtils.set(RedisConstants.CERT_TRANSACT_OTHER_MAXID, maxId);
-            transactPage=transactPage+1;
-        } while (certTransactCustomize.getMaxId()>certTransactCustomize.getLimitMaxId());
+            page=page+1;
+        } while (customize.getMaxId()>customize.getLimitMaxId());
         logger.info(logHeader + "CertTransactBatchController execute end...");
-
-
-        logger.info(logHeader + "CertInvestDetailBatchController execute start...");
-        CertAccountListIdCustomizeVO certInvestDetailCustomize=new CertAccountListIdCustomizeVO();
-        Integer investDetailPage=1;
-        do {
-            String certInvestDetailMaxId = RedisUtils.get(RedisConstants.CERT_INVEST_DETAIL_MAXID);
-            Map<String, Object> param =  new HashMap<String, Object>();
-            param.put("minId", certInvestDetailMaxId==null?"0":certInvestDetailMaxId);
-            param.put("limitStart", (investDetailPage-1)*size);
-            param.put("limitEnd", size);
-            certInvestDetailCustomize=certTransactService.queryCertAccountListId(param);
-            if(certInvestDetailMaxId==null||"".equals(certInvestDetailMaxId)){
-                RedisUtils.set(RedisConstants.CERT_INVEST_DETAIL_MAXID, certInvestDetailCustomize.getMaxId()+"");
-                logger.info(logHeader + "CertInvestDetailBatchController execute end...");
-                return new StringResponse("success");
-            }
-            String minId=certInvestDetailCustomize.getLimitMinId()+"";
-            String maxId=certInvestDetailCustomize.getLimitMaxId()+"";
-            String sumCount=certInvestDetailCustomize.getSumCount()+"";
-            if("0".equals(sumCount)){
-                logger.info(logHeader + "CertInvestDetailBatchController execute end...");
-                return new StringResponse("success");
-            }
-            // 加入到消息队列
-            Map<String, String> params = new HashMap<String, String>();
-            params.put("mqMsgId", GetCode.getRandomCode(10));
-            params.put("minId", minId);
-            params.put("maxId", maxId);
-            try{
-                commonProducer.messageSendDelay2(new MessageContent(MQConstant.HYJF_TOPIC, MQConstant.CERT_INVEST_DETAIL_TAG, UUID.randomUUID().toString(), params),
-                        MQConstant.HG_REPORT_DELAY_LEVEL);
-
-            }catch (Exception e){}
-            RedisUtils.set(RedisConstants.CERT_INVEST_DETAIL_MAXID, maxId);
-            investDetailPage=investDetailPage+1;
-        } while (certInvestDetailCustomize.getMaxId()>certInvestDetailCustomize.getLimitMaxId());
-        logger.info(logHeader + "CertInvestDetailBatchController execute end...");
-
-
         return new StringResponse("success");
     }
 }
