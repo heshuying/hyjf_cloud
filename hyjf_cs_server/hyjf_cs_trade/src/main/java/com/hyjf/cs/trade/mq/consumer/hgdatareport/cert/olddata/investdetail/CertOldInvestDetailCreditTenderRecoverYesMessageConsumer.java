166d70dc19bcc743322f8d2afe9748d14663c059
pangchengchao <pangchengchao@hyjf.com>
2019-04-28 16:34:49 +0800

    历史记录上报数据生成

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/consumer/hgdatareport/cert/olddata/investdetail/CertOldInvestDetailCreditTenderRecoverYesMessageConsumer.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/consumer/hgdatareport/cert/olddata/investdetail/CertOldInvestDetailCreditTenderRecoverYesMessageConsumer.java
@@ -30,7 +30,7 @@ import java.util.List;
  * @Date
  */
 @Service
-@RocketMQMessageListener(topic = MQConstant.HYJF_TOPIC, selectorExpression = MQConstant.CERT_OLD_INVEST_DETAIL_CREDIT_TENDER_RECOVER_YES_TAG, consumerGroup = MQConstant.CERT_OLD_INVEST_DETAIL_CREDIT_TENDER_RECOVER_YES_GROUP)
+@RocketMQMessageListener(topic = MQConstant.HYJF_CERT_OLD_TOPIC, selectorExpression = MQConstant.CERT_OLD_INVEST_DETAIL_CREDIT_TENDER_RECOVER_YES_TAG, consumerGroup = MQConstant.CERT_OLD_INVEST_DETAIL_CREDIT_TENDER_RECOVER_YES_GROUP)
 public class CertOldInvestDetailCreditTenderRecoverYesMessageConsumer implements RocketMQListener<MessageExt>, RocketMQPushConsumerLifecycleListener {
     Logger logger = LoggerFactory.getLogger(CertOldInvestDetailCreditTenderRecoverYesMessageConsumer.class);
 
