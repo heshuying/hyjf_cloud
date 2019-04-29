9ad33c8f754390cf389520646731f51c2f15fb44
pangchengchao <pangchengchao@hyjf.com>
2019-04-29 14:57:01 +0800

    历史记录上报数据生成

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/consumer/hgdatareport/cert/olddata/investdetail/CertOldInvestDetailAccedeAssignMessageConsumer.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/consumer/hgdatareport/cert/olddata/investdetail/CertOldInvestDetailAccedeAssignMessageConsumer.java
@@ -74,6 +74,7 @@ public class CertOldInvestDetailAccedeAssignMessageConsumer implements RocketMQL
             while (!"1".equals(RedisUtils.get("CREDIT_TENDER_ACCEDE_ASSIGN_RUN"))){
                 // --> 消息处理
                 List<CertAccountListCustomizeVO> accountLists=certOldInvestDetailService.getCertAccountListCustomizeVO(page,size,"accede_assign");
+                logger.info(logHeader + "accountLists.size():"+accountLists.size());
                 if (accountLists.size()==0){
                     logger.info(logHeader + "生成完成！");
                     RedisUtils.set("CREDIT_TENDER_ACCEDE_ASSIGN_RUN","1");
