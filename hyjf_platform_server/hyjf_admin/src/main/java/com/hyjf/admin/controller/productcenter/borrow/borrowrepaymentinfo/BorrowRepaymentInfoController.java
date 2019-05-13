1d3a21b6fee7d20aa7bf506e4089a29a85fc21d3
dongzeshan <dongzeshan@hyjf.com>
2019-05-13 18:09:04 +0800

    修复原始导出

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/borrow/borrowrepaymentinfo/BorrowRepaymentInfoController.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/controller/productcenter/borrow/borrowrepaymentinfo/BorrowRepaymentInfoController.java
@@ -490,18 +490,18 @@ public class BorrowRepaymentInfoController extends BaseController {
         map.put("repayType","还款方式");
         map.put("recoverUserName","出借人用户名");
         map.put("recoverUserId","出借人ID");
-        map.put("recoverUserAttribute","出借人用户属性（当前）");
+        map.put("recoverUserAttribute","出借人用户属性（当时）");
         if (StringUtils.isNotBlank(isOrganizationView)) {
-            map.put("recoverRegionName", "出借人所属一级分部（当前）");
-            map.put("recoverBranchName", "出借人所属二级分部（当前）");
-            map.put("recoverDepartmentName", "出借人所属团队（当前）");
+            map.put("recoverRegionName", "出借人所属一级分部（当时）");
+            map.put("recoverBranchName", "出借人所属二级分部（当时）");
+            map.put("recoverDepartmentName", "出借人所属团队（当时）");
         }
         map.put("referrerName","推荐人用户名（当时）");
         map.put("referrerTrueName","推荐人姓名（当时）");
         if (StringUtils.isNotBlank(isOrganizationView)) {
-            map.put("referrerRegionName", "推荐人所属一级分部（当时）");
-            map.put("referrerBranchName", "推荐人所属二级分部（当时）");
-            map.put("referrerDepartmentName", "推荐人所属团队（当时）");
+            map.put("referrerRegionName", "推荐人所属一级分部（当前）");
+            map.put("referrerBranchName", "推荐人所属二级分部（当前）");
+            map.put("referrerDepartmentName", "推荐人所属团队（当前）");
         }
         map.put("recoverTotal","出借金额");
        // map.put("recoverTotal","持有金额");
