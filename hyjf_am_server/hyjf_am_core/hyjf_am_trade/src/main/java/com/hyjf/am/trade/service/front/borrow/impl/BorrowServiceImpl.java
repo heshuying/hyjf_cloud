933545f81cd1b123620182a248b058f3239abc32
liuyang <964107171@qq.com>
2019-05-17 17:17:43 +0800

    牛头帮散标满标后发送募集结束MQ

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/front/borrow/impl/BorrowServiceImpl.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/front/borrow/impl/BorrowServiceImpl.java
@@ -520,6 +520,23 @@ public class BorrowServiceImpl extends BaseServiceImpl implements BorrowService 
             commonProducer.messageSendDelay2(new MessageContent(MQConstant.HYJF_TOPIC, MQConstant.ISSUE_INVESTED_TAG, UUID.randomUUID().toString(), params),
                     MQConstant.HG_REPORT_DELAY_LEVEL);
             // add by liushouyi nifa2 20181204 end
+
+            // add by liuyang wbs系统推送标的信息 20190517 start
+            try{
+                // 满标后,发送标的募集结束MQ
+                JSONObject borrowParams = new JSONObject();
+                // 产品编号
+                borrowParams.put("productNo", borrowNid);
+                // 产品状态 :3:募集结束
+                borrowParams.put("productStatus", "3");
+                // 产品类型 0 散标类, 1 计划类
+                borrowParams.put("productType", 0);
+                commonProducer.messageSend(new MessageContent(MQConstant.WBS_BORROW_INFO_TOPIC, MQConstant.WBS_BORROW_INFO_TAG, UUID.randomUUID().toString(), params));
+            }catch (Exception e){
+                logger.error(e.getMessage());
+                logger.error("wbs系统推送标的信息发送MQ失败");
+            }
+            // add by liuyang wbs系统推送标的信息 20190517 end
         } else if (accountWait.compareTo(BigDecimal.ZERO) < 0) {
             logger.error("用户:" + userId + "项目编号:" + borrowNid + "***********************************项目暴标");
             throw new RuntimeException("用户:" + userId + "项目编号:" + borrowNid + "***********************************项目暴标");
