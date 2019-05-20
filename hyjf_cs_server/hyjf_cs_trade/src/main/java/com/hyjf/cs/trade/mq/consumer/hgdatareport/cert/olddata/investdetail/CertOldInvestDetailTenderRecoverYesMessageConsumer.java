13033f468be465cc61e996e0bcfc129f571f93b9
pangchengchao <pangchengchao@hyjf.com>
2019-05-20 10:10:08 +0800

    生成历史数据生成修改

--- hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/consumer/hgdatareport/cert/olddata/investdetail/CertOldInvestDetailTenderRecoverYesMessageConsumer.java
+++ hyjf_cs_server/hyjf_cs_trade/src/main/java/com/hyjf/cs/trade/mq/consumer/hgdatareport/cert/olddata/investdetail/CertOldInvestDetailTenderRecoverYesMessageConsumer.java
@@ -75,7 +75,7 @@ public class CertOldInvestDetailTenderRecoverYesMessageConsumer implements Rocke
         String tradeDate = jsonObject.getString("tradeDate");
 
         Integer page=1;
-        Integer size=1000;
+        Integer size=100;
         try {
             while (!"1".equals(RedisUtils.get("CREDIT_TENDER_RECOVER_YES_RUN"))){
                 // --> 消息处理
