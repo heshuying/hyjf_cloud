a5225d06908ed582a0e46a7fa386e9d525e47d21
wenxin <wenxin@hyjf.com>
2019-05-29 10:17:20 +0800

    【青岛】 兑吧订单 兑吧订单类初始化构建

---
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/service/pointsshop/duiba/order/DuibaOrderListService.java
@@ -0,0 +1,13 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.hyjf.admin.service.pointsshop.duiba.order;
+
+import com.hyjf.admin.common.service.BaseService;
+
+/**
+ * @author WENXIN
+ * @version DuibaOrderListService, v0.1 2019/5/29 9:48
+ */
+public interface DuibaOrderListService extends BaseService {
+}
