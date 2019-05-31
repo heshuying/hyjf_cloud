e6f07d178a4c900e70e4f0ba5ee8b99f428d773b
PC-LIUSHOUYI <liushouyi@hyjf.com>
2019-05-31 17:06:38 +0800

    接口返回修正

--- hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/admin/DuibaPointsResponse.java
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/response/admin/DuibaPointsResponse.java
@@ -5,10 +5,49 @@ package com.hyjf.am.response.admin;
 
 import com.hyjf.am.response.AdminResponse;
 import com.hyjf.am.vo.admin.DuibaPointsVO;
+import com.hyjf.am.vo.config.ParamNameVO;
+
+import java.util.List;
 
 /**
  * @author PC-LIUSHOUYI
  * @version DuibaPointsResponse, v0.1 2019/5/30 10:26
  */
 public class DuibaPointsResponse extends AdminResponse<DuibaPointsVO> {
+    private List<ParamNameVO> integralTypeList;
+    private List<ParamNameVO> integralBusinessNameList;
+    private List<ParamNameVO> auditStatusList;
+    private List<ParamNameVO> operationTypeList;
+
+    public List<ParamNameVO> getIntegralTypeList() {
+        return integralTypeList;
+    }
+
+    public void setIntegralTypeList(List<ParamNameVO> integralTypeList) {
+        this.integralTypeList = integralTypeList;
+    }
+
+    public List<ParamNameVO> getIntegralBusinessNameList() {
+        return integralBusinessNameList;
+    }
+
+    public void setIntegralBusinessNameList(List<ParamNameVO> integralBusinessNameList) {
+        this.integralBusinessNameList = integralBusinessNameList;
+    }
+
+    public List<ParamNameVO> getAuditStatusList() {
+        return auditStatusList;
+    }
+
+    public void setAuditStatusList(List<ParamNameVO> auditStatusList) {
+        this.auditStatusList = auditStatusList;
+    }
+
+    public List<ParamNameVO> getOperationTypeList() {
+        return operationTypeList;
+    }
+
+    public void setOperationTypeList(List<ParamNameVO> operationTypeList) {
+        this.operationTypeList = operationTypeList;
+    }
 }
