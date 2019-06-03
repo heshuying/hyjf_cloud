8b87f687bcf5d993c9b0c720b8d06bc69d9b5eb6
limeng <466965272@qq.com>
2019-06-03 17:53:53 +0800

    优化投屏采集数据还款采集本息数据

--- hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/front/consumer/impl/BatchBorrowRepayZTServiceImpl.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_trade/src/main/java/com/hyjf/am/trade/service/front/consumer/impl/BatchBorrowRepayZTServiceImpl.java
@@ -1476,6 +1476,7 @@ public class BatchBorrowRepayZTServiceImpl extends BaseServiceImpl implements Ba
                 this.sendMessage(assignUserId, borrowNid, repayAccount, repayInterest, advance_status);
             }
             //发送大屏统计数据
+			logger.info("tenderOrderId:{},assignAccount:{}",tenderOrderId,assignAccount);
             ScreenDataBean screenDataBean = new ScreenDataBean(tenderOrderId,assignUserId,creditRepay.getUserName(),assignAccount,3);
             this.sendScreenDataMQ(screenDataBean);
         } catch (Exception e) {
@@ -2710,7 +2711,8 @@ public class BatchBorrowRepayZTServiceImpl extends BaseServiceImpl implements Ba
                 this.sendMessage(tenderUserId, borrowNid, repayAccount, repayInterest,advance_status);
             }
             //发送大屏统计数据
-            ScreenDataBean screenDataBean = new ScreenDataBean(tenderOrderId,tenderUserId,borrowRecover.getUserName(),repayCapital,3);
+			logger.info("tenderOrderId:{},repayAccount:{}",tenderOrderId,repayAccount);
+            ScreenDataBean screenDataBean = new ScreenDataBean(tenderOrderId,tenderUserId,borrowRecover.getUserName(),repayAccount,3);
             this.sendScreenDataMQ(screenDataBean);
         } catch (Exception e) {
             logger.error("【直投还款/出借人】发送短信和推送消息时发生系统异常！", e);
