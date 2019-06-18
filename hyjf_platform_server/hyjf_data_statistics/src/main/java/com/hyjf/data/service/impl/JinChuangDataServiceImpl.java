7fca05b09d1e747e9c4a140c4d81d9b2645d1082
yangchangwei <ycw123456>
2019-06-18 17:52:12 +0800

    需求开发

---
+++ hyjf_platform_server/hyjf_data_statistics/src/main/java/com/hyjf/data/service/impl/JinChuangDataServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hyjf.data.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Auther:yangchangwei
+ * @Date:2019/6/18
+ * @Description:
+ */
+@Service
+public class JinChuangDataServiceImpl {
+}
