5932a4c2f9f5eb92e85e39185d7de0a11d329aae
limeng <466965272@qq.com>
2019-06-18 11:51:45 +0800

    提交架构

---
+++ hyjf_platform_server/hyjf_data_statistics/src/main/java/com/hyjf/data/DataStatisticsApplication.java
@@ -0,0 +1,40 @@
+package com.hyjf.data;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@CrossOrigin(origins = "*")
+@ComponentScan(basePackages = {"com.hyjf.data","com.hyjf.common"})
+public class DataStatisticsApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+    @Bean(name = "rocketMQMessageObjectMapper")
+    public ObjectMapper jacksonObjectMapper(@Autowired Jackson2ObjectMapperBuilder builder) {
+        return builder.createXmlMapper(false).build();
+    }
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStatisticsApplication.class, args);
+	}
+}
