5fd38f1d76badae7a877f53bb92005e279862d13
yangchangwei <ycw123456>
2019-06-20 15:59:52 +0800

    需求开发

--- hyjf_platform_server/hyjf_data_statistics/src/main/java/com/hyjf/data/controller/JinChuangDataController.java
+++ hyjf_platform_server/hyjf_data_statistics/src/main/java/com/hyjf/data/controller/JinChuangDataController.java
@@ -2,6 +2,8 @@ package com.hyjf.data.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,14 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(value = "金创展厅数据",tags = "金创展厅数据")
 @RestController
-@RequestMapping("/hyjf_data_statistics")
+@RequestMapping("/jinchuang")
 public class JinChuangDataController {
 
-    @ApiOperation(value = "测试", notes = "测试")
-    @PostMapping(value = "/update")
-    @ResponseBody
-    public String updateData(){
 
+    private static final Logger logger = LoggerFactory.getLogger(JinChuangDataController.class);
+
+    @ApiOperation(value = "hive", notes = "hive")
+    @PostMapping(value = "/hive/updatedata")
+    @ResponseBody
+    public String updateHiveData(){
+        logger.info("--------------金创展厅开始更新hive相关数据！-------");
+        return "success";
+    }
+
+    @ApiOperation(value = "神策", notes = "神策")
+    @PostMapping(value = "/sensors/updatedata")
+    @ResponseBody
+    public String updateSensorsData(){
+        logger.info("--------------金创展厅开始更新神策相关数据！-------");
         return "success";
     }
 
