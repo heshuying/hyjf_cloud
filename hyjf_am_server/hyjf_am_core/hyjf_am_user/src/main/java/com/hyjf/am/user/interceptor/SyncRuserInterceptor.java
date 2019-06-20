1b29c0c0624c1a5ad1fd0c844e2bad4149cdbc7a
wanggongxi <wgx123456>
2019-06-20 13:57:19 +0800

    merge 删除未引用包

--- hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/interceptor/SyncRuserInterceptor.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_user/src/main/java/com/hyjf/am/user/interceptor/SyncRuserInterceptor.java
@@ -22,10 +22,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.Properties;
-import java.util.UUID;
-
 /**
  * 同步用户信息的mybaitis 拦截器
  * 
