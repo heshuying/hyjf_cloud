d391bebf6ee957f272dd8c22863b3dd4566218fb
dongzeshan <dongzeshan@hyjf.com>
2019-06-20 14:28:40 +0800

    代码提交

--- hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/user/service/admin/template/impl/TemplateDisposeServiceImpl.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/user/service/admin/template/impl/TemplateDisposeServiceImpl.java
@@ -1,15 +1,24 @@
 package com.hyjf.am.user.service.admin.template.impl;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hyjf.am.response.admin.TemplateDisposeResponse;
 import com.hyjf.am.resquest.admin.TemplateDisposeRequest;
 import com.hyjf.am.user.dao.mapper.auto.TemplateDisposeMapper;
+import com.hyjf.am.user.dao.model.auto.TemplateDispose;
 import com.hyjf.am.user.dao.model.auto.TemplateDisposeExample;
 import com.hyjf.am.user.dao.model.auto.TemplateDisposeExample.Criteria;
 import com.hyjf.am.user.service.admin.template.TemplateDisposeService;
+import com.hyjf.am.vo.admin.TemplateDisposeVO;
+import com.hyjf.common.paginator.Paginator;
+import com.hyjf.common.util.CommonUtils;
 
 
 @Service
@@ -21,13 +30,37 @@ public class TemplateDisposeServiceImpl implements TemplateDisposeService {
 	public TemplateDisposeResponse templateDisposeList(TemplateDisposeRequest templateDisposeRequest) {
 		TemplateDisposeExample example=new TemplateDisposeExample();
 		Criteria cr = example.createCriteria();
-		if(templateDisposeRequest.getId()!=null)
-		if(templateDisposeRequest.getUtmId()!=null)
-		if(templateDisposeRequest.getStatus()!=null)
+		if(templateDisposeRequest.getId()!=null) {
+			cr.andIdEqualTo(templateDisposeRequest.getId());
+		}
+		if(templateDisposeRequest.getUtmId()!=null) {
+			cr.andUtmIdEqualTo(templateDisposeRequest.getUtmId());
+		}
+		if(templateDisposeRequest.getStatus()!=null) {
+			cr.andStatusEqualTo(templateDisposeRequest.getStatus());
+		}
 		if(templateDisposeRequest.getTempName()!=null&&!templateDisposeRequest.getTempName().isEmpty())
-
-		templateDisposeMapper.countByExample(example);
-		return null;
+		if(templateDisposeRequest.getCreateTimeEnd()!=null&&templateDisposeRequest.getCreateTimeStart()!=null) {
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			Calendar cal = Calendar.getInstance();
+			try {
+				cal.setTime(simpleDateFormat.parse(templateDisposeRequest.getCreateTimeEnd()));
+				cal.add(Calendar.DATE, 1);
+				cr.andCreateTimeBetween(simpleDateFormat.parse(templateDisposeRequest.getCreateTimeStart()), cal.getTime());
+			} catch (ParseException e) {
+	//			logger.error("日期转换失败,不执行时间条件查询");
+			}
+		}
+		int count=templateDisposeMapper.countByExample(example);
+		TemplateDisposeResponse tr=new TemplateDisposeResponse();
+		tr.setRecordTotal(count);
+		if(count>0) {
+			 Paginator paginator = new Paginator(templateDisposeRequest.getCurrPage(),count,templateDisposeRequest.getPageSize());
+			 List<TemplateDispose> list = templateDisposeMapper.selectByExample(example);
+			 tr.setResultList(CommonUtils.convertBeanList(list,TemplateDisposeVO.class));
+		}
+		
+		return tr;
 	}
 
 	@Override
--- hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/resquest/admin/TemplateDisposeRequest.java
+++ hyjf_am_server/hyjf_am_model/src/main/java/com/hyjf/am/resquest/admin/TemplateDisposeRequest.java
@@ -366,4 +366,23 @@ public class TemplateDisposeRequest extends BasePage{
     public void setUpdateUserId(Integer updateUserId) {
         this.updateUserId = updateUserId;
     }
+    private String createTimeStart;
+    private String createTimeEnd;
+
+	public String getCreateTimeStart() {
+		return createTimeStart;
+	}
+
+	public void setCreateTimeStart(String createTimeStart) {
+		this.createTimeStart = createTimeStart;
+	}
+
+	public String getCreateTimeEnd() {
+		return createTimeEnd;
+	}
+
+	public void setCreateTimeEnd(String createTimeEnd) {
+		this.createTimeEnd = createTimeEnd;
+	}
+    
 }
