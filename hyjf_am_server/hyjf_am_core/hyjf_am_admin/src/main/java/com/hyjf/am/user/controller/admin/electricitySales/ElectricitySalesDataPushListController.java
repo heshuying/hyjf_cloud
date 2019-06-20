60e3d2d71342a584937034b3577445e2d6719294
dongzeshan <dongzeshan@hyjf.com>
2019-06-20 17:04:35 +0800

    修复utm娶不到

--- hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/user/controller/admin/electricitySales/ElectricitySalesDataPushListController.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/user/controller/admin/electricitySales/ElectricitySalesDataPushListController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hyjf.am.config.dao.model.auto.CustomerServiceChannel;
 import com.hyjf.am.config.dao.model.auto.CustomerServiceRepresentiveConfig;
 import com.hyjf.am.config.service.config.CustomerServiceRepresentiveConfigService;
 import com.hyjf.am.response.Response;
