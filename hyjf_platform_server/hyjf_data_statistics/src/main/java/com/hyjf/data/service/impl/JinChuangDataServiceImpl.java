ab038e37553f5d96456f966d572011b37ea9e7f3
zhangqingqing <zhangqingqing@hyjf.com>
2019-06-21 11:22:48 +0800

    还原代码

--- hyjf_platform_server/hyjf_data_statistics/src/main/java/com/hyjf/data/service/impl/JinChuangDataServiceImpl.java
+++ hyjf_platform_server/hyjf_data_statistics/src/main/java/com/hyjf/data/service/impl/JinChuangDataServiceImpl.java
@@ -1,55 +1,161 @@
-package com.hyjf.data.service.impl;
+package com.hyjf.data.market.service.impl;
 
+import com.hyjf.common.util.CommonUtils;
+import com.hyjf.data.bean.jinchuang.JcDataStatistics;
+import com.hyjf.data.bean.jinchuang.JcUserConversion;
+import com.hyjf.data.bean.jinchuang.JcUserPoint;
+import com.hyjf.data.bean.jinchuang.JcCustomerService;
 import com.hyjf.data.bean.jinchuang.JcRegisterTrade;
 import com.hyjf.data.bean.jinchuang.JcTradeAmount;
+import com.hyjf.data.bean.jinchuang.JcUserAnalysis;
+import com.hyjf.data.mongo.jinchuang.*;
+import com.hyjf.data.response.Interest;
 import com.hyjf.data.service.JinChuangDataService;
 import com.hyjf.data.vo.jinchuang.JcCustomerServiceVO;
 import com.hyjf.data.vo.jinchuang.JcDataStatisticsVO;
+import com.hyjf.data.vo.jinchuang.JcUserAnalysisVO;
 import com.hyjf.data.vo.jinchuang.JcUserConversionVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 /**
- * @Auther:yangchangwei
+ * @Author:yangchangwei
  * @Date:2019/6/18
  * @Description:
  */
 @Service
 public class JinChuangDataServiceImpl implements JinChuangDataService {
-    @Override
-    public JcUserConversionVO getUserConversion() {
-        return null;
-    }
+
+    @Autowired
+    private JcUserConversionDao userConversionDao;
+    @Autowired
+    private JcUserPointDao userPointDao;
+    @Autowired
+    private JcDataStatisticsDao statisticsDao;
+    @Autowired
+    private JcUserAnalysisDao analysisDao;
+    @Autowired
+    private JcTradeAmountDao tradeAmountDao;
+    @Autowired
+    private JcRegisterTradeDao registerTradeDao;
+    @Autowired
+    private JcCustomerServiceDao customerServiceDao;
 
     @Override
-    public List<JcDataStatisticsVO> getDataStatistics() {
-        return null;
+    public JcUserConversionVO getUserConversion() {
+        JcUserConversion userConversion = userConversionDao.getUserConversion();
+        JcUserConversionVO conversionVO = new JcUserConversionVO();
+        if (userConversion != null) {
+            conversionVO.setRegisterNum("100%");
+            conversionVO.setOpenAccountNum(userConversion.getOpenAccountNum() / userConversion.getRegisterNum() + "%");
+            conversionVO.setRechargeNum(userConversion.getRechargeNum() / userConversion.getRegisterNum() + "%");
+            conversionVO.setInvestNum(userConversion.getInvestNum() / userConversion.getRegisterNum() + "%");
+            conversionVO.setReInvestNum(userConversion.getReInvestNum() / userConversion.getRegisterNum() + "%");
+            return conversionVO;
+        }
+        return new JcUserConversionVO();
     }
 
     @Override
     public String getUserPoint() {
+        JcUserPoint userPoint = userPointDao.getUserPoint();
+        if (userPoint != null) {
+            return userPoint.getJo().toString();
+        }
         return null;
     }
 
     @Override
     public Map<String, Object> getUserAnalysis() {
+        JcUserAnalysisVO analysisVO = new JcUserAnalysisVO();
+        List<Interest> interests;
+        Map<String, Object> maps = new HashMap<>();
+        List<JcUserAnalysis> analysisList = analysisDao.getUserAnalysis();
+        Integer primaryInvest = 0;
+        Integer middleInvest = 0;
+        Integer seniorInvest = 0;
+        Integer highestInvest = 0;
+        Integer oneInvest = 0;
+        Integer twoInvest = 0;
+        Integer threeInvest = 0;
+        Integer fourInvest = 0;
+        Integer fiveInvest = 0;
+        Integer maleCount = 0;
+        Integer femaleCount = 0;
+        if (!CollectionUtils.isEmpty(analysisList)) {
+            for (JcUserAnalysis analysis : analysisList) {
+                primaryInvest += analysis.getPrimaryInvest();
+                middleInvest += analysis.getMiddleInvest();
+                seniorInvest += analysis.getSeniorInvest();
+                highestInvest += analysis.getHighestInvest();
+                oneInvest += analysis.getOneInvest();
+                twoInvest += analysis.getTwoInvest();
+                threeInvest += analysis.getThreeInvest();
+                fourInvest += analysis.getFourInvest();
+                fiveInvest += analysis.getFiveInvest();
+                maleCount += analysis.getMaleCount();
+                femaleCount += analysis.getFemaleCount();
+            }
+            Integer investCount = primaryInvest + middleInvest + seniorInvest + highestInvest;
+            Integer sumInvest = oneInvest + twoInvest + threeInvest + fourInvest + fiveInvest;
+            Integer person = maleCount + femaleCount;
+            analysisVO.setPrimaryInvest(primaryInvest / investCount + "%");
+            analysisVO.setMiddleInvest(middleInvest / investCount + "%");
+            analysisVO.setSeniorInvest(seniorInvest / investCount + "%");
+            analysisVO.setHighestInvest(highestInvest / investCount + "%");
+            analysisVO.setOneInvest(oneInvest / sumInvest + "%");
+            analysisVO.setTwoInvest(twoInvest / sumInvest + "%");
+            analysisVO.setThreeInvest(threeInvest / sumInvest + "%");
+            analysisVO.setFourInvest(fourInvest / sumInvest + "%");
+            analysisVO.setFiveInvest(fiveInvest / sumInvest + "%");
+            analysisVO.setMaleCount(maleCount / person + "%");
+            analysisVO.setFemaleCount(femaleCount / person + "%");
+            interests = CommonUtils.convertBeanList(analysisList, Interest.class);
+            maps.put("jcUserAnalysis", analysisVO);
+            maps.put("interests", interests);
+            return maps;
+        }
         return null;
     }
 
     @Override
     public JcTradeAmount getTradeAmount() {
-        return null;
+        return tradeAmountDao.getTradeAmount();
     }
 
     @Override
     public List<JcRegisterTrade> getRegisterTrade() {
-        return null;
+        return registerTradeDao.getRegisterTrade();
     }
 
     @Override
     public JcCustomerServiceVO getCustomerService() {
-        return null;
+        JcCustomerServiceVO customerServiceVO = new JcCustomerServiceVO();
+        JcCustomerService customerService = customerServiceDao.getCustomerService();
+        if (customerService != null) {
+            BeanUtils.copyProperties(customerService, customerServiceVO);
+            String complaint = customerService.getComplaintNum()/(customerService.getPhoneReception() + customerService.getQqReception() + customerService.getWxReception()) + "%";
+            customerServiceVO.setComplaintNum(complaint);
+        }
+        return customerServiceVO;
+    }
+
+    @Override
+    public List<JcDataStatisticsVO> getDataStatistics() {
+        List<JcDataStatistics> dataStatisticsList = statisticsDao.getDataStatistics();
+        List<JcDataStatisticsVO> statisticsVOS;
+        if (!CollectionUtils.isEmpty(dataStatisticsList)) {
+            statisticsVOS = CommonUtils.convertBeanList(dataStatisticsList, JcDataStatisticsVO.class);
+            return statisticsVOS;
+        }
+        return new ArrayList<>();
     }
 }
