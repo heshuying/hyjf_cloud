a0dd75d4733cf80112fe9804af7e22fe84c79ec7
nixiaoling <nixiaoling@hyjf.com>
2019-06-21 11:08:17 +0800

    着陆页管理_着陆页注册接口实现

--- hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/client/AmUserClient.java
+++ hyjf_cs_server/hyjf_cs_user/src/main/java/com/hyjf/cs/user/client/AmUserClient.java
@@ -6,6 +6,7 @@ import com.hyjf.am.resquest.trade.BatchUserPortraitQueryRequest;
 import com.hyjf.am.resquest.trade.MyInviteListRequest;
 import com.hyjf.am.resquest.user.*;
 import com.hyjf.am.vo.admin.AdminBankAccountCheckCustomizeVO;
+import com.hyjf.am.vo.admin.TemplateDisposeVO;
 import com.hyjf.am.vo.admin.UtmVO;
 import com.hyjf.am.vo.admin.locked.LockedUserInfoVO;
 import com.hyjf.am.vo.trade.BankReturnCodeConfigVO;
