4dcc1c1a8a4ed146b5dc67c36de93f90926bc384
jun <jijunjun@hyjf.com>
2019-06-21 17:15:01 +0800

    银行手机号同步列表,合作额度不足列表内容脱敏处理

--- hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/trade/service/admin/impl/AssetListServiceImpl.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/trade/service/admin/impl/AssetListServiceImpl.java
@@ -107,18 +107,6 @@ public class AssetListServiceImpl extends BaseServiceImpl implements AssetListSe
 			mapParam.put("limitEnd", limitEnd);
 		}
 		List<AssetListCustomizeVO> list = assetListServiceCustomizeMapper.findBZJBZList(mapParam);
-		if(!CollectionUtils.isEmpty(list)){
-			Map<String, String> assetStatusMap = CacheUtil.getParamNameMap("ASSET_STATUS");
-			Map<String, String> assetApplyStatusMap = CacheUtil.getParamNameMap("ASSET_APPLY_STATUS");
-			Map<String, String> accountStatusMap = CacheUtil.getParamNameMap("ACCOUNT_STATUS");
-			Map<String, String> userTypeMap = CacheUtil.getParamNameMap("USER_TYPE");
-			for(AssetListCustomizeVO assetListCustomizeVO : list){
-				assetListCustomizeVO.setStatus(assetStatusMap.getOrDefault(assetListCustomizeVO.getStatus(),null));
-				assetListCustomizeVO.setVerifyStatus(assetApplyStatusMap.getOrDefault(assetListCustomizeVO.getVerifyStatus(),null));
-				assetListCustomizeVO.setBankOpenAccount(accountStatusMap.getOrDefault(assetListCustomizeVO.getBankOpenAccount(),null));
-				assetListCustomizeVO.setUserType(userTypeMap.getOrDefault(assetListCustomizeVO.getUserType(),null));
-			}
-		}
 		return list;
 	}
 
