6f0559b37396a29515575db53b1fd6210a4366ca
zhangqingqing <zhangqingqing@hyjf.com>
2019-06-21 11:19:14 +0800

    添加swagger

--- hyjf_platform_server/hyjf_data_statistics/src/main/java/com/hyjf/data/service/impl/JinChuangDataServiceImpl.java
+++ hyjf_platform_server/hyjf_data_statistics/src/main/java/com/hyjf/data/service/impl/JinChuangDataServiceImpl.java
@@ -1,6 +1,15 @@
 package com.hyjf.data.service.impl;
 
+import com.hyjf.data.bean.jinchuang.JcRegisterTrade;
+import com.hyjf.data.bean.jinchuang.JcTradeAmount;
+import com.hyjf.data.service.JinChuangDataService;
+import com.hyjf.data.vo.jinchuang.JcCustomerServiceVO;
+import com.hyjf.data.vo.jinchuang.JcDataStatisticsVO;
+import com.hyjf.data.vo.jinchuang.JcUserConversionVO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther:yangchangwei
@@ -8,5 +17,39 @@ import org.springframework.stereotype.Service;
  * @Description:
  */
 @Service
-public class JinChuangDataServiceImpl {
+public class JinChuangDataServiceImpl implements JinChuangDataService {
+    @Override
+    public JcUserConversionVO getUserConversion() {
+        return null;
+    }
+
+    @Override
+    public List<JcDataStatisticsVO> getDataStatistics() {
+        return null;
+    }
+
+    @Override
+    public String getUserPoint() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getUserAnalysis() {
+        return null;
+    }
+
+    @Override
+    public JcTradeAmount getTradeAmount() {
+        return null;
+    }
+
+    @Override
+    public List<JcRegisterTrade> getRegisterTrade() {
+        return null;
+    }
+
+    @Override
+    public JcCustomerServiceVO getCustomerService() {
+        return null;
+    }
 }
