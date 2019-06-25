d67cd08ad073999107ce6760be6dfbb85b6dbc50
yaoyong <yaoyong@shizitegong.com>
2019-06-25 16:02:15 +0800

    销售日报mq延迟发送

--- hyjf_cs_server/hyjf_cs_market/src/main/java/com/hyjf/cs/market/service/impl/DailyGeneratorDataServiceImpl.java
+++ hyjf_cs_server/hyjf_cs_market/src/main/java/com/hyjf/cs/market/service/impl/DailyGeneratorDataServiceImpl.java
@@ -241,10 +241,10 @@ public class DailyGeneratorDataServiceImpl extends BaseMarketServiceImpl impleme
         params.put("startTime", startTime);
         params.put("endTime", endTime);
 		try {
-			commonProducer.messageSend(new MessageContent(MQConstant.SELL_DAILY_TOPIC,
-					MQConstant.SELL_DAILY_SELECT_TAG, UUID.randomUUID().toString(), params));
-		} catch (MQException e) {
-			logger.error("销日报发送消息失败......", e);
+			commonProducer.messageSendDelay2(new MessageContent(MQConstant.SELL_DAILY_TOPIC,
+					MQConstant.SELL_DAILY_SELECT_TAG, UUID.randomUUID().toString(), params), 1);
+		} catch (Exception e) {
+			logger.error("销售日报发送消息失败......", e);
 		}
     }
 
