825285ce829a7c61f10d99753ae1c92d3ca9bdad
sunshuaishuai <sss1234>
2019-07-09 15:40:18 +0800

    测试session过期。登录接口增加日志打印

--- hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/interceptor/AdminInterceptor.java
+++ hyjf_platform_server/hyjf_admin/src/main/java/com/hyjf/admin/interceptor/AdminInterceptor.java
@@ -43,6 +43,8 @@ public class AdminInterceptor implements HandlerInterceptor {
 		try {
 			String username = ((AdminSystemVO) request.getSession().getAttribute("user")).getUsername();
 			String val = RedisUtils.get(RedisConstants.ADMIN_UNIQUE_ID + username);
+			logger.info("cook:"+request.getHeader("Cookies"));
+			logger.info("user:"+ ((AdminSystemVO) request.getSession().getAttribute("user")).getUsername());
 			if (val != null && !val.equals(request.getHeader("Cookies"))) {
 				request.getSession().removeAttribute("user");
 				throw new ReturnMessageException(MsgEnum.ERR_USER_LOGIN_EXPIRE);
