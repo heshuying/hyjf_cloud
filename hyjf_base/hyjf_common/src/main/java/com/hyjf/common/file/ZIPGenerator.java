2d61cbe89c41e3730729f1dc741b5daac7d3cb22
zhadaojian <zhadaojian123>
2019-07-10 16:00:35 +0800

    协议压缩打包

--- hyjf_base/hyjf_common/src/main/java/com/hyjf/common/file/ZIPGenerator.java
+++ hyjf_base/hyjf_common/src/main/java/com/hyjf/common/file/ZIPGenerator.java
@@ -1,13 +1,16 @@
 package com.hyjf.common.file;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -90,7 +93,7 @@ public class ZIPGenerator {
          File f=subs.get(i);  
          zos.putNextEntry(new ZipEntry(baseName + f.getName()));     
          FileInputStream fis = new FileInputStream(f);     
-         byte[] buffer = new byte[20 * 1024];
+         byte[] buffer = new byte[1024];
          int r = 0;     
          while ((r = fis.read(buffer)) != -1) {     
              zos.write(buffer, 0, r);     
@@ -111,7 +114,7 @@ public class ZIPGenerator {
          File f=subs.get(i);  
          zos.putNextEntry(new ZipEntry(baseName + f.getName()));     
          FileInputStream fis = new FileInputStream(f);     
-         byte[] buffer = new byte[20 *1024];
+         byte[] buffer = new byte[1024];
          int r = 0;     
          while ((r = fis.read(buffer)) != -1) {     
              zos.write(buffer, 0, r);     
@@ -120,4 +123,56 @@ public class ZIPGenerator {
          f.delete();
         }  
    }
+    public static String createZipFile(List<File> fileList, String zipFileName) {
+
+        if(fileList == null || fileList.size() == 0 || StringUtils.isEmpty(zipFileName)){
+            return null;
+        }
+        //构建压缩文件File
+        File zipFile = new File(zipFileName);
+        //初期化ZIP流
+        ZipOutputStream out = null;
+        try{
+            //构建ZIP流对象
+            out = new ZipOutputStream(new FileOutputStream(zipFile));
+            //循环处理传过来的集合
+            for(int i = 0 ; i< fileList.size(); i++){
+                //获取目标文件
+                File inFile = fileList.get(i);
+                if(inFile.exists()){
+                    //定义ZipEntry对象
+                    ZipEntry entry = new ZipEntry(inFile.getName());
+                    //赋予ZIP流对象属性
+                    out.putNextEntry(entry);
+                    int len = 0 ;
+                    //缓冲
+                    byte[] buffer = new byte[1024];
+                    //构建FileInputStream流对象
+                    FileInputStream fis;
+                    fis = new FileInputStream(inFile);
+                    while ((len = fis.read(buffer))< 0) {
+                        out.write(buffer, 0, len);
+                        out.flush();
+                    }
+                    //关闭closeEntry
+                    out.closeEntry();
+                    //关闭FileInputStream
+                    fis.close();
+                }
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                //最后关闭ZIP流
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return zipFileName;
+
+    }
 }
