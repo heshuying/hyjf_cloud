33e28bb2485f60ba808d490cf3d6ce4f1d1b4dbf
dongzeshan <dongzeshan@hyjf.com>
2019-07-12 18:01:44 +0800

    修复分页问题

--- hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/user/service/admin/template/impl/TemplateDisposeServiceImpl.java
+++ hyjf_am_server/hyjf_am_core/hyjf_am_admin/src/main/java/com/hyjf/am/user/service/admin/template/impl/TemplateDisposeServiceImpl.java
@@ -58,7 +58,6 @@ public class TemplateDisposeServiceImpl implements TemplateDisposeService {
 		if(count>0) {
 			example.setOrderByClause("`create_time` Desc");
 			 Paginator paginator = new Paginator(templateDisposeRequest.getCurrPage(),count,templateDisposeRequest.getPageSize());
-
 		        example.setLimitStart(paginator.getOffset());
 		         example.setLimitEnd(paginator.getLimit());
 			 List<TemplateDispose> list = templateDisposeMapper.selectByExample(example);
