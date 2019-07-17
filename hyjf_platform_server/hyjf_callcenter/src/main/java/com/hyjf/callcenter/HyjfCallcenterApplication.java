a567aa7b6c6dae3250a43e294d2d70272abd8f0c
zhangqingqing <zhangqingqing@hyjf.com>
2019-07-17 10:09:28 +0800

    弃用callcenter

--- hyjf_platform_server/hyjf_callcenter/src/main/java/com/hyjf/callcenter/HyjfCallcenterApplication.java
+++ hyjf_platform_server/hyjf_callcenter/src/main/java/com/hyjf/callcenter/HyjfCallcenterApplication.java
@@ -2,10 +2,8 @@ package com.hyjf.callcenter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,7 +11,7 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.hyjf")
 public class HyjfCallcenterApplication {
 	@Bean
@@ -28,6 +26,7 @@ public class HyjfCallcenterApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(HyjfCallcenterApplication.class, args);
+		//不用了
+		//SpringApplication.run(HyjfCallcenterApplication.class, args);
 	}
 }
